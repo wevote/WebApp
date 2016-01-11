aafb49891e6b8a2890e96e9df3641c5f3541725c
Nick Fiorini <nf071590@gmail.com>
2016-01-10 23:55:44 -0500

    updating candidate route with back option and more opinions link to header

--- src/routes/Ballot/Candidate.jsx
+++ src/routes/Ballot/Candidate.jsx
@@ -1,4 +1,5 @@
 import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
 
 import CandidateDetail from 'components/Ballot/CandidateDetail';
 import CandidateStore from 'stores/CandidateStore';
@@ -28,12 +29,37 @@ export default class Candidate extends Component {
   componentWillUnmount () { }
 
   render() {
-    if (this.state.candidate)
-      return <CandidateDetail {...this.state.candidate} />;
-    else
-      return (
-        <div className="loading-wheel">loading...</div>
-      );
+    var content = this.state.candidate ?
+    <CandidateDetail {...this.state.candidate} /> :
+    (<div className="loading-wheel">loading...</div>);
+
+
+
+    {/* Trying to mirror from invisionApp design */}
+    return (
+      <div className='candidate-detail-route'>
+        <header className="row">
+          <div className="col-xs-6 col-md-6 text-center">
+            <Link to='/ballot'>
+              &lt; Back to My Ballot
+            </Link>
+          </div>
+          <div className="col-xs-6 col-md-6 text-center">
+            <i className="icon-icon-more-opinions-2-2 icon-light icon-medium">
+            </i>
+            <Link
+              to="/ballot/opinions"
+              className="font-darkest fluff-left-narrow">
+                More Opinions
+            </Link>
+          </div>
+        </header>
+
+        { content }
+      </div>
+
+    );
+
   }
 
   _onChange () { }
