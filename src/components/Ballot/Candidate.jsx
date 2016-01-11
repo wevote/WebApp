3decef76206968b68c9d0d5ae927481463aa44b1
Nick Fiorini <nf071590@gmail.com>
2016-01-11 16:52:43 -0500

    updating ballot items and other styles

--- src/components/Ballot/Candidate.jsx
+++ src/components/Ballot/Candidate.jsx
@@ -63,23 +63,26 @@ export default class Candidate extends Component {
     return (
       <section className="list-group-item">
         <StarAction we_vote_id={we_vote_id}/>
-        <div className="row">
-          <div className="pull-left col-xs-4 col-md-4">
+        <div className="row" style={{ paddingBottom: '10px' }}>
+          <div
+            className="col-xs-4"
+            style={candidate_photo_url ? {} : {height:'95px'}}>
 
             {/* adding inline style to img until Rob can style... */}
             {
               candidate_photo_url ?
 
                 <img
+                  className="img-circle"
                   style={{display:'block', width:'100px'}}
                   src={candidate_photo_url}
                   alt="candidate-photo"/> :
 
-              <i className="icon-xl icon-main icon-icon-person-placeholder-6-1 icon-light"/>
+              <i className="icon-lg icon-main icon-icon-person-placeholder-6-1 icon-light"/>
 
             }
           </div>
-          <div className="pull-right col-xs-8 col-md-8">
+          <div className="col-xs-8">
             <h4 className="bufferNone">
               <Link
                   className="linkLight"
@@ -88,13 +91,13 @@ export default class Candidate extends Component {
                   { ballot_item_display_name }
               </Link>
             </h4>
-            <p className="typeXLarge">
+            <h5>
               {supportCount} support
               <span className="small"> (more) </span>
-            </p>
-            <p className="bufferNone">
+            </h5>
+            <h6 className="bufferNone">
               { opposeCount } oppose
-            </p>
+            </h6>
           </div>
         </div>
         <ItemActionbar action={CandidateActions} we_vote_id={we_vote_id} />
