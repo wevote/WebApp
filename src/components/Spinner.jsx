1957a1877571f097bc9e04ca582ec7e13a3d090a
Rob Simpson <rsimpson2@me.com>
2016-01-11 20:43:43 -0500

    Loading more spinner component

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+class Spinner extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <svg className="spinner"
+                width="50px"
+                height="50px"
+                viewBox="0 0 66 66"
+                xmlns="http://www.w3.org/2000/svg">
+                <circle className="path"
+                        file="none"
+                        stokeWidth="5"
+                        stokeLinecap="round"
+                        cx="33"
+                        cy="33"
+                        r="30">
+                </circle>
+            </svg>
+        );
+    }
+};
+
+export default Spinner;
