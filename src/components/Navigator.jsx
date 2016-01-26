48bdf8015637e2a0ceeddc1c9b354e66f0dd5803
Rob Simpson <rsimpson2@me.com>
2016-01-25 19:52:21 -0500

    adding title attribute for icons with hidden text

--- src/components/Navigator.jsx
+++ src/components/Navigator.jsx
@@ -13,26 +13,33 @@ export default class Navigator extends Component {
                       <div className="row">
                         <Link to="/ballot">
                             <div className="col-xs-3 center-block text-center">
-                              <span className="glyphicon glyphicon-list-alt glyphicon-line-adjustment font-footer_icon"></span><br />
+                              <span className="glyphicon glyphicon-list-alt glyphicon-line-adjustment font-footer_icon"
+                                    title="Ballot">
+                              </span><br />
                               <span className="text-center small device-small--hide">Ballot</span>
                             </div>
                         </Link>
                         <Link to="/requests">
                             <div className="col-xs-3 center-block text-center">
-                              <span className="glyphicon glyphicon-inbox glyphicon-line-adjustment font-footer_icon">
+                              <span className="glyphicon glyphicon-inbox glyphicon-line-adjustment font-footer_icon"
+                                    title="Requests">
                               <span className="badgeTotal badge">10</span></span><br />
                               <span className="text-center small device-small--hide">Requests</span>
                             </div>
                         </Link>
                         <Link to="/connect">
                             <div className="col-xs-3 center-block text-center">
-                              <span className="glyphicon icon-icon-connect-1-3 font-footer_icon"></span><br />
+                              <span className="glyphicon icon-icon-connect-1-3 font-footer_icon"
+                                    title="Connect">
+                              </span><br />
                               <span className="text-center small device-small--hide">Connect</span>
                             </div>
                         </Link>
                         <Link to="/activity">
                             <div className="col-xs-3 center-block text-center">
-                              <span className="glyphicon icon-icon-activity-1-4 font-footer_icon"></span><br />
+                              <span className="glyphicon icon-icon-activity-1-4 font-footer_icon"
+                                    title="Activity">
+                              </span><br />
                               <span className="text-center small device-small--hide">Activity</span>
                             </div>
                         </Link>
