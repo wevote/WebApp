ea59c04e15286735036ba130705b9db7deacf69a
Rob Simpson <rsimpson2@me.com>
2016-02-08 00:16:03 -0500

    adding components for header and subheader

---
+++ src/js/components/Header.jsx
@@ -0,0 +1,31 @@
+import React, { Component, PropTypes } from 'react';
+import { Link } from 'react-router';
+import Headroom from "react-headroom";
+
+export default class Header extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <header className="row">
+        <Headroom>
+          <section className="separate-bottom container-fluid">
+            <h4 className="pull-left gutter-left--window bold">
+            <Link to="/more">
+              <span className="glyphicon glyphicon-menu-hamburger glyphicon-line-adjustment device-icon--large"></span>
+            </Link>
+              My Ballot
+            </h4>
+            <aside className="pull-right gutter-right--window gutter-top--small">
+              <Link to="/settings/location"className="font-lightest">
+                Oakland, CA
+              </Link>
+            </aside>
+          </section>
+        </Headroom>
+      </header>
+    );
+  }
+}
---
+++ src/js/components/SubHeader.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router';
+
+export default class SubHeader extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render () {
+    return (
+      <section className="container-fluid ballotList-bg fluff-tight--full separate-bottom">
+          <div className="row">
+            <div className="col-xs-6 col-md-6 text-center">
+              <i className="icon-icon-add-friends-2-1 icon-light icon-medium">
+              </i>
+              <Link
+                to="/friends/add"
+                className="font-darkest fluff-left-narrow utils-align--super">
+                  Add Friends
+              </Link>
+            </div>
+            <div className="col-xs-6 col-md-6 text-center">
+              <i className="icon-icon-more-opinions-2-2 icon-light icon-medium">
+              </i>
+              <Link
+                to="/opinions"
+                className="font-darkest fluff-left-narrow utils-align--super">
+                  More Opinions
+              </Link>
+            </div>
+          </div>
+      </section>
+    );
+  }
+}
