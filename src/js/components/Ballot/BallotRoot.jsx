1c772c0c2163913e6fb5969fc2485d18d0ba651e
nick fiorini <nf071590@gmail.com>
2016-02-22 18:50:51 -0500

    cleanup to requests

--- src/js/components/Ballot/BallotRoot.jsx
+++ src/js/components/Ballot/BallotRoot.jsx
@@ -1,35 +1,40 @@
 import React, { Component, PropTypes } from "react";
 
 import Office from "./Office";
+import Measure from "./Measure";
 
 export default class BallotRoot extends Component {
+
   static propTypes = {
+    // RAW ballot data from server
     ballot: PropTypes.array
   };
 
   constructor (props) {
     super(props);
     this.state = {};
+    this.ballot = this.props.ballot.map(this.mapItems);
+  }
 
-    this.ballot.map( (item) => {
+  mapItems (item) {
+    var {
+      we_vote_id: key,
+      kind_of_ballot_item: type,
+      ballot_item_display_name: displayName,
+      candidate_list: candidates
+    } = item;
 
-      var {
-        we_vote_id: key,
-        kind_of_ballot_item: type,
-        ballot_item_display_name: displayName,
-        candidate_list: candidates
-      } = item;
+    if (type === "OFFICE")
+      return <Office key={key} displayName={displayName} candidates={candidates} />;
 
-      if (type === "OFFICE")
-        return <Office key={key} displayName={displayName} candidates={candidates} />;
-
-    });
-
+    else if (type === "MEASURE")
+      return <Measure key={key} displayName={displayName} />;
   }
 
   render () {
     const ballot =
       <div className="ballot">
+        {this.ballot}
       </div>;
 
     return ballot;
