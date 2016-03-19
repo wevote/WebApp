df0a5858b282f1f587d8c954d8fb719590b7d6af
Rob Simpson <rsimpson2@me.com>
2016-03-19 12:37:52 -0400

    Margin and padding give space to container

--- src/js/routes/Home.jsx
+++ src/js/routes/Home.jsx
@@ -69,31 +69,31 @@ export default class Home extends Component {
 				<i className="fa fa-spinner fa-pulse"></i>
 				<p>Loading ... One Moment</p>
 			</div>
-        ) : 
+        ) :
         (
-        <div className="container-fluid well well-90">
-        <h2 className="text-center">We Vote Social Voter Guide</h2>
-		<ul className="list-group">
-			<li className="list-group-item">Research ballot items</li>
-			<li className="list-group-item">Learn from friends</li>
-			<li className="list-group-item">Take to the polls</li>
-		</ul>
+        <div className="container-fluid well well-90 gutter-top--small">
+          <h2 className="text-center">We Vote Social Voter Guide</h2>
+          <ul className="list-group">
+            <li className="list-group-item">Research ballot items</li>
+            <li className="list-group-item">Learn from friends</li>
+            <li className="list-group-item">Take to the polls</li>
+          </ul>
 
-		<ul className="list-group">
-			<li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;Neutral and private</li>
-			<li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;{this.state.voter_count} voters</li>
-			<li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;{this.state.organization_count} not-for-profit organizations</li>
-			<li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;and you.</li>
-		</ul>
-		<label htmlFor="address">My Ballot Location</label><br />
-		<span className="small">This is our best guess - feel free to change</span>
-		<Input type="text" name="address" className="form-control" defaultValue="Oakland, CA" />
-		<Link to="intro_opinions">
-			<Button bsStyle="primary">Go</Button>
-		</Link>
-		<br />
-		<br />
-		</div> 
+        <ul className="list-group">
+          <li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;Neutral and private</li>
+          <li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;{this.state.voter_count} voters</li>
+          <li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;{this.state.organization_count} not-for-profit organizations</li>
+          <li className="list-group-item"><span className="glyphicon glyphicon-small glyphicon-ok-sign"></span>&nbsp;and you.</li>
+        </ul>
+        <label htmlFor="address">My Ballot Location</label><br />
+        <span className="small">This is our best guess - feel free to change</span>
+        <Input type="text" name="address" className="form-control" defaultValue="Oakland, CA" />
+        <Link to="intro_opinions">
+          <Button bsStyle="primary">Go</Button>
+        </Link>
+        <br />
+        <br />
+        </div>
 		);
 
 		return view;
