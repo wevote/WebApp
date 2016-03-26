e3d9c07867ee47db4bd9a378259841a62ff37c51
nick fiorini <nf071590@gmail.com>
2016-03-26 14:04:28 -0400

    adding a production task, `npm run prod` that avoids browsersync and watching files for changes. also added a timestamp to the browsersync prefix and updated the server task to a module that takes production flag as input.

--- server.js
+++ server.js
@@ -2,15 +2,15 @@
 const express = require("express");
 const app = express();
 
-const port = 3003;
-const opts = {
-  redirect: true
-};
+module.exports = function (PROD) {
+  const port = PROD ? 3000 : 3003;
+  const opts = { redirect: true };
+  const hostname = PROD ? "wevote.me" : "localhost";
 
-app
-  .use( "/", express.static("build", opts))
-  .all( "*", (req, res) => res.sendFile(__dirname + "/build/index.html"))
-  .listen(port, () =>
-    console.log("INFO: " + "express server started", new Date()) ||
-    console.log("INFO: " + "Server is at http://localhost:%d", port)
-  );
+  app.use("/", express.static("build", opts));
+  app.all("*", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+  app.listen(port, () => {
+      console.log("INFO: " + "express server started", new Date());
+      console.log("INFO: " + `Server is at http://${hostname}:${port}`);
+  });
+};
