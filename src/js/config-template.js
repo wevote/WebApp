fd7c1434fc3432c89aa9575fa86db742505bcd75
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2016-05-07 12:01:48 -0700

    Migrated site hostname 'wevote.me' to a config variable. Added config-template.js file so we can track changes to the config file. Converted common css files to use public CDN for speed increase.

---
+++ src/js/config-template.js
@@ -0,0 +1,20 @@
+// Note that we import these values into "web_app_config" (so we can search for it)
+module.exports = {
+    WE_VOTE_HOSTNAME: "wevote.me",  // This should be without "https://"
+
+    WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
+    WE_VOTE_SERVER_API_ROOT_URL: "https://api.wevoteusa.org/apis/v1/",
+
+    DEBUG_MODE: true,
+
+    // Use 1 or 0 as opposed to true or false
+    test: {
+        use_test_election: 0
+    },
+
+    location: {
+        text_for_map_search: ""
+    },
+
+    FACEBOOK_APP_ID: ""
+};
