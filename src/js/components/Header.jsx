135444ce87b917335fd787164dcb72306c94ee1d
Jeff French <jfrench@ixley.com>
2016-06-19 12:43:49 -0700

    fix headroom styling

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -36,102 +36,100 @@ export default class Header extends Component {
     const logOut = FacebookActions.appLogout;
 
     const header =
-      <header className="page-header">
-        <section className="separate-bottom u-gutter-top--small bs-container-fluid">
-          {/* The components/MoreMenu code has to be reproduced here for mobile */}
-          <Menu styles={ menuStyles }>
-            <div className="device-menu--mobile">
-              <ul className="bs-nav bs-nav-stacked">
+      <section className="separate-bottom u-gutter-top--small bs-container-fluid">
+        {/* The components/MoreMenu code has to be reproduced here for mobile */}
+        <Menu styles={ menuStyles }>
+          <div className="device-menu--mobile">
+            <ul className="bs-nav bs-nav-stacked">
+              <li>
+                <div><span className="we-vote-promise">We Vote's Promise: We will never sell your email.</span></div>
+              </li>
+              <li>
+                <Link onClick={this.hide.bind(this)} to={{ pathname: "/ballot", query: { type: "filterRemaining" } }}>
+                  <div>
+                  Choices Remaining on My Ballot
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link onClick={this.hide.bind(this)} to={{ pathname: "/ballot", query: { type: "filterSupport" } }}>
+                  <div>
+                    What I Support on My Ballot
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link onClick={this.hide.bind(this)} to="/ballot">
+                  <div>
+                  All Ballot Items
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link onClick={this.hide.bind(this)} to="/bookmarks">
+                  <div>
+                  What I Have Bookmarked
+                  </div>
+                </Link>
+              </li>
+            </ul>
+            <h4 className="bs-text-left"></h4>
+            <ul className="bs-nav bs-nav-stacked">
+              <li>
+                <Link onClick={this.hide.bind(this)} to="/more/opinions/followed">
+                  <div>
+                  Who I'm Following
+                  </div>
+                </Link>
+              </li>
+              <li>
+                <Link onClick={this.hide.bind(this)} to="/settings/location">
+                  <div>
+                  My Address
+                  </div>
+                </Link>
+              </li>
+              { signed_in_personal ?
                 <li>
-                  <div><span className="we-vote-promise">We Vote's Promise: We will never sell your email.</span></div>
-                </li>
+                  <div onClick={logOut}>
+                    <a>
+                    Sign Out
+                    </a>
+                  </div>
+                </li> :
                 <li>
-                  <Link onClick={this.hide.bind(this)} to={{ pathname: "/ballot", query: { type: "filterRemaining" } }}>
+                  <Link onClick={this.hide.bind(this)} to="/more/sign_in">
                     <div>
-                    Choices Remaining on My Ballot
+                    Sign In
                     </div>
                   </Link>
-                </li>
-                <li>
-                  <Link onClick={this.hide.bind(this)} to={{ pathname: "/ballot", query: { type: "filterSupport" } }}>
-                    <div>
-                      What I Support on My Ballot
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link onClick={this.hide.bind(this)} to="/ballot">
-                    <div>
-                    All Ballot Items
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link onClick={this.hide.bind(this)} to="/bookmarks">
-                    <div>
-                    What I Have Bookmarked
-                    </div>
-                  </Link>
-                </li>
-              </ul>
-              <h4 className="bs-text-left"></h4>
-              <ul className="bs-nav bs-nav-stacked">
-                <li>
-                  <Link onClick={this.hide.bind(this)} to="/more/opinions/followed">
-                    <div>
-                    Who I'm Following
-                    </div>
-                  </Link>
-                </li>
-                <li>
-                  <Link onClick={this.hide.bind(this)} to="/settings/location">
-                    <div>
-                    My Address
-                    </div>
-                  </Link>
-                </li>
-                { signed_in_personal ?
-                  <li>
-                    <div onClick={logOut}>
-                      <a>
-                      Sign Out
-                      </a>
-                    </div>
-                  </li> :
-                  <li>
-                    <Link onClick={this.hide.bind(this)} to="/more/sign_in">
-                      <div>
-                      Sign In
-                      </div>
-                    </Link>
-                  </li> }
-              </ul>
-              <h4 className="bs-text-left"></h4>
-              <ul className="bs-nav bs-nav-stacked">
-                <li>
-                  <Link onClick={this.hide.bind(this)} to="/more/about">
-                    <div>
-                    About <strong>We Vote</strong>
-                    </div>
-                  </Link>
-                </li>
-              </ul>
-            </div>
-          </Menu>
-          <h4 className="bs-pull-left page-logo">
-            <Link to="/ballot">
-              My Voter Guide
-            </Link>
-            <span className="page-header__version"> demo</span>
-          </h4>
-          <div className="page-header__address bs-pull-right u-gutter-right--window">
-            <Link to="/settings/location" className="font-lightest">
-              {location}
-            </Link>
+                </li> }
+            </ul>
+            <h4 className="bs-text-left"></h4>
+            <ul className="bs-nav bs-nav-stacked">
+              <li>
+                <Link onClick={this.hide.bind(this)} to="/more/about">
+                  <div>
+                  About <strong>We Vote</strong>
+                  </div>
+                </Link>
+              </li>
+            </ul>
           </div>
-          <HeaderIcons />
-        </section>
-      </header>;
+        </Menu>
+        <h4 className="bs-pull-left page-logo">
+          <Link to="/ballot">
+            My Voter Guide
+          </Link>
+          <span className="page-header__version"> demo</span>
+        </h4>
+        <div className="page-header__address bs-pull-right u-gutter-right--window">
+          <Link to="/settings/location" className="font-lightest">
+            {location}
+          </Link>
+        </div>
+        <HeaderIcons />
+      </section>;
 
     return header;
   }
