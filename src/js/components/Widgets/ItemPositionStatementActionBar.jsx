7c9494238a0a9901b1367bb17fdc557fc201cd0f
Jeff French <jfrench@ixley.com>
2016-08-10 23:07:48 -0700

    styling position-statement WIP

--- src/js/components/Widgets/ItemPositionStatementActionBar.jsx
+++ src/js/components/Widgets/ItemPositionStatementActionBar.jsx
@@ -4,6 +4,7 @@ import { Link } from "react-router";
 import PositionInformationOnlySnippet from "../Widgets/PositionInformationOnlySnippet";
 import PositionSupportOpposeSnippet from "../Widgets/PositionSupportOpposeSnippet";
 import SupportActions from "../../actions/SupportActions";
+var Icon = require("react-svg-icons");
 
 export default class ItemPositionStatementActionBar extends Component {
   static propTypes = {
@@ -71,32 +72,33 @@ export default class ItemPositionStatementActionBar extends Component {
     var statement_placeholder_text;
     if (is_support) {
       if (this.props.ballot_item_display_name) {
-        statement_placeholder_text = "Why do you support " + this.props.ballot_item_display_name + "?";
+        statement_placeholder_text = "Why you support " + this.props.ballot_item_display_name + "…";
       } else {
-        statement_placeholder_text = "Why do you support?";
+        statement_placeholder_text = "Why you support…";
       }
     } else if (is_oppose) {
       if (this.props.ballot_item_display_name) {
-        statement_placeholder_text = "Why do you oppose " + this.props.ballot_item_display_name + "?";
+        statement_placeholder_text = "Why you oppose " + this.props.ballot_item_display_name + "…";
       } else {
-        statement_placeholder_text = "Why do you oppose?";
+        statement_placeholder_text = "Why you oppose…";
       }
     } else {
       if (this.props.ballot_item_display_name) {
-        statement_placeholder_text = "Share your thoughts about " + this.props.ballot_item_display_name + "...";
+        statement_placeholder_text = "Share your thoughts about " + this.props.ballot_item_display_name + "…";
       } else {
-        statement_placeholder_text = "Share your thoughts...";
+        statement_placeholder_text = "Share your thoughts…";
       }
     }
 
     let speaker_image_url_https = "";
+    let speaker_display_name = "Jeff";
 
     let image_placeholder = "";
     let speaker_type = "V";  // TODO DALE make this dynamic
     if (speaker_type === "O") {
-        image_placeholder = <i className="icon-org-lg icon-icon-org-placeholder-6-2 icon-org-resting-color position-item__avatar" />;
+        image_placeholder = <span className="position-statement__avatar"><Icon name="avatar-generic" width={34} height={34} /></span>;
     } else {
-        image_placeholder = <i className="icon-org-lg icon-icon-person-placeholder-6-1 icon-org-resting-color position-item__avatar" />;
+        image_placeholder = <span className="position-statement__avatar"><Icon name="avatar-generic" width={34} height={34} /></span>;
     }
 
     // The short version can be used to cut-off an exceedingly long comment. This applies to entries by the viewer,
@@ -106,39 +108,42 @@ export default class ItemPositionStatementActionBar extends Component {
     var no_statement_text = statement_text_to_be_saved.length ? false : true;
     var edit_mode = this.state.showEditPositionStatementInput || no_statement_text;
     const onSavePositionStatementClick = this.state.showEditPositionStatementInput ? this.closeEditPositionStatementInput.bind(this) : this.openEditPositionStatementInput.bind(this);
-    return <div className="item-positionstatementactionbar-outer-div">
+    return <div className="position-statement__container">
       { // Show the edit box
         edit_mode ?
         <form onSubmit={this.savePositionStatement.bind(this)}>
-          <div className="bs-input-group position-statement-input-div">
+          <div className="position-statement">
             { speaker_image_url_https ?
-              <img className="bs-img-square position-item__avatar"
+              <img className="position-statement__avatar"
                     src={speaker_image_url_https}
-                    width="50px"
+                    width="34px"
               /> :
             image_placeholder }
-            <input
-              type="text"
-              onChange={this.updateStatementTextToBeSaved.bind(this)}
-              name="statement_text_to_be_saved"
-              value={statement_text_to_be_saved}
-              className="position-statement-input"
-              placeholder={statement_placeholder_text}
-            />
-            <button className="bs-btn bs-btn-default" type="submit">Post</button>
+            <span className="position-statement__input-group">
+              <input
+                type="text"
+                onChange={this.updateStatementTextToBeSaved.bind(this)}
+                name="statement_text_to_be_saved"
+                value={statement_text_to_be_saved}
+                className="position-statement__input bs-form-control"
+                placeholder={statement_placeholder_text}
+              />
+              <button className="bs-btn bs-btn-default" type="submit">Post</button>
+            </span>
           </div>
         </form> :
         // Show the comment, but in read-only mode
-        <div className={short_version ? "position-statement__description-container--truncated" : "position-statement__description-container"}>
+        <div className={short_version ? "position-statement--truncated" : "position-statement"}>
           { speaker_image_url_https ?
-            <img className="bs-img-square position-item__avatar"
+            <img className="position-item__avatar"
                   src={speaker_image_url_https}
-                  width="50px"
+                  width="34px"
             /> :
           image_placeholder }
           <span className="position-statement__description edit-position-action"
                 onClick={onSavePositionStatementClick}
                 title="Edit this position">
+            <span className="position-statement__speaker-name">{speaker_display_name} </span>
             {statement_text_to_be_saved}
           </span>
 
