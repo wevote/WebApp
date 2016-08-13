28ec480b8adaca4eae1e8351c41c6920e6ec5cac
Jeff French <jfrench@ixley.com>
2016-08-13 12:44:00 -0700

    custom compile of bootstrap

--- Gulpfile.js
+++ Gulpfile.js
@@ -13,6 +13,10 @@ const buffer = require("vinyl-buffer");
 const del = require("del");
 const server = require("./server");
 
+const config = {
+    bootstrapDir: './node_modules/bootstrap-sass'
+};
+
 const PRODUCTION = process.env.NODE_ENV === "production";
 
 gulp.task("browserify", function () {
@@ -50,6 +54,7 @@ gulp.task("browserify", function () {
 
 });
 
+// Run server
 gulp.task("server", PRODUCTION ? () => server(PRODUCTION) : function () {
   server();
   // only start browserSync when this is development
@@ -65,6 +70,16 @@ gulp.task("server", PRODUCTION ? () => server(PRODUCTION) : function () {
   });
 });
 
+// Compile Bootstrap allowing for custom variables and selective imports
+gulp.task('compile-bootstrap', function() {
+    return gulp.src('./src/sass/bootstrap.scss')
+    .pipe(sass({
+        includePaths: [config.bootstrapDir + '/assets/stylesheets'],
+    }))
+    .pipe(gulp.dest("./build/css/"));
+});
+
+// Compile main stylesheet and copy to Build directory
 gulp.task("sass", function () {
   return gulp.src("./src/sass/main.scss")
   .pipe(sourcemaps.init())
@@ -76,45 +91,54 @@ gulp.task("sass", function () {
   .pipe(browserSync.stream());
 });
 
+// Clean out Build directory
 gulp.task("clean:build", function () {
   return del.sync(["./build/**"]);
 });
 
+// Copy font files to Build directory
 gulp.task("copy-fonts", function () {
   gulp.src("./src/sass/base/fonts/**")
     .pipe(gulp.dest("./build/fonts"))
     .pipe(browserSync.stream());
 });
 
+// Copy Index page to Build directory
 gulp.task("copy-index", function () {
   gulp.src("./src/index.html")
     .pipe(gulp.dest("./build"))
     .pipe(browserSync.stream());
 });
 
-gulp.task("prefix", ["copy-css"], function() {
+// Prefix all Bootstrap classes with 'bs-' for namespacing
+gulp.task("prefix", ["compile-bootstrap"], function() {
   return gulp.src("./build/css/bootstrap.css")
     .pipe(classPrefix("bs-"))
     .pipe(gulp.dest("./build/css"));
 });
 
+// Copy CSS files to Build directory
 gulp.task("copy-css", function () {
   return gulp.src("./src/css/**/*.css")
     .pipe(gulp.dest("./build/css"))
     .pipe(browserSync.stream());
 });
 
+// Copy image files to Build directory
 gulp.task("copy-img", function () {
   return gulp.src("./src/img/**/*")
     .pipe(gulp.dest("./build/img"))
     .pipe(browserSync.stream());
 });
 
-gulp.task("build", ["copy-fonts", "copy-index", "prefix", "copy-css", "copy-img", "browserify", "sass"]);
+// Build tasks
+gulp.task("build", ["copy-fonts", "copy-index", "compile-bootstrap", "prefix", "copy-css", "copy-img", "browserify", "sass"]);
 
+// Watch tasks
 gulp.task("watch", ["build"], PRODUCTION ? ()=>{} : function () {
   gulp.watch(["./src/index.html"], ["copy-index"]);
   gulp.watch(["./src/sass/base/base/fonts/**"], ["copy-fonts"]);
+  gulp.watch(["./src/sass/bootstrap/**"], ["compile-bootstrap"]);
   gulp.watch(["./build/css/bootstrap.css"], ["prefix"]);
   gulp.watch(["./src/css/**/*.css"], ["copy-css"]);
   gulp.watch(["./src/img/**/*"], ["copy-img"]);
@@ -122,6 +146,7 @@ gulp.task("watch", ["build"], PRODUCTION ? ()=>{} : function () {
   gulp.watch(["./src/js/**/*.js?(x)"], ["browserify"]);
 });
 
+// Default
 gulp.task("default", [
   "clean:build",
   "watch",
