67d9fab3ffe3ccf873efef9ce7c6833697d3fdb3
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2016-09-07 13:13:32 -0700

    Transitioning to compressed contests on the main ballot page.

---
+++ src/js/components/Ballot/MeasureItemCompressed.jsx
@@ -0,0 +1,85 @@
+import React, { Component, PropTypes } from "react";
+import { Link, browserHistory } from "react-router";
+import StarAction from "../../components/Widgets/StarAction";
+import SupportStore from "../../stores/SupportStore";
+import { capitalizeString } from "../../utils/textFormat";
+
+export default class MeasureItemCompressed extends Component {
+  static propTypes = {
+    key: PropTypes.string,
+    we_vote_id: PropTypes.string.isRequired,
+    measure_subtitle: PropTypes.string,
+    measure_text: PropTypes.string,
+    kind_of_ballot_item: PropTypes.string.isRequired,
+    ballot_item_display_name: PropTypes.string.isRequired,
+    link_to_ballot_item_page: PropTypes.bool,
+    measure_url: PropTypes.string
+  };
+  constructor (props) {
+    super(props);
+    this.state = {transitioning: false};
+  }
+
+  componentDidMount () {
+    this.supportStoreListener = SupportStore.addListener(this._onChange.bind(this));
+    this.setState({ supportProps: SupportStore.get(this.props.we_vote_id) });
+  }
+
+  componentWillUnmount () {
+    this.supportStoreListener.remove();
+  }
+
+  _onChange () {
+    this.setState({ supportProps: SupportStore.get(this.props.we_vote_id), transitioning: false });
+  }
+  render () {
+    const { supportProps, transitioning } = this.state;
+    let { ballot_item_display_name, measure_subtitle,
+          measure_text, we_vote_id } = this.props;
+    let measureLink = "/measure/" + we_vote_id;
+    let goToMeasureLink = function () { browserHistory.push(measureLink); };
+
+    measure_subtitle = capitalizeString(measure_subtitle);
+    ballot_item_display_name = capitalizeString(ballot_item_display_name);
+
+    return <div className="measure-card__container">
+      <div className="measure-card">
+        <div className="measure-card__media-object">
+          <div className="measure-card__media-object-anchor">
+
+          </div>
+          <div className="measure-card__media-object-content">
+            {
+              supportProps && supportProps.is_support ?
+              <img src="/img/global/icons/thumbs-up-color-icon.svg" className="measure-card__position-icon" width="20" height="20" /> : null
+            }
+            {
+              supportProps && supportProps.is_oppose ?
+              <img src="/img/global/icons/thumbs-down-color-icon.svg" className="measure-card__position-icon" width="20" height="20" /> : null
+            }
+            <h2 className="measure-card__display-name">
+              { this.props.link_to_ballot_item_page ?
+                <Link to={measureLink}>{ballot_item_display_name}</Link> :
+                  ballot_item_display_name
+              }
+            </h2>
+            <StarAction we_vote_id={we_vote_id} type="MEASURE"/>
+
+            <div className={ this.props.link_to_ballot_item_page ?
+                    "cursor-pointer" : null }
+                  onClick={ this.props.link_to_ballot_item_page ?
+                    goToMeasureLink : null }>{measure_subtitle}</div>
+              { this.props.measure_text ?
+                <div className="measure_text">{measure_text}</div> :
+                null }
+
+            <div className="row" style={{ paddingBottom: "0.5rem" }}>
+              <div className="col-xs-12">
+              </div>
+            </div>
+              </div> {/* END .measure-card__media-object-content */}
+            </div> {/* END .measure-card__media-object */}
+          </div>
+        </div>;
+      }
+    }
---
+++ src/js/components/Ballot/OfficeItemCompressed.jsx
@@ -0,0 +1,81 @@
+import React, { Component, PropTypes } from "react";
+import { Link, browserHistory } from "react-router";
+import StarAction from "../../components/Widgets/StarAction";
+import SupportStore from "../../stores/SupportStore";
+import { capitalizeString } from "../../utils/textFormat";
+
+export default class OfficeItemCompressed extends Component {
+  static propTypes = {
+    key: PropTypes.string,
+    we_vote_id: PropTypes.string.isRequired,
+    kind_of_ballot_item: PropTypes.string.isRequired,
+    ballot_item_display_name: PropTypes.string.isRequired,
+    link_to_ballot_item_page: PropTypes.bool,
+  };
+  constructor (props) {
+    super(props);
+    this.state = {transitioning: false};
+  }
+
+  componentDidMount () {
+    this.supportStoreListener = SupportStore.addListener(this._onChange.bind(this));
+    this.setState({ supportProps: SupportStore.get(this.props.we_vote_id) });
+  }
+
+  componentWillUnmount () {
+    this.supportStoreListener.remove();
+  }
+
+  _onChange () {
+    this.setState({ supportProps: SupportStore.get(this.props.we_vote_id), transitioning: false });
+  }
+  render () {
+    const { supportProps, transitioning } = this.state;
+    let { ballot_item_display_name, we_vote_id } = this.props;
+    let officeLink = "/office/" + we_vote_id;
+    let goToOfficeLink = function () { browserHistory.push(officeLink); };
+
+    ballot_item_display_name = capitalizeString(ballot_item_display_name);
+
+    return <div className="office-card__container">
+      <div className="office-card">
+        <div className="office-card__media-object">
+          <div className="office-card__media-object-anchor">
+
+          </div>
+          <div className="office-card__media-object-content">
+            {
+              supportProps && supportProps.is_support ?
+              <img src="/img/global/icons/thumbs-up-color-icon.svg" className="office-card__position-icon" width="20" height="20" /> : null
+            }
+            {
+              supportProps && supportProps.is_oppose ?
+              <img src="/img/global/icons/thumbs-down-color-icon.svg" className="office-card__position-icon" width="20" height="20" /> : null
+            }
+            <h2 className="office-card__display-name">
+              { this.props.link_to_ballot_item_page ?
+                <Link to={officeLink}>{ballot_item_display_name}</Link> :
+                  ballot_item_display_name
+              }
+            </h2>
+            <StarAction we_vote_id={we_vote_id} type="MEASURE"/>
+
+            <div className={ this.props.link_to_ballot_item_page ?
+                    "cursor-pointer" : null }
+                  onClick={ this.props.link_to_ballot_item_page ?
+                    goToOfficeLink : null }>
+              { this.props.candidate_list.map( (one_candidate) =>
+                <span key={one_candidate.we_vote_id}>{one_candidate.ballot_item_display_name}. </span>)
+              }
+            </div>
+
+            <div className="row" style={{ paddingBottom: "0.5rem" }}>
+              <div className="col-xs-12">
+              </div>
+            </div>
+              </div> {/* END .office-card__media-object-content */}
+            </div> {/* END .office-card__media-object */}
+          </div>
+        </div>;
+      }
+    }
