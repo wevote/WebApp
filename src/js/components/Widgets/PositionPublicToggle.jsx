4f33f8a3b04e09a14068165eeda9e82d57057c37
Jeff French <jfrench@ixley.com>
2016-09-10 23:44:05 -0700

    use icons with tooltips for visibility toggle

--- src/js/components/Widgets/PositionPublicToggle.jsx
+++ src/js/components/Widgets/PositionPublicToggle.jsx
@@ -1,6 +1,8 @@
 import React, { Component, PropTypes } from "react";
 import SupportActions from "../../actions/SupportActions";
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
 import ReactBootstrapToggle from "react-bootstrap-toggle";
+const Icon = require("react-svg-icons");
 
 export default class PositionPublicToggle extends Component {
   static propTypes = {
@@ -21,7 +23,6 @@ export default class PositionPublicToggle extends Component {
     this.setState({transitioning: false});
   }
 
-
   showItemToFriendsOnly () {
     if (this.state.transitioning){ return; }
     SupportActions.voterPositionVisibilitySave(this.props.ballot_item_we_vote_id, this.props.type, "FRIENDS_ONLY");
@@ -40,8 +41,12 @@ export default class PositionPublicToggle extends Component {
     }
 
     var { is_public_position } = this.props.supportProps;
-    let on = "Public";
-    let off = "Friends";
+    let vPublic = "Public";
+    let vFriendsOnly = "Your friends";
+    const publicIcon = <Icon name="public-icon" color="#fff" width={18} height={18} />;
+    const friendsIcon = <Icon name="group-icon" color="#555" width={18} height={18} />;
+    const tooltip = <Tooltip id="tooltip">{is_public_position ? vPublic : vFriendsOnly}</Tooltip>;
+
     var onChange;
     if (is_public_position) {
       onChange = this.showItemToFriendsOnly.bind(this);
@@ -49,13 +54,16 @@ export default class PositionPublicToggle extends Component {
       onChange = this.showItemToPublic.bind(this);
     }
 
+
     const positionPublicToggle =
     <div className={this.props.className}>
-      <ReactBootstrapToggle on={on} off={off} active={is_public_position}
-                            onstyle="success" size="mini"
-                            width="55px" height="20px"
-                            onChange={onChange} />
-                        </div>;
+      <OverlayTrigger className="trigger" placement="top" overlay={tooltip}><div>
+        <ReactBootstrapToggle on={publicIcon} off={friendsIcon} active={is_public_position}
+                              onstyle="success" size="mini"
+                              width="40px"
+                              onChange={onChange} />
+                            </div></OverlayTrigger>
+                          </div>;
     return positionPublicToggle;
   }
 }
