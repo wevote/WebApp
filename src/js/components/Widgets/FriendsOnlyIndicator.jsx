52a54a4256f8486e90124b4b992b7139449aa929
Jeff French <jfrench@ixley.com>
2016-09-11 12:25:41 -0700

    change visibility label to use icons

--- src/js/components/Widgets/FriendsOnlyIndicator.jsx
+++ src/js/components/Widgets/FriendsOnlyIndicator.jsx
@@ -1,4 +1,6 @@
 import React, { Component, PropTypes } from "react";
+import { Tooltip, OverlayTrigger } from "react-bootstrap";
+const Icon = require("react-svg-icons");
 
 export default class FriendsOnlyIndicator extends Component {
   static propTypes = {
@@ -16,14 +18,21 @@ export default class FriendsOnlyIndicator extends Component {
       is_friends_only = true;
     }
     let labelText = "";
+    let visibilityIcon = "";
     if (is_friends_only) {
       labelText = "Friends";
+      visibilityIcon = <Icon name="group-icon" color="#999" width={18} height={18} />;
     } else {
       labelText = "Public";
+      visibilityIcon = <Icon name="public-icon" color="#999" width={18} height={18} />;
     }
 
-    const friendsOnlyIndicator = <span className="friends-only-indicator">{labelText}</span>;
+    const tooltip = <Tooltip id="tooltip">{labelText}</Tooltip>;
 
-    return friendsOnlyIndicator;
+    const visibilityIndicator = (<OverlayTrigger placement="top" overlay={tooltip}>
+        <div className="visibility-indicator">{visibilityIcon}</div>
+      </OverlayTrigger>);
+
+    return visibilityIndicator;
 	}
 }
