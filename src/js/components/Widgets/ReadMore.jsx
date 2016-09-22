b1244890476a5105ea81e197ee18bf04169e7090
zachmonteith <zachmonteith@gmail.com>
2016-09-21 19:59:18 -0700

    ReadMore expanded text now only returns single line break

--- src/js/components/Widgets/ReadMore.jsx
+++ src/js/components/Widgets/ReadMore.jsx
@@ -15,13 +15,11 @@ export default class ReadMore extends Component {
       this.state = {
         readMore: true
       };
-
       this.toggleLines = this.toggleLines.bind(this);
     }
 
     toggleLines (event) {
         event.preventDefault();
-
         this.setState({
             readMore: !this.state.readMore
         });
@@ -38,11 +36,21 @@ export default class ReadMore extends Component {
         if (collapse_text === undefined) {
           collapse_text = " ...Less";
         }
-        let expanded_text_to_display = text_to_display.split(/(?:\r\n|\r|\n)/g).map(function (item, key){
-          return <span key={key}>
+        let expanded_text_array = text_to_display.split(/(?:\r\n|\r|\n)+/g);
+
+        let expanded_text_to_display = expanded_text_array.map(function (item, key){
+          if (item.trim() === "") {
+            return null;
+          } else if (key === 0) {
+            return <span key={key}>
               {item}
-              <br/>
-            </span>;
+              </span>;
+          } else {
+          return <span key={key}>
+            <br/>
+            <br/>
+              {item}
+            </span>; }
         });
 
         if (this.state.readMore) {
@@ -55,7 +63,7 @@ export default class ReadMore extends Component {
               />
           </span>;
         } else {
-          return <span>{expanded_text_to_display}<a href="#" onClick={this.toggleLines}>{collapse_text}</a></span>;
+          return <span>{expanded_text_to_display}<a href="#" onClick={this.toggleLines}><br/>{collapse_text}</a></span>;
         }
     }
 }
