c3bcea8f22beda6c8bd4999c8958f01902a61ba5
zachmonteith <zachmonteith@gmail.com>
2016-09-28 10:39:12 -0700

    fix for Copy Link problem on mobile #487

--- src/js/components/Widgets/CopyLinkModal.jsx
+++ src/js/components/Widgets/CopyLinkModal.jsx
@@ -29,20 +29,32 @@ export default class CopyLinkModal extends Component {
 
 render () {
   let urlBeingShared = this.props.urlBeingShared;
+  let browser_supports_CopyToClipboard = true; //latest iOS update supports CopyToClipboard, check for users version and let them copy if latest, perhaps with npm pckg "mobile-detect"
+  let copy_btn_className;
+  let select_all_button;
+  if (browser_supports_CopyToClipboard) {
+    copy_btn_className = "copy-btn"; // display copy button at all times
+    select_all_button = null;
+  } else {
+    copy_btn_className = "copy-btn__hide-mobile"; // display: none; in mobile view
+    select_all_button = <button className="select-all-btn btn btn-default" onClick={()=>{this.refs.input.select();}}>Select All</button>;
+  }
+
   return <Modal {...this.props} bsSize="large" aria-labelledby="contained-modal-title-lg">
     <Modal.Header closeButton>
       <Modal.Title id="contained-modal-title-lg">Copy link to clipboard</Modal.Title>
     </Modal.Header>
     <Modal.Body>
       <div className="input-group">
-        <input value={urlBeingShared} className="form-control" style={{marginTop: "17px"}} onChange={({target: {value}}) => this.setState({value, copied: false})} />&nbsp;
+        <input ref="input" value={urlBeingShared} className="form-control" style={{marginTop: "17px"}} onChange={({target: {value}}) => this.setState({value, copied: false})} onFocus={()=>{this.refs.input.select();}} />&nbsp;
           <span className="input-group-btn">
-        <CopyToClipboard text={urlBeingShared} onCopy={() => this.setState({copied: true})}>
-          <button className="copy-btn btn btn-default">Copy</button>
-        </CopyToClipboard>
-        </span>
+            <CopyToClipboard text={urlBeingShared} onCopy={() => this.setState({copied: true})}>
+              <button className={"btn btn-default " + copy_btn_className}>Copy</button>
+            </CopyToClipboard>
+            {select_all_button}
+          </span>
       </div>
-        {this.state.copied ? <span style={{color: "red"}}>Copied.  Can now paste into an email or social media!</span> : null}
+    {this.state.copied ? <span style={{color: "red"}}>Copied.  Can now paste into an email or social media!</span> : null}
     </Modal.Body>
   </Modal>;
   }
