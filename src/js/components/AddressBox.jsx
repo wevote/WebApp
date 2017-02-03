17ff610a809cea4d3aa7fc1842b6158f2a10d7a9
Sarah Clements <biting.the.sun@gmail.com>
2017-02-03 15:14:02 -0800

    Added google Autocomplete API to address search box on ballot route

--- src/js/components/AddressBox.jsx
+++ src/js/components/AddressBox.jsx
@@ -42,18 +42,48 @@ export default class AddressBox extends Component {
     browserHistory.push(this.props.saveUrl);
   }
 
+  _placeChanged (addressAutocomplete) {
+    let place = addressAutocomplete.getPlace();
+
+    if(place.formatted_address) {
+      this.setState({
+        voter_address: place.formatted_address
+      });
+      console.log("place.formatted_address: " + place.formatted_address);
+      console.log("formatted address state change: " + this.state.voter_address);
+    } else {
+      this.setState({
+        voter_address: place.name
+      });
+      console.log("place.name: " + place.name);
+      console.log("place.name state change: " + this.state.voter_address);
+    }
+  }
+
   updateVoterAddress (event) {
-    this.setState({
-      voter_address: event.target.value
-    });
+    let addressAutocomplete = new google.maps.places.Autocomplete(this.refs.autocomplete);
+
+    addressAutocomplete.addListener('place_changed', this._placeChanged.bind(this, addressAutocomplete));
+  }
+
+  onSearchBlur () {
+    // Delay closing the drop down so that a click on the Link can have time to work
+    setTimeout(() => {
+      //this.clear();
+    }, 250);
   }
 
   voterAddressSave (e) {
-    e.preventDefault();
-    var { voter_address } = this.state;
-    VoterActions.voterAddressSave(voter_address);
-    this.setState({loading: true});
+    console.log("state change: " + this.state.voter_address);
+    setTimeout(() => {
+      //this.clear();
+      e.preventDefault();
+      var { voter_address } = this.state;
+      VoterActions.voterAddressSave(voter_address);
+      this.setState({loading: true});
+    }, 250);
   }
+ // this was removed because the value was sometimes not letting me type a new address in the input {/*this.state.voter_address*/}
 
   render () {
     var { loading } = this.state;
@@ -66,8 +96,8 @@ export default class AddressBox extends Component {
           type="text"
           onChange={this.updateVoterAddress}
           name="address"
-          value={this.state.voter_address}
           className="form-control"
+          ref="autocomplete"
           placeholder="Enter address where you are registered to vote"
         />
         </form>
