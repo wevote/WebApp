70a7942b0950005c05513f829048902089a65304
Krishan Arya <krishanmarya1@gmail.com>
2017-02-14 02:41:53 -0700

    Retain ballot scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { render } from "react-dom";
-import { browserHistory, Router} from "react-router";
+import { browserHistory, Router, applyRouterMiddleware } from "react-router";
+import { useScroll } from 'react-router-scroll';
 import routes from "./Root";
 
 // polyfill
 if (!Object.assign) Object.assign = React.__spread;
 
-render(<Router history={browserHistory}>
+render(<Router history={browserHistory} render={applyRouterMiddleware(useScroll(()=>true))}>
     { routes() }
   </Router>, document.getElementById("app"));
