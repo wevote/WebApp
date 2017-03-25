9cf011c64b9f1a0ad9a12808b42275d016c5032e
Krishan Arya <krishanmarya1@gmail.com>
2017-03-24 17:02:17 -0700

    Refactor email functionality

--- src/js/utils/email-functions.js
+++ src/js/utils/email-functions.js
@@ -1,16 +1,5 @@
 
 export function validateEmail (email) {
-
-  if (arguments.length === 1) {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  } else {
-    let onePass = false;
-    Object.values(arguments).forEach(argument => {
-      if (validateEmail(argument)) {
-        onePass = true;
-      }
-    });
-    return onePass;
-  }
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
