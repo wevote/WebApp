1991e278bc7b0793d1b555beedb991120231143d
Dale John McGrew <dalemcgrew@gmail.com>
2017-04-08 09:13:15 -0700

    Changed variable names to lower case with dash format.

--- src/js/routes/Intro/GetStarted.jsx
+++ src/js/routes/Intro/GetStarted.jsx
@@ -74,7 +74,7 @@ export default class SignIn extends Component {
           {this.state.voter.signed_in_facebook ?
             null :
             <div className="row">
-              <div id = "facebookIntroConnect" className="col-md-4 col-md-offset-4 xs-block form-group">
+              <div id = "facebook-intro-connect" className="col-md-4 col-md-offset-4 xs-block form-group">
                 <FacebookSignIn /><br />
                 Sign in with Facebook so you can<br />
                 ask your friends for voting advice.<br />
@@ -84,7 +84,7 @@ export default class SignIn extends Component {
           {this.state.voter.signed_in_twitter ?
             null :
             <div className="row">
-              <div id = "twitterIntroConnect" className="col-md-4 col-md-offset-4 xs-block form-group">
+              <div id = "twitter-intro-connect" className="col-md-4 col-md-offset-4 xs-block form-group">
                 <TwitterSignIn /><br />
                 Sign in with Twitter to see<br />
                 the voter guides of everyone<br />
