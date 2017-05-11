73b3c0582573a09a453c36e75628a611534cb17f
stevepodell <steve@podell.com>
2017-05-10 17:05:03 -0700

    DonationList component, Dale was ok with inline react-bootstrap style settings, so I removed some className and some bsStyle props.  Now shows all of the subscriptions, (which until I make the sync feature, all show as active). DonationListForm now is suppressed if there is no data to show. ProcessingDonation: Improvement on React lifecycle usage.

--- src/js/components/Donation/DonationList.jsx
+++ src/js/components/Donation/DonationList.jsx
@@ -34,27 +34,28 @@ export default class DonationPaymentList extends Component {
 
   render () {
     if ( this.props.displayDonations ) {
-      return <div className="history-list">
-        <Panel style={styles.Panel}>
-          <Table striped condensed hover responsive>
-            <thead>
-            <tr>
-              <th style={styles.th}>Date</th>
-              <th style={styles.th}>Amount</th>
-              <th style={styles.th}>Payment</th>
-              <th style={styles.th}>Funding</th>
-              <th style={styles.th}>Card</th>
-              <th style={styles.th}>Ends with</th>
-              <th style={styles.th}>Expires</th>
-              <th style={styles.th}>Status</th>
-              <th style={styles.th}>Info</th>
-            </tr>
-            </thead>
-            <tbody> {typeof this.state.data !== "undefined" ? this.state.data.map(function (item, key) {
+      return <Panel style={styles.Panel}>
+        <Table striped condensed hover responsive>
+          <thead>
+          <tr>
+            <th style={styles.th}>Date</th>
+            <th style={styles.th}>Amount</th>
+            <th style={styles.th}>Payment</th>
+            <th style={styles.th}>Funding</th>
+            <th style={styles.th}>Card</th>
+            <th style={styles.th}>Ends with</th>
+            <th style={styles.th}>Expires</th>
+            <th style={styles.th}>Status</th>
+            <th style={styles.th}>Info</th>
+          </tr>
+          </thead>
+          <tbody> {this.state.data.map(function (item, key) {
+            if ( item.record_enum === "PAYMENT_FROM_UI" || item.record_enum === "PAYMENT_AUTO_SUBS") {
               return <tr key={key}>
                 <td style={styles.td}>{moment.utc(item.created).format("MMM D, YYYY")}</td>
                 <td style={styles.td}>{item.amount}</td>
-                <td style={styles.td}>Subscription</td>
+                <td style={styles.td}>{item.record_enum === "PAYMENT_FROM_UI" ? "One time" :
+                                                                                "Subscription"}</td>
                 <td style={styles.td}>{item.funding}</td>
                 <td style={styles.td}>{item.brand}</td>
                 <td style={styles.td}>{item.last4}</td>
@@ -62,44 +63,56 @@ export default class DonationPaymentList extends Component {
                 <td style={styles.td}>{item.stripe_status === "succeeded" ? "Paid" : item.stripe_status}</td>
                 <td style={styles.td}><Button bsSize="small" disabled>Details</Button></td>
               </tr>;
-            }) : ""}
-            </tbody>
-          </Table>
-        </Panel>
-      </div>;
+            } else {
+              return "";
+            }
+          })}
+          </tbody>
+        </Table>
+      </Panel>;
     } else {
-      return <div className="history-list">
-        <Panel style={styles.Panel}>
-          <Table striped condensed hover responsive>
-            <thead>
-            <tr>
-              <th style={styles.th}>Started</th>
-              <th style={styles.th}>Monthly</th>
-              <th style={styles.th}>Payment</th>
-              <th style={styles.th}>Funding</th>
-              <th style={styles.th}>Card</th>
-              <th style={styles.th}>Ends with</th>
-              <th style={styles.th}>Expires</th>
-              <th style={styles.th}>Status</th>
-              <th style={styles.th}>Info</th>
-            </tr>
-            </thead>
-            <tbody> {typeof this.state.data !== "undefined" ? this.state.data.map(function (item, key) {
+      return <Panel style={styles.Panel}>
+        <Table striped condensed hover responsive>
+          <thead>
+          <tr>
+            <th style={styles.th}>Active</th>
+            <th style={styles.th}>Started</th>
+            <th style={styles.th}>Monthly</th>
+            <th style={styles.th}>Funding</th>
+            <th style={styles.th}>Card</th>
+            <th style={styles.th}>Ends with</th>
+            <th style={styles.th}>Expires</th>
+            <th style={styles.th}>Status</th>
+            <th style={styles.th}>Ended</th>
+            <th style={styles.th}>Canceled</th>
+            <th style={styles.th}>Info</th>
+          </tr>
+          </thead>
+          <tbody> {this.state.data.map(function (item, key) {
+            if ( item.record_enum === "SUBS_SETUP_AND_INITIAL") {
+              let active = item.subs_canceled_at === null || item.subs_ended_at !== null;
+              let ended = item.subs_ended_at !== null ? moment.utc(item.subs_ended_at).format("MMM D, YYYY") : "";
+              let cancel = item.subs_canceled_at !== null ? moment.utc(item.subs_canceled_at).format("MMM D, YYYY") :"";
               return <tr key={key}>
+                <td style={styles.td}>{active ? "Active" : "----"}</td>
                 <td style={styles.td}>{moment.utc(item.created).format("MMM D, YYYY")}</td>
                 <td style={styles.td}>{item.amount}</td>
-                <td style={styles.td}>One time</td>
+                <td style={styles.td}>{item.funding}</td>
                 <td style={styles.td}>{item.brand}</td>
                 <td style={styles.td}>{item.last4}</td>
                 <td style={styles.td}>{item.exp_month + "/" + item.exp_year}</td>
                 <td style={styles.td}>{item.stripe_status === "succeeded" ? "Paid" : item.stripe_status}</td>
+                <td style={styles.td}>{ended}</td>
+                <td style={styles.td}>{cancel}</td>
                 <td style={styles.td}><Button bsSize="small" disabled>Details</Button></td>
               </tr>;
-            }) : ""}
-            </tbody>
-          </Table>
-        </Panel>
-      </div>;
+            } else {
+              return "";
+            }
+          })}
+          </tbody>
+        </Table>
+      </Panel>;
     }
   }
 }
