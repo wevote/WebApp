8d40c0cc0358f8dd9dd1c31b011d39e74d8c0c42
Steve Podell <steve@podell.com>
2017-06-29 09:41:08 -0700

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -3,13 +3,9 @@ module.exports = {
   WE_VOTE_URL_PROTOCOL: "http://",  // "http://" for local dev or "https://" for live server
   WE_VOTE_HOSTNAME: "localhost:3000",  // This should be without "http...". This is "WeVote.US" on live server.
 
-WE_VOTE_SERVER_ROOT_URL: "http://localhost:8000/",
-WE_VOTE_SERVER_ADMIN_ROOT_URL: "http://localhost:8000/admin/",
-WE_VOTE_SERVER_API_ROOT_URL: "http://localhost:8000/apis/v1/",
-
-//  WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
-//  WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
-//  WE_VOTE_SERVER_API_ROOT_URL: "https://api.wevoteusa.org/apis/v1/",
+  WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
+  WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
+  WE_VOTE_SERVER_API_ROOT_URL: "https://api.wevoteusa.org/apis/v1/",
 
   DEBUG_MODE: false,
 
@@ -24,5 +20,5 @@ WE_VOTE_SERVER_API_ROOT_URL: "http://localhost:8000/apis/v1/",
 
   FACEBOOK_APP_ID: "",
 
-  STRIPE_API_KEY: "pk_test_bWuWGC3jrMIFH3wvRvHR6Z5H"
+  STRIPE_API_KEY: ""
 };
