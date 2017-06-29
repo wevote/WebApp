3b53ff99700921fb36d36d4c88e27d40745152c3
stevepodell <steve@podell.com>
2017-06-29 09:07:11 -0700

    A first checkin for issue #863: show a list of issues in left pane.

--- src/js/config.js
+++ src/js/config.js
@@ -1,24 +1,28 @@
 // Note that we import these values into "web_app_config" (so we can search for it)
 module.exports = {
-    WE_VOTE_URL_PROTOCOL: "http://",  // "http://" for local dev or "https://" for live server
-    WE_VOTE_HOSTNAME: "localhost:3000",  // This should be without "http...". This is "WeVote.US" on live server.
+  WE_VOTE_URL_PROTOCOL: "http://",  // "http://" for local dev or "https://" for live server
+  WE_VOTE_HOSTNAME: "localhost:3000",  // This should be without "http...". This is "WeVote.US" on live server.
 
-    WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
-    WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
-    WE_VOTE_SERVER_API_ROOT_URL: "https://api.wevoteusa.org/apis/v1/",
+WE_VOTE_SERVER_ROOT_URL: "http://localhost:8000/",
+WE_VOTE_SERVER_ADMIN_ROOT_URL: "http://localhost:8000/admin/",
+WE_VOTE_SERVER_API_ROOT_URL: "http://localhost:8000/apis/v1/",
 
-    DEBUG_MODE: false,
+//  WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
+//  WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
+//  WE_VOTE_SERVER_API_ROOT_URL: "https://api.wevoteusa.org/apis/v1/",
 
-    // Use 1 or 0 as opposed to true or false
-    test: {
-        use_test_election: 0
-    },
+  DEBUG_MODE: false,
 
-    location: {
-        text_for_map_search: ""
-    },
+  // Use 1 or 0 as opposed to true or false
+  test: {
+    use_test_election: 0
+  },
 
-    FACEBOOK_APP_ID: "",
+  location: {
+    text_for_map_search: ""
+  },
 
-    STRIPE_API_KEY: ""
+  FACEBOOK_APP_ID: "",
+
+  STRIPE_API_KEY: "pk_test_bWuWGC3jrMIFH3wvRvHR6Z5H"
 };
