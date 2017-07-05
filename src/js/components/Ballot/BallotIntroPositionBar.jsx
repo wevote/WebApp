10e0ee65b96e8778e4df840553b396b0ad34c7c4
Keith Underwood <keithunderwood510@gmail.com>
2017-07-05 14:44:49 -0700

    Style position bar screen

--- src/js/components/Ballot/BallotIntroPositionBar.jsx
+++ src/js/components/Ballot/BallotIntroPositionBar.jsx
@@ -15,14 +15,16 @@ export default class BallotIntroPositionBar extends Component {
     return <div className="intro-modal">
       <div className="intro-modal__h1">Congratulations!</div>
       <div className="intro-story__h2">Now that you have a network of advisers, you will see a position bar next to ballot items..</div>
-      <div className="intro-story__h2">Position Bar</div>
-      <div className="intro-story__h2">
-        Position Bar Goes here
+      <div className="intro-modal__position-wrap">
+        <img className="intro-modal__position-img" src="/img/global/icons/position-bar-v1-265x43.png" alt="position bar" />
+        <div className="intro-modal__position-description-wrap">
+          <p className="intro-modal__p">Supporters in your network</p>
+          <p className="intro-modal__p">Opposers in your network</p>
+        </div>
       </div>
-      <div className="intro-modal__padding-btn">
-        <button type="button" className="btn btn-success" onClick={this.props.next}>See Your Ballot &nbsp;&nbsp;&gt;</button>
+      <div className="intro-modal__padding-btn__btn-center intro-modal__button-wrap">
+        <button type="button" className="btn btn-success intro-modal__button" onClick={this.props.next}>See Your Ballot &nbsp;&nbsp;&gt;</button>
       </div>
-      <br/>
     </div>;
   }
 }
