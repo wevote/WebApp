8660c27c8873b217fde67a99a502bbd32d34cdaf
stevepodell <steve@podell.com>
2017-07-11 19:00:10 -0700

    Fix for issue #934, page reload on Enter in money input for donation Donate.jsx render() mixed original bootstrap with react-bootstrap, so I redid it all in react-bootstrap to see if that solved the problem. There were a number of Stack overflow about similar issues, and the solution was to just swallow the Enter if in the input field.  The fix is in _handleKeyPress in Donate.jsx.

--- src/js/routes/More/Donate.jsx
+++ src/js/routes/More/Donate.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import { Button, FormGroup, Radio } from "react-bootstrap";
+import { Button, FormGroup, Radio, InputGroup, FormControl } from "react-bootstrap";
 import Helmet from "react-helmet";
 import DonationForm from "../../components/Donation/DonationForm";
 import DonationError from "../../components/Donation/DonationError";
@@ -64,6 +64,17 @@ export default class Donate extends Component {
     this.setState({ custom_amount: event.target.value });
   }
 
+  /*
+  An enter keystroke in the react-bootstrap InputGroup, (or in the original react "input-group",)
+  causes a page reload, and you lose context.  So swallow the 'Enter' keystroke event while in
+  the InputGroup.
+   */
+  _handleKeyPress(event) {
+    if (event.key == 'Enter') {
+      event.preventDefault();
+    }
+  }
+
   render () {
     return <div>
       <Helmet title="Donate - We Vote"/>
@@ -71,9 +82,9 @@ export default class Donate extends Component {
         <h1 className="h4">Your donations keep us going. Thank you!</h1>
 
         <div className="Donate">
-           {this.state.donationErrorMessage.length > 0 ?
-             <DonationError errorMessage={this.state.donationErrorMessage} /> :
-             <p>Please give what you can to help us reach more voters.</p>}
+          {this.state.donationErrorMessage.length > 0 ?
+            <DonationError errorMessage={this.state.donationErrorMessage} /> :
+            <p>Please give what you can to help us reach more voters.</p>}
           <div className="hidden-xs"><br /></div>
           <br />
           Gift Type:
@@ -110,21 +121,34 @@ export default class Donate extends Component {
             <br/>&nbsp;
             <br />
           </div>
-           {this.state.showCustomInput ? <span>
-             <form className="form-inline">
-              <div className="input-group">
-               <span className="input-group-addon">$</span>
-                <input className="form-control form-control mb-2 mr-sm-2 mb-sm-0"
-                       value={this.state.custom_amount} type="text" placeholder="250.00"
-                       onChange={this._updateCustomAmount} />
-              </div>&nbsp;
-                <DonationForm donationAmount={parseInt(parseFloat(
-                  this.state.custom_amount.replace(/[^0-9.]+/g, "")) * 100)}
-                donateMonthly={this.state.donateMonthly} donateButtonText="Go" />
-             </form></span> : null}
 
-            {isNaN(this.state.custom_amount) || this.state.custom_amount === "0" ?
-            <span><p>Please enter a valid number</p></span> : null}
+          {this.state.showCustomInput ?
+            <span>
+              <form className="form-inline">
+                <FormGroup>
+                  <InputGroup>
+                    <InputGroup.Addon>$</InputGroup.Addon>
+                    <FormControl type="text"
+                                 onKeyPress={this._handleKeyPress}
+                                 onChange={this._updateCustomAmount}
+                                 value={this.state.custom_amount}
+                                 placeholder="250.00" />
+                    <InputGroup.Button>
+                      <DonationForm donationAmount={parseInt(parseFloat(
+                          this.state.custom_amount.replace(/[^0-9.]+/g, "")) * 100)}
+                                    donateMonthly={this.state.donateMonthly}
+                                    donateButtonText="Go" donateOther />
+                    </InputGroup.Button>
+                  </InputGroup>
+                </FormGroup>
+              </form>
+            </span> : null }
+
+          {isNaN(this.state.custom_amount) || this.state.custom_amount === "0" ?
+            <span>
+              <p>Please enter a valid number</p>
+            </span> : null}
+
           <div className="hidden-xs"><br /></div>
           <br />
           Contributions or gifts are not tax deductible. We Vote is a 501(c)(4) nonprofit.<br />
