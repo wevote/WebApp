bf25a39625056b9bbb75667442220358817c69d7
edward-ly <edvard.ly@gmail.com>
2017-07-24 16:44:48 -0700

    Refactor EditAddress function as class

--- src/js/components/Widgets/EditAddress.jsx
+++ src/js/components/Widgets/EditAddress.jsx
@@ -1,17 +1,19 @@
-import React from 'react';
+import React, { Component } from "react";
 
-export default function EditAddress (props) {
-  return (
-    <div>
-      {props.address ?
-        <p className="ballot__date_location">
-          {props.address}
-          <span className="hidden-print"> (<a onClick={props._toggleSelectAddressModal}>Edit</a>)</span>
-        </p> :
-        <p className="ballot__date_location">
-          In order to see your ballot, please enter your address.
-          <span className="hidden-print"> (<a onClick={props._toggleSelectAddressModal}>Add Your Address</a>)</span>
-        </p> }
-    </div>
-  );
+export default class EditAddress extends Component {
+  render () {
+    return (
+      <div>
+        {this.props.address ?
+          <p className="ballot__date_location">
+            {this.props.address}
+            <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Edit</a>)</span>
+          </p> :
+          <p className="ballot__date_location">
+            In order to see your ballot, please enter your address.
+            <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Add Your Address</a>)</span>
+          </p> }
+      </div>
+    );
+  }
 }
