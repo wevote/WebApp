0c8d644244b23b0453f3582ba0aa9a04544b9f7b
edward-ly <edvard.ly@gmail.com>
2017-07-24 16:48:33 -0700

    Remove unnecessary <div>

--- src/js/components/Widgets/EditAddress.jsx
+++ src/js/components/Widgets/EditAddress.jsx
@@ -2,18 +2,20 @@ import React, { Component } from "react";
 
 export default class EditAddress extends Component {
   render () {
-    return (
-      <div>
-        {this.props.address ?
-          <p className="ballot__date_location">
-            {this.props.address}
-            <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Edit</a>)</span>
-          </p> :
-          <p className="ballot__date_location">
-            In order to see your ballot, please enter your address.
-            <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Add Your Address</a>)</span>
-          </p> }
-      </div>
-    );
+    if (this.props.address) {
+      return (
+        <p className="ballot__date_location">
+          {this.props.address}
+          <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Edit</a>)</span>
+        </p>
+      );
+    } else {
+      return (
+        <p className="ballot__date_location">
+          In order to see your ballot, please enter your address.
+          <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Add Your Address</a>)</span>
+        </p>
+      );
+    }
   }
 }
