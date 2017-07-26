ef22c905d310e310c92a196224f94fd38f0fcaee
edward-ly <edvard.ly@gmail.com>
2017-07-26 14:19:11 -0700

    Add EditAddress overlay

---
+++ src/js/components/Widgets/EditAddressPopover.jsx
@@ -0,0 +1,44 @@
+import React, { Component, PropTypes } from "react";
+import { OverlayTrigger, Popover } from "react-bootstrap";
+
+export default class EditAddressPopover extends Component {
+  static propTypes = {
+    placement: PropTypes.string.isRequired,
+    popover_off: PropTypes.bool.isRequired,
+    onClick: PropTypes.func.isRequired,
+  };
+
+  constructor (props, context) {
+    super(props, context);
+    this.closePopover = this.closePopover.bind(this);
+  }
+
+  closePopover () {
+    this.refs.overlay.hide();
+  }
+
+  render () {
+    const AddressPopover = <Popover id="popover-trigger-click-root-close"
+      onClick={this.closePopover}>
+      <div style={{textAlign: "center"}}>
+        <p><span style={{color: "#ef1e26"}}>This is our best guess</span> <i className="fa fa-times pull-right u-cursor--pointer" aria-hidden="true" /></p>
+        <p>Want to make sure these are your ballot items? Enter the full address where you are registered to vote.</p>
+        <button className="btn btn-success" onClick={this.props.onClick}>Enter Address &gt;&gt;</button>
+      </div>
+    </Popover>;
+
+    return (
+      <span>{ this.props.popover_off ? <span className="position-rating__source">&nbsp;&nbsp;<i className="fa fa-exclamation-circle" aria-hidden="true" style={{color: "#fc0d1b"}}></i>&nbsp;&nbsp;</span> :
+        <OverlayTrigger
+          trigger="click"
+          ref="overlay"
+          onExit={this.closePopover}
+          rootClose
+          placement={this.props.placement}
+          overlay={AddressPopover}>
+          <span className="position-rating__source with-popover">&nbsp;&nbsp;<i className="fa fa-exclamation-circle" aria-hidden="true" style={{color: "#fc0d1b"}}></i>&nbsp;&nbsp;</span>
+        </OverlayTrigger> }
+      </span>
+    );
+  }
+}
