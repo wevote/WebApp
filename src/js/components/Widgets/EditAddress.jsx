6e9eb4467090c6861c73ce260d41401cc89f3068
edward-ly <edvard.ly@gmail.com>
2017-07-26 14:19:42 -0700

    Add EditAddress overlay and behavior

--- src/js/components/Widgets/EditAddress.jsx
+++ src/js/components/Widgets/EditAddress.jsx
@@ -1,8 +1,10 @@
 import React, { Component, PropTypes } from "react";
+import EditAddressPopover from "./EditAddressPopover";
 
 export default class EditAddress extends Component {
   static propTypes = {
     address: PropTypes.string.isRequired,
+    isNormalized: PropTypes.bool.isRequired,
     _toggleSelectAddressModal: PropTypes.func.isRequired,
   };
 
@@ -15,15 +17,16 @@ export default class EditAddress extends Component {
     if (this.props.address) {
       return (
         <p className="ballot__date_location">
-          {this.props.address}
-          <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Edit</a>)</span>
+          { this.props.address }
+          { this.props.isNormalized ? <span>&nbsp;</span> : <EditAddressPopover popover_off={false} placement={"bottom"} onClick={this.props._toggleSelectAddressModal} /> }
+          <span className="hidden-print">(<a onClick={this.props._toggleSelectAddressModal}>Edit</a>)</span>
         </p>
       );
     } else {
       return (
         <p className="ballot__date_location">
           In order to see your ballot, please enter your address.
-          <span className="hidden-print"> (<a onClick={this.props._toggleSelectAddressModal}>Add Your Address</a>)</span>
+          <span className="hidden-print">&nbsp;(<a onClick={this.props._toggleSelectAddressModal}>Add Your Address</a>)</span>
         </p>
       );
     }
