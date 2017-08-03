ee0d81fbc6da8135b700972ca379b6b89f8984d0
edward-ly <edvard.ly@gmail.com>
2017-08-03 12:42:35 -0700

    Customize ballot filter tab styling

--- src/js/components/Navigation/BallotFilter.jsx
+++ src/js/components/Navigation/BallotFilter.jsx
@@ -13,21 +13,21 @@ export default class BallotFilter extends Component {
   render () {
     const {ballot_type, length, length_remaining} = this.props;
 
-    return <ul className="nav nav-tabs">
+    return <ul className="nav ballot__tabs">
       <li>
-        <Link to="/ballot" className={ballot_type === "ALL_BALLOT_ITEMS" ? "active btn btn-default" : "btn btn-default"}>
+        <Link to="/ballot" className={ballot_type === "ALL_BALLOT_ITEMS" ? "tab tab-active" : "tab tab-default"}>
           <span>All Items ({length})</span>
         </Link>
       </li>
 
       <li>
-        <Link to={{ pathname: "/ballot", query: { type: "filterRemaining" } }} className={ballot_type === "CHOICES_REMAINING" ? "active btn btn-default" : "btn btn-default"}>
+        <Link to={{ pathname: "/ballot", query: { type: "filterRemaining" } }} className={ballot_type === "CHOICES_REMAINING" ? "tab tab-active" : "tab tab-default"}>
           <span>Remaining Decisions ({length_remaining})</span>
         </Link>
       </li>
 
       <li>
-        <Link to={{ pathname: "/ballot", query: { type: "filterReadyToVote" } }} className={ ballot_type === "READY_TO_VOTE" ? "active btn btn-default" : "btn btn-default"}>
+        <Link to={{ pathname: "/ballot", query: { type: "filterReadyToVote" } }} className={ ballot_type === "READY_TO_VOTE" ? "tab tab-active" : "tab tab-default"}>
           <span>Vote!</span>
         </Link>
       </li>
