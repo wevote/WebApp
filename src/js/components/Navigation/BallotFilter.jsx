326a0df0f00c9a8f4a866da10e520582ff36b745
edward-ly <edvard.ly@gmail.com>
2017-08-09 12:26:55 -0700

    Refactor tab eslint styling

--- src/js/components/Navigation/BallotFilter.jsx
+++ src/js/components/Navigation/BallotFilter.jsx
@@ -14,19 +14,19 @@ export default class BallotFilter extends Component {
     const {ballot_type, length, length_remaining} = this.props;
 
     return <ul className="nav ballot__tabs">
-      <li>
+      <li className="tab-item">
         <Link to="/ballot" className={ballot_type === "ALL_BALLOT_ITEMS" ? "tab tab-active" : "tab tab-default"}>
           <span>All Items ({length})</span>
         </Link>
       </li>
 
-      <li>
+      <li className="tab-item">
         <Link to={{ pathname: "/ballot", query: { type: "filterRemaining" } }} className={ballot_type === "CHOICES_REMAINING" ? "tab tab-active" : "tab tab-default"}>
           <span>Remaining Decisions ({length_remaining})</span>
         </Link>
       </li>
 
-      <li>
+      <li className="tab-item">
         <Link to={{ pathname: "/ballot", query: { type: "filterReadyToVote" } }} className={ ballot_type === "READY_TO_VOTE" ? "tab tab-active" : "tab tab-default"}>
           <span>Vote!</span>
         </Link>
