c28526580282946ac2e035dcf92680c1eda5a0c2
edward-ly <edvard.ly@gmail.com>
2017-08-08 20:13:49 -0700

    Add tab overflow scrolling (temporary values)

--- src/js/routes/Ballot/Ballot.jsx
+++ src/js/routes/Ballot/Ballot.jsx
@@ -365,10 +365,12 @@ export default class Ballot extends Component {
               <EditAddress address={voter_address_object} _toggleSelectAddressModal={this._toggleSelectAddressModal} />
             </div>
             { text_for_map_search ?
-              <div className="ballot__filter hidden-print">
-                <BallotFilter ballot_type={this.getBallotType()}
-                              length={BallotStore.ballotLength}
-                              length_remaining={BallotStore.ballot_remaining_choices_length} />
+              <div className="ballot__filter-container">
+                <div className="ballot__filter hidden-print">
+                  <BallotFilter ballot_type={this.getBallotType()}
+                                length={BallotStore.ballotLength}
+                                length_remaining={BallotStore.ballot_remaining_choices_length} />
+                </div>
               </div> :
               null }
             <div className="visible-xs-block hidden-print">
