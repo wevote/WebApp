5f87570347c0445ccd47b6642599f06c9897e104
edward-ly <edvard.ly@gmail.com>
2017-08-31 15:35:21 -0700

    Update issue image naming and classes

--- src/js/components/Ballot/IssueFollowToggleSquare.jsx
+++ src/js/components/Ballot/IssueFollowToggleSquare.jsx
@@ -6,22 +6,26 @@ export default class IssueFollowToggleSquare extends IssueFollowToggle {
   render () {
     if (!this.state) { return <div />; }
 
-    let issue_name_base = this.props.issue_name.toLowerCase().replace(/ /g, "-");
+    let issue_name_base = this.props.issue_name.toLowerCase().replace(/[^a-z0-9_\']/g, "-").replace(/-+/g, "-");
 
-    return (
-      <div className="col-4 col-sm-3 intro-modal__issue-square" onClick={ this.state.is_following ? this.onIssueStopFollowing : this.onIssueFollow }>
+    return this.state.is_following ?
+      <div className="col-4 col-sm-3 intro-modal__issue-square" onClick={this.onIssueStopFollowing}>
+        <ImageHandler sizeClassName="intro-modal__issue-square-image u-cursor--pointer intro-modal__issue-square-following"
+                      imageUrl={ "/img/global/issues/" + issue_name_base + "-110x110.jpg" }
+                      alt={this.props.issue_name}
+                      kind_of_image="ISSUE-PHOTO" />
+        <ImageHandler className="intro-modal__issue-square-check-mark u-cursor--pointer"
+                      imageUrl="/img/global/svg-icons/check-mark-v2-40x43.svg"
+                      alt="Following" />
+        <h4 className="intro-modal__white-space intro-modal__issue-square-name">{this.props.issue_name}</h4>
+      </div> :
+      <div className="col-4 col-sm-3 intro-modal__issue-square" onClick={this.onIssueFollow}>
         <ImageHandler sizeClassName="intro-modal__issue-square-image u-cursor--pointer"
                       imageUrl={ "/img/global/issues/" + issue_name_base + "-110x110.jpg" }
                       alt={this.props.issue_name}
                       kind_of_image="ISSUE-PHOTO" />
-        { this.state.is_following ?
-          <ImageHandler className="intro-modal__issue-square-following u-cursor--pointer"
-                        imageUrl="/img/global/svg-icons/check-mark-v2-40x43.svg"
-                        alt="Following" /> :
-          null
-        }
         <h4 className="intro-modal__white-space intro-modal__issue-square-name">{this.props.issue_name}</h4>
       </div>
-    );
+    ;
   }
 }
