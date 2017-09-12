19886638ce1a73124f21caeff9c70ad62cad7272
Bharath Reddy <bharathmh@gmail.com>
2017-09-12 16:14:08 -0700

    Updates to bookmark page to show counts

--- src/js/components/Bookmarks/Bookmarks.jsx
+++ src/js/components/Bookmarks/Bookmarks.jsx
@@ -21,11 +21,11 @@ export default class Bookmarks extends Component {
     this._onBallotStoreChange();
   }
 
-  componentWillUnmount (){
+  componentWillUnmount () {
     this.ballotStoreListener.remove();
   }
 
-  _onBallotStoreChange (){
+  _onBallotStoreChange () {
     this.setState({bookmarks: BallotStore.bookmarks });
   }
 
@@ -39,25 +39,41 @@ export default class Bookmarks extends Component {
     const electionTooltip = election_date ? <Tooltip id="tooltip">Ballot for {election_date}</Tooltip> : <span />;
 
     return <div className="ballot">
-      <div className="ballot__heading u-stack--lg">
-        <OverlayTrigger placement="top" overlay={electionTooltip} >
-          <h1 className="h1 ballot__election-name">{election_name}</h1>
-        </OverlayTrigger>
-        <p className="ballot__date_location">
-          {text_for_map_search}
-          <span> (<Link to="/settings/location">Edit</Link>)</span>
-        </p>
-        <div className="ballot__filter"><BallotFilter ballot_type="BOOKMARKS" /></div>
-      </div>
-      {
-        this.state.bookmarks.length === 0 && <p>No bookmarks yet</p>
-      }
-      <div className="bookmarks-list">
+      <div className="ballot__heading">
+        <div className="page-content-container">
+          <div className="container-fluid">
+            <div className="row">
+            <div className="col-md-12">
+              <OverlayTrigger placement="top" overlay={electionTooltip} >
+                <header className="ballot__header-group">
+                <h1 className="h1 ballot__election-name">{election_name}</h1>
+                </header>
+              </OverlayTrigger>
+              <p className="ballot__date_location">
+                {text_for_map_search}
+                <span> (<Link to="/settings/location">Edit</Link>)</span>
+              </p>
+            </div>
+            </div>
+          <div className="ballot__filter-container">
+            <div className="ballot__filter hidden-print">
+              <BallotFilter ballot_type="BOOKMARKS"
+                length={BallotStore.ballotLength}
+                length_remaining={BallotStore.ballot_remaining_choices_length} />
+            </div>
+          </div>
+        </div>
         {
-          this.state.bookmarks.map(bookmark => {
-            return <BookmarkItem key={bookmark.ballot_item_display_name} bookmark={bookmark}/>;
-          })
+          this.state.bookmarks.length === 0 && <p>No bookmarks yet</p>
         }
+        <div className="bookmarks-list">
+          {
+            this.state.bookmarks.map(bookmark => {
+              return <BookmarkItem key={bookmark.ballot_item_display_name} bookmark={bookmark}/>;
+            })
+          }
+        </div>
+      </div>
       </div>
     </div>;
   }
