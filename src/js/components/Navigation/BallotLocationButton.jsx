d5dc598e544541de1a96c4ae1ea0f3ebb8f16572
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2017-11-10 14:31:40 -0800

    Upgraded to use new BallotLocationChoices button code. Fixed problem with switching elections.

--- src/js/components/Navigation/BallotLocationButton.jsx
+++ src/js/components/Navigation/BallotLocationButton.jsx
@@ -1,15 +1,20 @@
 import React, { PropTypes, Component } from "react";
 import { Button } from "react-bootstrap";
 import BallotStore from "../../stores/BallotStore";
+import { shortenText } from "../../utils/textFormat";
 
 export default class BallotLocationButton extends Component {
-  constructor(props){
+  static propTypes = {
+    ballot_location: PropTypes.object,
+    goToDifferentBallot: PropTypes.func.isRequired
+  };
+
+  constructor (props) {
     super(props);
   }
 
-  render(){
+  render () {
     let ballot_location = this.props.ballot_location;
-    let key = this.props.key;
 
     if (ballot_location.ballot_item_display_name !== "" || ballot_location.text_for_map_search !== "") {
       let ballot_location_shortcut_of_retrieved_ballot = "";
@@ -37,18 +42,19 @@ export default class BallotLocationButton extends Component {
       if (ballot_location.ballot_location_display_name && ballot_location.ballot_location_display_name !== "") {
         ballot_location_display_name = ballot_location.ballot_location_display_name;
       } else if (ballot_location.text_for_map_search !== "") {
-        ballot_location_display_name = ballot_location.text_for_map_search;
+        const maximum_address_display_length = 25;
+        ballot_location_display_name = shortenText(ballot_location.text_for_map_search, maximum_address_display_length);
       } else {
         ballot_location_display_name = "My Address";
       }
-      return <span key={key} className="u-push--md">
+      return <span className="u-push--md">
         <Button bsStyle={ballot_location_shortcut_matches || ballot_returned_we_vote_id_matches ? "info" : "default"}
-                onClick={() => { this.handleClick(ballot_location.ballot_returned_we_vote_id, ballot_location.ballot_location_shortcut); }} >
+                onClick={() => { this.props.goToDifferentBallot(ballot_location.ballot_returned_we_vote_id, ballot_location.ballot_location_shortcut); }} >
           <span>{ballot_location_display_name}</span>
         </Button>
       </span>;
     } else {
-      return <span key={key} />;
+      return <span />;
     }
   }
 }
