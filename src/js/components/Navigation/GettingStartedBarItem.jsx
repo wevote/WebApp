6432539e72b1bf18f696da0fea2cc2d2e799dd4c
Anisha Jain <jainanisha90@gmail.com>
2017-11-22 11:24:01 -0800

    #1181 Added print icon to secondary navigation

--- src/js/components/Navigation/GettingStartedBarItem.jsx
+++ src/js/components/Navigation/GettingStartedBarItem.jsx
@@ -1,26 +1,29 @@
 import React, {PropTypes} from "react";
 import { Link } from "react-router";
 
-const GettingStartedBarItem = props =>
-  <Link onClick={props.show} className={ "header-getting-started-nav__item header-getting-started-nav__item--has-icon"}>
-    <span className="header-getting-started-nav__item-image-wrapper" title={props.title}>
-      { props.completed ?
-        <img className="glyphicon nav-getting-started__image-checked"
-          src="/img/global/svg-icons/check-mark-v2-21x21.svg" /> :
-        null
-      }
-      <img className={`glyphicon nav-getting-started__image${props.completed ? "-fade" : ""}`} src={props.source} />
-    </span>
-    <span className={`header-getting-started-nav__label${props.completed ? "-fade" : ""}`}>
-      {props.title}
+const GettingStartedBarItem = props => <Link onClick={props.show} className={ (props.printIcon ? "hidden-xs " : "") + "header-getting-started-nav__item header-getting-started-nav__item--has-icon"}>
+      <span className="header-getting-started-nav__item-image-wrapper" title={props.title}>
+        { props.completed ?
+          <img className="glyphicon nav-getting-started__image-checked"
+            src="/img/global/svg-icons/check-mark-v2-21x21.svg" /> :
+          null
+        }
+        { props.printIcon ? <span className="glyphicon glyphicon-print fa-2x"/> : null }
+        { props.emailIcon ? <span className="glyphicon glyphicon-envelope fa-2x"/> : null }
+        { !props.printIcon && !props.emailIcon ? <img className={`glyphicon nav-getting-started__image${props.completed ? "-fade" : ""}`} src={props.source} /> : null }
       </span>
-  </Link>;
+      <span className={`header-getting-started-nav__label${props.completed ? "-fade" : ""}`}>
+        {props.title}
+        </span>
+    </Link>;
 
 GettingStartedBarItem.propTypes = {
   show: PropTypes.func.isRequired,
-  completed: PropTypes.number.isRequired,
-  source: PropTypes.string.isRequired,
-  title: PropTypes.string.isRequired
+  completed: PropTypes.number,
+  source: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  printIcon: PropTypes.bool,
+  emailIcon: PropTypes.bool,
 };
 
 export default GettingStartedBarItem;
