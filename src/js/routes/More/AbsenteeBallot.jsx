f366ac149cbf196cb2db74b2c9e50099980d2bf3
Edward Ly <edvard.ly@gmail.com>
2018-02-09 21:17:25 -0800

    Fixed ids

--- src/js/routes/More/AbsenteeBallot.jsx
+++ src/js/routes/More/AbsenteeBallot.jsx
@@ -2,5 +2,5 @@ import React from "react";
 import ReactIframeResizer from "react-iframe-resizer-super";
 
 export default function AbsenteeBallot () {
-  return <ReactIframeResizer iframeResizerOptions={{ checkOrigin: false }} id="register-to-vote-iframe" src="https://absentee.vote.org/?partner=111111&campaign=free-tools" />;
+  return <ReactIframeResizer iframeResizerOptions={{ checkOrigin: false }} id="absentee-ballot-iframe" src="https://absentee.vote.org/?partner=111111&campaign=free-tools" />;
 }
