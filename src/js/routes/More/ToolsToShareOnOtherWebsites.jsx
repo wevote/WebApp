bb105c0a1a0daf7fd62c572e84d2c483c21b60b8
Edward Ly <edvard.ly@gmail.com>
2018-02-21 13:13:22 -0800

    Cleaned up React syntax

--- src/js/routes/More/ToolsToShareOnOtherWebsites.jsx
+++ src/js/routes/More/ToolsToShareOnOtherWebsites.jsx
@@ -2,84 +2,74 @@ import React from "react";
 import Helmet from "react-helmet";
 import CodeCopier from "../../components/Widgets/CodeCopier";
 
-export default class ToolsToShareOnOtherWebsites extends React.Component {
-  constructor (props) {
-    super(props);
-  }
+export default function ToolsToShareOnOtherWebsites () {
+  return <div>
+    <Helmet title="Free civic engagement tools - We Vote" />
+    <div className="container-fluid well">
+      <h1 className="h1">Free Civic Engagement Tools</h1>
 
-  static getProps () {
-    return {};
-  }
+      <p>WeVote.US wants to give you our technology to use — for <strong>FREE</strong><br />on your website.</p>
 
-  render () {
-    return <div>
-      <Helmet title="Free civic engagement tools - We Vote" />
-      <div className="container-fluid well">
-        <h1 className="h1">Free Civic Engagement Tools</h1>
+      <h2 className="h3">Why use WeVote.US tools?</h2>
+      <ul>
+        <li>All our of tools work in all 50 states and the District of Columbia.</li>
+        <li>They’re fast and mobile-optimized, they’re free to use, and they’re the best tools on the market.</li>
+        <li>Battle tested: these are the exact same tools we use at WeVote.US.</li>
+      </ul>
 
-        <p>WeVote.US wants to give you our technology to use — for <strong>FREE</strong><br />on your website.</p>
+      <h2 className="h3">Adding the tools to your website takes less than 2 minutes.</h2>
+      <ol>
+        <li>
+          Copy the code for the tool, or click to view the code.<br />
+          {/* Image here */}
+      </li>
+        <li>Paste the code on your website where you want the tool to appear.</li>
+        <li>We recommend putting each tool on its own page so you don’t overwhelm your visitors.</li>
+      </ol>
 
-        <h2 className="h3">Why use WeVote.US tools?</h2>
-        <ul>
-          <li>All our of tools work in all 50 states and the District of Columbia.</li>
-          <li>They’re fast and mobile-optimized, they’re free to use, and they’re the best tools on the market.</li>
-          <li>Battle tested: these are the exact same tools we use at WeVote.US.</li>
-        </ul>
+      {/* eslint quotes: [1, "double", { "allowTemplateLiterals": true }] */}
 
-        <h2 className="h3">Adding the tools to your website takes less than 2 minutes.</h2>
-        <ol>
-          <li>
-            Copy the code for the tool, or click to view the code.<br />
-            {/* Image here */}
-        </li>
-          <li>Paste the code on your website where you want the tool to appear.</li>
-          <li>We recommend putting each tool on its own page so you don’t overwhelm your visitors.</li>
-        </ol>
+      <h4 className="h2">Free voter registration tool from Vote.org</h4>
+      {/* enclose html within {` and `} to make them literal string in JSX */}
+      <CodeCopier>
+        {`<iframe src="https://register.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe>`}
+        {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>`}
+        {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
+      </CodeCopier>
+      <br />
 
-        {/* eslint quotes: [1, "double", { "allowTemplateLiterals": true }] */}
+      <h4 className="h2">Free absentee ballot tool from Vote.org</h4>
+      {/* enclose html within {` and `} to make them literal string in JSX */}
+      <CodeCopier>
+        {`<iframe src="https://absentee.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame2" scrollable="no"></iframe>`}
+        {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>`}
+        {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
+      </CodeCopier>
+      <br />
 
-        <h4 className="h2">Free voter registration tool from Vote.org</h4>
-        {/* enclose html within {` and `} to make them literal string in JSX */}
-        <CodeCopier>
-          {`<iframe src="https://register.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe>`}
-          {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>`}
-          {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
-        </CodeCopier>
-        <br />
+      <h4 className="h2">Free check registration status tool from Vote.org</h4>
+      {/* enclose html within {` and `} to make them literal string in JSX */}
+      <CodeCopier>
+        {`<iframe src="https://verify.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame3" scrollable="no"></iframe>`}
+        {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js" ></script>`}
+        {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
+      </CodeCopier>
+      <br />
 
-        <h4 className="h2">Free absentee ballot tool from Vote.org</h4>
-        {/* enclose html within {` and `} to make them literal string in JSX */}
-        <CodeCopier>
-          {`<iframe src="https://absentee.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame2" scrollable="no"></iframe>`}
-          {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>`}
-          {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
-        </CodeCopier>
-        <br />
-
-        <h4 className="h2">Free check registration status tool from Vote.org</h4>
-        {/* enclose html within {` and `} to make them literal string in JSX */}
-        <CodeCopier>
-          {`<iframe src="https://verify.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame3" scrollable="no"></iframe>`}
-          {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js" ></script>`}
-          {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
-        </CodeCopier>
-        <br />
-
-        <h4 className="h2">Election reminders tool from Vote.org</h4>
-        {/* enclose html within {` and `} to make them literal string in JSX */}
-        <CodeCopier>
-          {`<iframe src="https://reminders.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe>`}
-          {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>`}
-          {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
-        </CodeCopier>
-        <p>&nbsp;</p>
-        <h2 className="h3">Access even more:</h2>
-        <ul>
-          <li>You can place any page on www.WeVote.US on your organizational website. Learn more about fine-tuning our tools for your website.</li>
-          <li>If you need access to the data gathered via your instance of the Vote.org toolset, <a href="https://vip.vote.org" target="_blank">check out Vote.org premium tools.</a></li>
-        </ul>
-        <p>&nbsp;</p>
-      </div>
-    </div>;
-  }
+      <h4 className="h2">Election reminders tool from Vote.org</h4>
+      {/* enclose html within {` and `} to make them literal string in JSX */}
+      <CodeCopier>
+        {`<iframe src="https://reminders.vote.org/?partner=111111&campaign=free-tools" width="100%" marginheight="0" frameborder="0" id="frame1" scrollable ="no"></iframe>`}
+        {`<script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/iframe-resizer/3.5.3/iframeResizer.min.js"></script>`}
+        {`<script type="text/javascript">iFrameResize({ log:true, checkOrigin:false});</script>`}
+      </CodeCopier>
+      <p>&nbsp;</p>
+      <h2 className="h3">Access even more:</h2>
+      <ul>
+        <li>You can place any page on www.WeVote.US on your organizational website. Learn more about fine-tuning our tools for your website.</li>
+        <li>If you need access to the data gathered via your instance of the Vote.org toolset, <a href="https://vip.vote.org" target="_blank">check out Vote.org premium tools.</a></li>
+      </ul>
+      <p>&nbsp;</p>
+    </div>
+  </div>;
 }
