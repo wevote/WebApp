10d3c7ef0679a8f2573c4ee5241c7ea4af1afbb7
Dale John McGrew <dale.mcgrew@wevoteusa.org>
2018-02-21 18:51:59 -0800

    Renamed "retrieveIssuesForVoter" to "issuesRetrieve". Reduced voterGuidesToFollowRetrieve to only retrieve 150 for speed. Added more size options when displaying IssueCard. Added first version of Issue images under each candidate. Collapsed vision page into organization page.

--- src/js/components/Intro/IntroNetworkSafety.jsx
+++ src/js/components/Intro/IntroNetworkSafety.jsx
@@ -26,11 +26,12 @@ export default class IntroNetworkSafety extends Component {
                 style={isCordova() ? { backgroundImage: "url(./img/global/intro-story/slide2-lady-liberty-698x600.jpg)" } : null} >
         <div className="intro-story__h1">We Vote in Safety</div>
         <div className="intro-story__h2">You control who is in<br/>
-          your <strong>We Vote</strong> network.
+          your <strong>We Vote</strong> network.<br/>
+          No fist fights required.
         </div>
         <div><br/></div>
         <div><img className="center-block intro-story__img-height"
-                  src={cordovaDot("/img/global/intro-story/slide2-ignore-troll-282x282-min.png")}/></div>
+                  src={cordovaDot("/img/global/intro-story/no-fighting-256x256.png")}/></div>
         <div className="intro-story__padding-btn">
           <button type="button" className="btn btn-success" onClick={this.props.next}>Next&nbsp;&nbsp;&gt;</button>
         </div>
