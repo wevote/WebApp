d271cfd11a3bd0722b42b040fabdd49d63b9b21b
Edward Ly <edvard.ly@gmail.com>
2018-02-27 16:50:37 -0800

    Added link to ballot tool image

--- src/js/components/Widgets/CodeCopier.jsx
+++ src/js/components/Widgets/CodeCopier.jsx
@@ -137,10 +137,12 @@ export default class CodeCopier extends Component {
                           value={source_code}
                           readOnly /> :
                 <div>
-                  <ImageHandler className="code-copier__image u-stack--sm"
-                                hidePlaceholder
-                                imageUrl={this.props.imageUrl}
-                                alt={this.props.title} />
+                  <a href={this.props.imageUrl} target="_blank">
+                    <ImageHandler className="code-copier__image u-stack--sm"
+                                  hidePlaceholder
+                                  imageUrl={this.props.imageUrl}
+                                  alt={this.props.title} />
+                  </a>
                   <br />
                   <a className="code-copier__link" href={this.props.imageUrl} target="_blank">
                     Click to view full size
