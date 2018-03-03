6de7800088c4b4ae21af5aa15b6945680464b20b
Edward Ly <edvard.ly@gmail.com>
2018-03-02 20:28:03 -0800

    Implemented hiding scroll icons if list is not scrollable at all

--- src/js/components/Issues/IssuesFollowedByBallotItemDisplayList.jsx
+++ src/js/components/Issues/IssuesFollowedByBallotItemDisplayList.jsx
@@ -21,6 +21,8 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
     this.state = {
       transitioning: false,
       showModal: false,
+      can_scroll_desktop: false,
+      can_scroll_mobile: false,
       can_scroll_left_desktop: false,
       can_scroll_left_mobile: false,
       can_scroll_right_desktop: true,
@@ -38,6 +40,7 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
     this.issueStoreListener = IssueStore.addListener(this.onIssueStoreChange.bind(this));
     this.voterGuideStoreListener = VoterGuideStore.addListener(this.onVoterGuideStoreChange.bind(this));
     this.onVoterGuideStoreChange();
+    this.setScrollState();
     this.setState({
       ballotItemWeVoteId: this.props.ballotItemWeVoteId,
       ballot_item_display_name: this.props.ballot_item_display_name ? this.props.ballot_item_display_name : "this candidate",
@@ -49,6 +52,7 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
   }
 
   componentWillReceiveProps (nextProps) {
+    this.setScrollState();
     this.setState({
       ballotItemWeVoteId: nextProps.ballotItemWeVoteId,
       ballot_item_display_name: nextProps.ballot_item_display_name ? nextProps.ballot_item_display_name : "this candidate",
@@ -69,6 +73,7 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
   }
 
   onIssueStoreChange () {
+    this.setScrollState();
     this.setState({
       issues_under_this_ballot_item: IssueStore.getIssuesUnderThisBallotItem(this.state.ballotItemWeVoteId),
       issues_under_this_ballot_item_voter_is_following: IssueStore.getIssuesUnderThisBallotItemVoterIsFollowing(this.state.ballotItemWeVoteId),
@@ -127,6 +132,19 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
     });
   }
 
+  setScrollState () {
+    const desktop_list = findDOMNode(this.refs[`${this.props.ballotItemWeVoteId}-issue-list-desktop`]);
+    const mobile_list = findDOMNode(this.refs[`${this.props.ballotItemWeVoteId}-issue-list-mobile`]);
+    let desktop_list_visible_width = $(desktop_list).width();
+    let desktop_list_width = $(desktop_list).children().eq(0).children().eq(0).width();
+    let mobile_list_visible_width = $(mobile_list).width();
+    let mobile_list_width = $(mobile_list).children().eq(0).children().eq(0).width();
+    this.setState({
+      can_scroll_desktop: desktop_list_visible_width <= desktop_list_width,
+      can_scroll_mobile: mobile_list_visible_width <= mobile_list_width,
+    });
+  }
+
   render () {
     let issues_under_this_ballot_item_voter_is_following_found = this.state.issues_under_this_ballot_item_voter_is_following && this.state.issues_under_this_ballot_item_voter_is_following.length;
     let issues_under_this_ballot_item_voter_is_not_following = this.state.issues_under_this_ballot_item_voter_not_following && this.state.issues_under_this_ballot_item_voter_not_following.length;
@@ -136,7 +154,7 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
       return null;
     }
 
-    let issues_count = issues_under_this_ballot_item_voter_is_following_found + issues_under_this_ballot_item_voter_is_not_following;
+    // let issues_count = issues_under_this_ballot_item_voter_is_following_found + issues_under_this_ballot_item_voter_is_not_following;
     // console.log(issues_count);
 
     const issuesLabelPopover =
@@ -159,12 +177,12 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
     return (
       <div className="issues-list-stacked__support-list u-flex u-justify-between u-items-center">
         {/* Click to scroll left through list Desktop */}
-        { issues_count > 7 && this.state.can_scroll_left_desktop ?
+        { this.state.can_scroll_desktop && this.state.can_scroll_left_desktop ?
           <i className="fa fa-2x fa-chevron-left issues-list-stacked__support-list__scroll-icon u-cursor--pointer hidden-xs" aria-hidden="true" onClick={this.scrollLeft.bind(this, "desktop")} /> :
           <i className="fa fa-2x fa-chevron-left network-positions-stacked__support-list__scroll-icon-disabled hidden-xs" aria-hidden="true" />
         }
         {/* Click to scroll left through list Mobile */}
-        { issues_count > 4 && this.state.can_scroll_left_mobile ?
+        { this.state.can_scroll_mobile && this.state.can_scroll_left_mobile ?
           <i className="fa fa-2x fa-chevron-left issues-list-stacked__support-list__scroll-icon u-cursor--pointer visible-xs" aria-hidden="true" onClick={this.scrollLeft.bind(this, "mobile")} /> :
           <i className="fa fa-2x fa-chevron-left network-positions-stacked__support-list__scroll-icon-disabled visible-xs" aria-hidden="true" />
         }
@@ -212,12 +230,12 @@ export default class IssuesFollowedByBallotItemDisplayList extends Component {
           </span>
         </div>
         {/* Click to scroll right through list Desktop */}
-        { issues_count > 7 && this.state.can_scroll_right_desktop ?
+        { this.state.can_scroll_desktop && this.state.can_scroll_right_desktop ?
           <i className="fa fa-2x fa-chevron-right issues-list-stacked__support-list__scroll-icon u-cursor--pointer hidden-xs" aria-hidden="true" onClick={this.scrollRight.bind(this, "desktop")} /> :
           <i className="fa fa-2x fa-chevron-right network-positions-stacked__support-list__scroll-icon-disabled hidden-xs" aria-hidden="true" />
         }
         {/* Click to scroll right through list Mobile */}
-        { issues_count > 4 && this.state.can_scroll_right_mobile ?
+        { this.state.can_scroll_mobile && this.state.can_scroll_right_mobile ?
           <i className="fa fa-2x fa-chevron-right issues-list-stacked__support-list__scroll-icon u-cursor--pointer visible-xs" aria-hidden="true" onClick={this.scrollRight.bind(this, "mobile")} /> :
           <i className="fa fa-2x fa-chevron-right network-positions-stacked__support-list__scroll-icon-disabled visible-xs" aria-hidden="true" />
         }
