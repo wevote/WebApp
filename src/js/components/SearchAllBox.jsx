53eca225151d9132d1e2d9ddf31e130a10d672e0
stevepodell <steve@podell.com>
2018-03-28 12:31:50 -0700

    Probable fix to SearchBox suggestions covering the SearchBox in Cordova
    wevote/WeVoteCordova/issues/36

--- src/js/components/SearchAllBox.jsx
+++ src/js/components/SearchAllBox.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 import BallotActions from "../actions/BallotActions";
 import classNames from "classnames";
 import { Link } from "react-router";
-import { historyPush, isCordova, isWebApp } from "../utils/cordovaUtils";
+import { historyPush, isCordova, isWebApp, screenHeight } from "../utils/cordovaUtils";
 import ImageHandler from "../components/ImageHandler";
 import { renderLog } from "../utils/logging";
 import SearchAllActions from "../actions/SearchAllActions";
@@ -83,6 +83,18 @@ export default class SearchAllBox extends Component {
 
     if (SearchAllStore.getSearchResults()) {
       newState.searchResults = SearchAllStore.getSearchResults();
+      if (isCordova()) {
+        if (window.screen.height < 500) {   // iPhone 4, show two results
+          newState.searchResults = newState.searchResults.slice(0, 2);
+        } else if (window.screen.height < 600) {   // iPhone 5, show four results
+          newState.searchResults = newState.searchResults.slice(0, 4);
+        } else if (window.screen.height < 700) {   // iPhone 8, show five results
+          newState.searchResults = newState.searchResults.slice(0, 5);
+        } else if (window.screen.height < 800) {   // iPhone 6p, 7p, 8p show six results
+          newState.searchResults = newState.searchResults.slice(0, 6);
+        }
+      }
+
       this.links = newState.searchResults.map(r => makeSearchLink(r.twitter_handle, r.we_vote_id, r.kind_of_owner, r.link_internal, r.google_civic_election_id));
     }
 
