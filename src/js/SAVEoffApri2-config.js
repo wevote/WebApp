1bfc76e29feb53fdab660bc80ae5c86c0b5c9d0d
stevepodell <steve@podell.com>
2018-04-16 12:19:16 -0700

    Allow SignInByEmail to signin when email is read on the phone in Cordova Resolves the iOS portion of wevote/WeVoteCordova/issues/11

---
+++ src/js/SAVEoffApri2-config.js
@@ -0,0 +1,41 @@
+// Note that we import these values into "webAppConfig" (so we can search for it)
+module.exports = {
+  WE_VOTE_URL_PROTOCOL: "http://",  // "http://" for local dev or "https://" for live server
+  WE_VOTE_HOSTNAME: "10.0.2.2:3000",  // This should be without "http...". This is "WeVote.US" on live server.
+  // WE_VOTE_HOSTNAME: "localhost:3000",  // This should be without "http...". This is "WeVote.US" on live server.
+
+  WE_VOTE_SERVER_ROOT_URL: "http://10.0.2.2:8000/",
+  WE_VOTE_SERVER_ADMIN_ROOT_URL: "http://10.0.2.2:8000/admin/",
+  WE_VOTE_SERVER_API_ROOT_URL: "http://10.0.2.2:8000/apis/v1/",
+
+  // WE_VOTE_SERVER_ROOT_URL: "http://localhost:8000/",
+  // WE_VOTE_SERVER_ADMIN_ROOT_URL: "http://localhost:8000/admin/",
+  // WE_VOTE_SERVER_API_ROOT_URL: "http://localhost:8000/apis/v1/",
+
+  // WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
+  // WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
+  // WE_VOTE_SERVER_API_ROOT_URL: "https://api.wevoteusa.org/apis/v1/",
+
+  DEBUG_MODE: false,
+
+  LOG_RENDER_EVENTS: true,
+  LOG_ONLY_FIRST_RENDER_EVENTS: true,
+  LOG_HTTP_REQUESTS: true,
+  LOG_ROUTING: true,
+  LOG_SIGNIN_STEPS: true,
+
+  // Use 1 or 0 as opposed to true or false
+  test: {
+    use_test_election: 0,
+  },
+
+  location: {
+    text_for_map_search: "",
+  },
+
+  FACEBOOK_APP_ID: "1097389196952441",
+
+  STRIPE_API_KEY: "pk_test_bWuWGC3jrMIFH3wvRvHR6Z5H",
+
+  IS_CORDOVA: false,
+};
