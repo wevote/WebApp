d1ba7511fd237b855f233be189a7d6a39bb29bfd
stevepodell <steve@podell.com>
2018-04-19 20:59:44 -0700

    Changed "< Return to Ballot" into a Button Tested with Cordova, WebApp, and Mobile WebApp On the HamburgerMenu, Bookmarks menu item doesn't have a "Return" so I removed the indent that indicates it is a setting. BallotStore has a hard to reproduce bug where ballotItemListCandidatesDict is occasionally is not initialized, so I put in an error log line so we will have a hint about what went wrong.

--- src/js/components/Navigation/HeaderBackToBar.jsx
+++ src/js/components/Navigation/HeaderBackToBar.jsx
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
-import { Link } from "react-router";
 import Icon from "react-svg-icons";
 import BallotStore from "../../stores/BallotStore";
 import BookmarkStore from "../../stores/BookmarkStore";
