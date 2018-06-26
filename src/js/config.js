fe449ef143343840c5a5a0ea895567125a5b83b4
SailingSteve <steve@podell.com>
2018-06-26 13:22:50 -0700

    Cleared npm test errors Some other lint cleanup

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 // Note that we import these values where needed as "webAppConfig"
 module.exports = {
-  WE_VOTE_URL_PROTOCOL: "http://",  // "http://" for local dev or "https://" for live server
-  WE_VOTE_HOSTNAME: "localhost:3000",  // This should be without "http...". This is "WeVote.US" on live server.
+  WE_VOTE_URL_PROTOCOL: "https://",  // "http://" for local dev or "https://" for live server
+  WE_VOTE_HOSTNAME: "wevote.us",  // This should be without "http...". This is "WeVote.US" on live server.
 
   WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
   WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
@@ -13,7 +13,7 @@ module.exports = {
   LOG_ONLY_FIRST_RENDER_EVENTS: false,
   LOG_HTTP_REQUESTS: false,
   LOG_ROUTING: false,
-  LOG_SIGNIN_STEPS: false,
+  LOG_SIGNIN_STEPS: true,
 
   // Use 1 or 0 as opposed to true or false
   test: {
@@ -24,7 +24,7 @@ module.exports = {
     text_for_map_search: "",
   },
 
-  FACEBOOK_APP_ID: "",
+  FACEBOOK_APP_ID: "1097389196952441",
 
-  STRIPE_API_KEY: "",
+  STRIPE_API_KEY: "pk_live_zpl7eSkLnySR2i4vtJQ1lJmi",
 };
--- src/js/stores/CandidateStore.js
+++ src/js/stores/CandidateStore.js
@@ -5,6 +5,8 @@ import OfficeActions from "../actions/OfficeActions";
 import OfficeStore from "../stores/OfficeStore";
 import { stringContains } from "../utils/textFormat";
 
+/* jscs:disable requireCamelCaseOrUpperCaseIdentifiers */
+/* TODO: June 2018, there are just too many non-camelCase vars in this file, and they are in state which is passed as an object, suppressing so we can see other warnings */
 class CandidateStore extends ReduceStore {
 
   getInitialState () {
@@ -63,6 +65,7 @@ class CandidateStore extends ReduceStore {
     switch (action.type) {
 
       case "candidateRetrieve":
+
         // Make sure we have information for the office the candidate is running for
         if (action.res.contest_office_we_vote_id) {
           let office = OfficeStore.getOffice(action.res.contest_office_we_vote_id);
@@ -70,17 +73,19 @@ class CandidateStore extends ReduceStore {
             OfficeActions.officeRetrieve(action.res.contest_office_we_vote_id);
           }
         }
+
         candidate = action.res;
         all_cached_candidates = state.all_cached_candidates;
         all_cached_candidates[candidate.we_vote_id] = candidate;
         return {
           ...state,
-          all_cached_candidates: all_cached_candidates
+          all_cached_candidates: all_cached_candidates,
         };
 
       case "candidatesRetrieve":
-        let contest_office_we_vote_id = '';
+        let contest_office_we_vote_id = "";
         let number_of_candidates_retrieved_by_office = state.number_of_candidates_retrieved_by_office;
+
         // Make sure we have information for the office the candidate is running for
         if (action.res.contest_office_we_vote_id) {
           contest_office_we_vote_id = action.res.contest_office_we_vote_id;
@@ -89,16 +94,18 @@ class CandidateStore extends ReduceStore {
             OfficeActions.officeRetrieve(action.res.contest_office_we_vote_id);
           }
         }
+
         let incoming_candidate_count = 0;
         candidate_list = action.res.candidate_list;
         all_cached_candidates = state.all_cached_candidates;
-        candidate_list.forEach( one_candidate => {
+        candidate_list.forEach(one_candidate => {
           all_cached_candidates[one_candidate.we_vote_id] = one_candidate;
           incoming_candidate_count += 1;
         });
         if (contest_office_we_vote_id.length) {
           number_of_candidates_retrieved_by_office[contest_office_we_vote_id] = incoming_candidate_count;
         }
+
         return {
           ...state,
           all_cached_candidates: all_cached_candidates,
@@ -106,12 +113,14 @@ class CandidateStore extends ReduceStore {
         };
 
       case "positionListForBallotItem":
+
         // console.log("positionListForBallotItem action.res:", action.res);
         if (action.res.kind_of_ballot_item === "CANDIDATE") {
           candidate_we_vote_id = action.res.ballot_item_we_vote_id;
           new_position_list = action.res.position_list;
           position_list_from_advisers_followed_by_voter = state.position_list_from_advisers_followed_by_voter;
           position_list_from_advisers_followed_by_voter[candidate_we_vote_id] = new_position_list;
+
           // console.log("positionListForBallotItem position_list_from_advisers_followed_by_voter[ballot_item_we_vote_id]:", position_list_from_advisers_followed_by_voter[candidate_we_vote_id]);
           return {
             ...state,
@@ -121,18 +130,22 @@ class CandidateStore extends ReduceStore {
           office_position_list = action.res.position_list;
           let office_we_vote_id = action.res.ballot_item_we_vote_id;
           position_list_from_advisers_followed_by_voter = state.position_list_from_advisers_followed_by_voter;
+
           // Now reset the lists for all candidates under this office
           let candidate_we_vote_ids_under_this_office = BallotStore.getCandidateWeVoteIdsForOfficeWeVoteId(office_we_vote_id);
+
           // console.log("candidate_we_vote_ids_under_this_office: ", candidate_we_vote_ids_under_this_office);
           if (candidate_we_vote_ids_under_this_office) {
-            candidate_we_vote_ids_under_this_office.forEach( candidate_we_vote_id_temp => {
+            candidate_we_vote_ids_under_this_office.forEach(candidate_we_vote_id_temp => {
               position_list_from_advisers_followed_by_voter[candidate_we_vote_id_temp] = []; // Reset list
             });
           }
-          office_position_list.forEach( one_position_temp => {
+
+          office_position_list.forEach(one_position_temp => {
             candidate_we_vote_id = one_position_temp.ballot_item_we_vote_id;
             position_list_from_advisers_followed_by_voter[candidate_we_vote_id].push(one_position_temp);
           });
+
           // console.log("positionListForBallotItem OFFICE position_list_from_advisers_followed_by_voter[ballot_item_we_vote_id]:", position_list_from_advisers_followed_by_voter[candidate_we_vote_id]);
           return {
             ...state,
@@ -143,6 +156,7 @@ class CandidateStore extends ReduceStore {
         }
 
       case "voterGuidesToFollowRetrieve":
+
         // This code harvests the positions that are passed in along with voter guides,
         //  and stores them so we can request them in cases where the response package for
         //  voterGuidesToFollowRetrieve does not include the position data
@@ -159,13 +173,15 @@ class CandidateStore extends ReduceStore {
           // console.log("exiting CandidateStore voterGuidesToFollowRetrieve");
           return state;
         }
+
         all_cached_positions_about_candidates = state.all_cached_positions_about_candidates;
-        voter_guides.forEach( one_voter_guide => {
+        voter_guides.forEach(one_voter_guide => {
           // Make sure we have a position in the voter guide
           if (one_voter_guide.is_support_or_positive_rating || one_voter_guide.is_oppose_or_negative_rating || one_voter_guide.is_information_only) {
             if (!all_cached_positions_about_candidates[ballot_item_we_vote_id]) {
               all_cached_positions_about_candidates[ballot_item_we_vote_id] = {};
             }
+
             if (!all_cached_positions_about_candidates[ballot_item_we_vote_id][one_voter_guide.organization_we_vote_id]) {
               all_cached_positions_about_candidates[ballot_item_we_vote_id][one_voter_guide.organization_we_vote_id] = {};
             }
@@ -179,8 +195,10 @@ class CandidateStore extends ReduceStore {
               ballot_item_twitter_handle: one_voter_guide.ballot_item_twitter_handle,
               ballot_item_political_party: one_voter_guide.ballot_item_political_party,
               kind_of_ballot_item: "CANDIDATE",
+
               // ballot_item_id: 0,
               ballot_item_we_vote_id: ballot_item_we_vote_id,
+
               // ballot_item_state_code: "",
               // contest_office_id: 0,
               // contest_office_we_vote_id: "",
@@ -197,15 +215,18 @@ class CandidateStore extends ReduceStore {
               vote_smart_rating: one_voter_guide.vote_smart_rating,
               vote_smart_time_span: one_voter_guide.vote_smart_time_span,
               google_civic_election_id: one_voter_guide.google_civic_election_id,
+
               // state_code: "",
               more_info_url: one_voter_guide.more_info_url,
               statement_text: one_voter_guide.statement_text,
-              last_updated: one_voter_guide.last_updated
+              last_updated: one_voter_guide.last_updated,
             };
+
             // console.log("CandidateStore one_position: ", one_position);
             all_cached_positions_about_candidates[ballot_item_we_vote_id][one_voter_guide.organization_we_vote_id] = one_position;
           }
         });
+
         // console.log("CandidateStore all_cached_positions_about_candidates:", all_cached_positions_about_candidates);
         return {
           ...state,
