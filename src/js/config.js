8f1075660862d3a6504a9767f15f65348a4548d9
SailingSteve <steve@podell.com>
2018-06-26 13:26:08 -0700

    Cleared npm test errors Some other lint cleanup

--- src/js/config.js
+++ src/js/config.js
@@ -1,7 +1,7 @@
 // Note that we import these values where needed as "webAppConfig"
 module.exports = {
-  WE_VOTE_URL_PROTOCOL: "https://",  // "http://" for local dev or "https://" for live server
-  WE_VOTE_HOSTNAME: "wevote.us",  // This should be without "http...". This is "WeVote.US" on live server.
+  WE_VOTE_URL_PROTOCOL: "http://",  // "http://" for local dev or "https://" for live server
+  WE_VOTE_HOSTNAME: "localhost:3000",  // This should be without "http...". This is "WeVote.US" on live server.
 
   WE_VOTE_SERVER_ROOT_URL: "https://api.wevoteusa.org/",
   WE_VOTE_SERVER_ADMIN_ROOT_URL: "https://api.wevoteusa.org/admin/",
@@ -13,7 +13,7 @@ module.exports = {
   LOG_ONLY_FIRST_RENDER_EVENTS: false,
   LOG_HTTP_REQUESTS: false,
   LOG_ROUTING: false,
-  LOG_SIGNIN_STEPS: true,
+  LOG_SIGNIN_STEPS: false,
 
   // Use 1 or 0 as opposed to true or false
   test: {
@@ -23,8 +23,9 @@ module.exports = {
   location: {
     text_for_map_search: "",
   },
+  // We currently store the Google Places API key in /src/index.html
 
-  FACEBOOK_APP_ID: "1097389196952441",
+  FACEBOOK_APP_ID: "",
 
-  STRIPE_API_KEY: "pk_live_zpl7eSkLnySR2i4vtJQ1lJmi",
+  STRIPE_API_KEY: "",
 };
