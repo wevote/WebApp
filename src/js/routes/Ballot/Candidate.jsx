129e26702a3809bac402247c9bfaa39af4dcb4d8
Edward Ly <edvard.ly@gmail.com>
2018-09-17 17:56:40 -0700

    Moved related issues list to bottom of candidate page

--- src/js/routes/Ballot/Candidate.jsx
+++ src/js/routes/Ballot/Candidate.jsx
@@ -9,6 +9,7 @@ import { capitalizeString } from "../../utils/textFormat";
 import GuideList from "../../components/VoterGuide/GuideList";
 import Helmet from "react-helmet";
 import IssueActions from "../../actions/IssueActions";
+import IssuesFollowedByBallotItemDisplayList from "../../components/Issues/IssuesFollowedByBallotItemDisplayList";
 import IssueStore from "../../stores/IssueStore";
 import LoadingWheel from "../../components/LoadingWheel";
 import { renderLog } from "../../utils/logging";
@@ -211,6 +212,11 @@ export default class Candidate extends Component {
                        ballotItemWeVoteId={this.state.candidate_we_vote_id}
                        organizationsToFollow={this.state.voter_guides_to_follow_for_this_ballot_item}/></div>
           }
+        </div>
+        <div className="card__additional u-inset__squish--md">
+          <IssuesFollowedByBallotItemDisplayList ballot_item_display_name={this.state.candidate.ballot_item_display_name}
+                                                 ballotItemWeVoteId={this.state.candidate_we_vote_id}
+                                                 overlayTriggerOnClickOnly />
         </div>
       </section>
       <OpenExternalWebSite url="https://api.wevoteusa.org/vg/create/"
