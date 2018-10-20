1e4e523987c16c51f9fa5f28b9c0cd1e66a74afb
Gavin Jefferies <gavin@runtime.io>
2018-10-20 16:56:42 -0700

    [Issue #1732] get npm run autoTest to a working place.
    . Removed the unused & obsolete chai-jsx dependency.
    . Fixed the example test.

--- tests/component/tests.js
+++ tests/component/tests.js
@@ -1,15 +1,16 @@
-import React from 'react';
-import {render} from 'enzyme';
-import {expect} from 'chai';
-import FriendsOnlyIndicator from '../../src/js/components/Widgets/FriendsOnlyIndicator.jsx';
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import React from "react";
+import { mount } from "enzyme";
+import { describe, it } from "mocha";
+import { expect } from "chai";
+import FriendsOnlyIndicator from "../../src/js/components/Widgets/FriendsOnlyIndicator.jsx";
+import Enzyme from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
 Enzyme.configure({ adapter: new Adapter() });
 
-// describe('FriendsOnlyIndicator', () => {
-//   it('Should have appropriate classname', () => {
-//     const wrapper = render(<FriendsOnlyIndicator />);
-//     expect(wrapper.find('div')).to.have.className('public-friends-indicator');
-//   });
-// });
+describe("FriendsOnlyIndicator", () => {
+  it("Should have an appropriate classname", () => {
+    const wrapper = mount(<FriendsOnlyIndicator />);
+    expect(wrapper.find("span")).to.have.className("public-friends-indicator");
+  });
+});
