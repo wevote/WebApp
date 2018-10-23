98b9f6d11c03cca928ecd7e20efafc2053b774ab
ACODE91 <aaron.z.lao@gmail.com>
2018-10-22 21:48:57 -0700

    Fixed issue #1743 ItemSupportOppose

--- src/js/components/Widgets/ItemActionBar.jsx
+++ src/js/components/Widgets/ItemActionBar.jsx
@@ -527,25 +527,25 @@ export default class ItemActionBar extends Component {
       <div className={(this.state.yesVoteDescriptionExists || this.state.noVoteDescriptionExists ? "" : "btn-group") + (!this.props.shareButtonHide ? " u-push--sm" : "")}>
 
         {/* Start of Support Button */}
-        {/* Hide on screens smaller than large*/}
-        <div className="d-block d-none d-lg-block item-actionbar__position-bar">
-          {/* <OverlayTrigger placement="top" overlay={supportButtonPopoverTooltip}>{supportButton}</OverlayTrigger>  */}
+        {/* Visible on desktop screens */}
+        <div className="d-none d-lg-block item-actionbar__position-bar">
+          <OverlayTrigger placement="top" overlay={supportButtonPopoverTooltip}>{supportButton}</OverlayTrigger> 
           {this.state.yesVoteDescriptionExists ? <span className="item-actionbar__following-text">{this.state.yesVoteDescription}</span> : null}
         </div>
-         {/* Hide on screens wider than medium */}
-        <div className="d-block d-md-none item-actionbar__position-bar item-actionbar__position-bar--mobile">
+         {/* Visible on mobile devices and tablets */}
+        <div className="d-lg-none d-xl-none item-actionbar__position-bar item-actionbar__position-bar--mobile">
           {supportButton}
           {this.state.yesVoteDescriptionExists ? <span className="item-actionbar__following-text">{this.state.yesVoteDescription}</span> : null}
         </div>
 
         {/* Start of Oppose Button */}
-        {/* Hide on screens smaller than large*/}
-        <div className="d-block d-none d-lg-block  item-actionbar__position-bar">
+        {/* Visible on desktop screens */}
+        <div className="d-none d-lg-block item-actionbar__position-bar">
           <OverlayTrigger placement="top" overlay={opposeButtonPopoverTooltip}>{opposeButton}</OverlayTrigger>
           {this.state.noVoteDescriptionExists ? <span className="item-actionbar__following-text">{this.state.noVoteDescription}</span> : null}
         </div>
-        {/* Hide on screens wider than medium */}
-        <div className="d-block d-md-none item-actionbar__position-bar item-actionbar__position-bar--mobile">
+        {/* Visible on mobile devices and tablets */}
+        <div className="d-lg-none d-xl-none item-actionbar__position-bar item-actionbar__position-bar--mobile">
           {opposeButton}
           {this.state.noVoteDescriptionExists ? <span className="item-actionbar__following-text">{this.state.noVoteDescription}</span> : null}
         </div>
