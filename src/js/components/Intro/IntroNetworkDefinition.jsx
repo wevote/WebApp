87c094934f4524b463fccf58a88c547e121bda93
stevepodell <steve@podell.com>
2018-11-07 09:07:39 -0800

    Re-enabled the propTypes in the slider pane classes, the underlying issue has been resolved by other changes.

--- src/js/components/Intro/IntroNetworkDefinition.jsx
+++ src/js/components/Intro/IntroNetworkDefinition.jsx
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
-// import PropTypes from "prop-types";
+import PropTypes from "prop-types";
 import { cordovaDot, isCordova } from "../../utils/cordovaUtils";
 import { renderLog } from "../../utils/logging";
 
 export default class IntroNetworkDefinition extends Component {
-  // Oct 1, 2018: Seems to cause the compenent to initialize too early with React 16
-  // static propTypes = {
-  //   history: PropTypes.object,
-  //   next: PropTypes.func,
-  // };
+  static propTypes = {
+    history: PropTypes.object,
+    next: PropTypes.func,
+  };
 
   constructor (props) {
     super(props);
