b9b3dfe605442af637635568c7b1fc4488e02b73
stevepodell <steve@podell.com>
2018-12-07 09:24:35 -0800

    Fix to CandidateActions, send non-camel case to api

--- src/js/actions/CandidateActions.js
+++ src/js/actions/CandidateActions.js
@@ -2,11 +2,11 @@ import Dispatcher from "../dispatcher/Dispatcher";
 
 export default {
   candidateRetrieve (candidateWeVoteId) {
-    Dispatcher.loadEndpoint("candidateRetrieve", { candidateWeVoteId });
+    Dispatcher.loadEndpoint("candidateRetrieve", { candidate_we_vote_id: candidateWeVoteId });
   },
 
   candidatesRetrieve (officeWeVoteId) {
-    Dispatcher.loadEndpoint("candidatesRetrieve", { officeWeVoteId });
+    Dispatcher.loadEndpoint("candidatesRetrieve", { office_we_vote_id: officeWeVoteId });
   },
 
   positionListForBallotItem (weVoteId) {
