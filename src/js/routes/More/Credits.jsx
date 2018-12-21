f156ccf393bad449b9c22747f668ee3718ecc5f9
lazorfuzz <leontosy@gmail.com>
2018-12-20 17:41:11 -0800

    [Issue #1755] Update Credits page to support Company Logos

--- src/js/routes/More/Credits.jsx
+++ src/js/routes/More/Credits.jsx
@@ -18,19 +18,19 @@ export default class Credits extends Component {
           <h1 className="h1">Credits</h1>
 
           <h3 className="h4">We are grateful for these organizations that are critical to our work.</h3>
-          <ul>
-            { organizationalDonors.map(item => (
-              <div key={item.name}>
-                <li>
-                  <strong>{item.name}</strong>
-                  {" "}
-                  -
-                  {" "}
-                  {item.title}
-                </li>
+          { organizationalDonors.map(item => (
+            <div className="company" key={item.name}>
+              <div className="company__logo_container">
+                <img className="company__logo" src={item.logo} />
               </div>
-            )) }
-          </ul>
+              <div className="company__info">
+                <strong>{item.name}</strong>
+                <p className="company__title">
+                  {item.title}
+                </p>
+              </div>
+            </div>
+          )) }
           <h3 className="h4">Special thanks to our team of volunteers.</h3>
           <p>
             You are the best!
