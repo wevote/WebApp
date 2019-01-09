956867add762bdaa7f17c15b1438c8fe172f2605
coffeewarrior <lionlikelucas@gmail.com>
2019-01-09 11:11:24 -0500

    updated to use camelCased variables and fields

--- src/js/routes/Settings/Location.jsx
+++ src/js/routes/Settings/Location.jsx
@@ -18,8 +18,8 @@ export default class Location extends Component {
   constructor (props) {
     super(props);
     this.state = {
-      elections_locations_list: [],
-      voter_ballot_list: [],
+      electionLocationList: [],
+      voterBallotList: [],
     };
   }
 
@@ -38,41 +38,41 @@ export default class Location extends Component {
   }
 
   onElectionStoreChange () {
-    const elections_list = ElectionStore.getElectionList();
-    const elections_locations_list = [];
-    let voter_ballot; // A different format for much of the same data
-    const voter_ballot_list = [];
-    let one_ballot_location;
-    let ballot_location_shortcut;
-    let ballot_returned_we_vote_id;
+    const electionList = ElectionStore.getElectionList();
+    const electionLocationList = [];
+    let voterBallot; // A different format for much of the same data
+    const voterBallotList = [];
+    let oneBallotLocation;
+    letballotLocationShortcut;
+    let ballotReturnedWeVoteId;
 
-    for (let i = 0; i < elections_list.length; i++) {
-      const election = elections_list[i];
-      elections_locations_list.push(election);
-      ballot_returned_we_vote_id = "";
-      ballot_location_shortcut = "";
+    for (let i = 0; i < electionList.length; i++) {
+      const election = electionList[i];
+      electionLocationList.push(election);
+      ballotReturnedWeVoteId = "";
+     ballotLocationShortcut = "";
       if (election.ballot_location_list && election.ballot_location_list.length) {
         // We want to add the shortcut and we_vote_id for the first ballot location option
-        one_ballot_location = election.ballot_location_list[0];
-        ballot_location_shortcut = one_ballot_location.ballot_location_shortcut || "";
-        ballot_location_shortcut = ballot_location_shortcut.trim();
-        ballot_returned_we_vote_id = one_ballot_location.ballot_returned_we_vote_id || "";
-        ballot_returned_we_vote_id = ballot_returned_we_vote_id.trim();
+        oneBallotLocation = election.ballot_location_list[0];
+       ballotLocationShortcut = oneBallotLocation.ballot_location_shortcut || "";
+       ballotLocationShortcut =ballotLocationShortcut.trim();
+        ballotReturnedWeVoteId = oneBallotLocation.ballot_returned_we_vote_id || "";
+        ballotReturnedWeVoteId = ballotReturnedWeVoteId.trim();
       }
-      voter_ballot = {
-        google_civic_election_id: election.google_civic_election_id,
-        election_description_text: election.election_name,
-        election_day_text: election.election_day_text,
-        original_text_for_map_search: "",
-        ballot_location_shortcut,
-        ballot_returned_we_vote_id,
+      voterBallot = {
+        googleCivicElectionId: election.google_civic_election_id,
+        electionDescriptionText: election.election_name,
+        electionDayText: election.election_day_text,
+        originalTextForMapSearch: "",
+       ballotLocationShortcut,
+        ballotReturnedWeVoteId,
       };
-      voter_ballot_list.push(voter_ballot);
+      voterBallotList.push(voterBallot);
     }
 
     this.setState({
-      elections_locations_list,
-      voter_ballot_list,
+      electionLocationList,
+      voterBallotList,
     });
   }
 
@@ -91,7 +91,7 @@ export default class Location extends Component {
           </div>
         </div>
         <div className="elections-list-container container-fluid well u-stack--md u-inset--md">
-          <BallotElectionList ballotElectionList={this.state.voter_ballot_list} ballotBaseUrl="/ballot" />
+          <BallotElectionList ballotElectionList={this.state.voterBallotList} ballotBaseUrl="/ballot" />
         </div>
       </div>
     );
