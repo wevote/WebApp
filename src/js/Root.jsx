95aaf28c072e7083b3f52e096eee09752eacb4eb
urosh <urosh.stojkovikj@gmail.com>
2019-01-30 22:49:46 -0800

    removed /Intro/IntroOpinions

--- src/js/Root.jsx
+++ src/js/Root.jsx
@@ -26,7 +26,6 @@ import HamburgerMenu from "./routes/More/HamburgerMenu";
 import HowToUse from "./routes/More/HowToUse";
 import Intro from "./routes/Intro/Intro";
 import IntroNetwork from "./routes/Intro/IntroNetwork";
-import IntroOpinions from "./routes/Intro/IntroOpinions";
 import IntroStory from "./routes/Intro/IntroStory";
 import IssuesToFollow from "./routes/IssuesToFollow";
 import IssuesFollowed from "./routes/IssuesFollowed";
@@ -121,7 +120,6 @@ const routes = () => (
 
     <Route path="/intro" component={Intro} />
     <Route path="/wevoteintro/network" component={IntroNetwork} />
-    <Route path="/intro/opinions" component={IntroOpinions} />
     <Route path="/intro/story" component={IntroStory} />
     <Route path="/intro/sample_ballot" component={SampleBallot} />
     <Route path="/intro/get_started" component={GetStarted} />
