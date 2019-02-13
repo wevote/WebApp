04b48f3d7a68b91f1b139935d9d61306d0c0b2dc
Denize Ignacio <denizeignacio@gmail.com>
2019-02-13 05:12:21 +0000

    WebPack now resolves jsx and transpiles jsx and js.

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const path = require('path');
 const port = process.env.PORT || 3000;
 
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: {
-    filename: 'bundle.[hash].js'
+    filename: 'bundle.[hash].js',
+    path: path.resolve(__dirname, 'build')
   }, 
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
 };
