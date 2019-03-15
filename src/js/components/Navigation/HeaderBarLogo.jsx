9e68a7aa3e1b6716b21518cf0ab56d5e9eb5cef5
Amy Wall <amywa11@yahoo.com>
2019-03-14 21:17:03 -0700

    fixed header bar logo to work with webpack

--- src/js/components/Navigation/HeaderBarLogo.jsx
+++ src/js/components/Navigation/HeaderBarLogo.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router';
 import { cordovaDot, isCordova } from '../../utils/cordovaUtils';
-
+import logo from "../../../img/global/svg-icons/we-vote-logo-horizontal-color-dark.svg";
 const HeaderBarLogo = ({ isBeta }) => (
   <span>
     <Link to={`${isCordova() ? '/ballot' : '/welcome'}`} className="page-logo page-logo-full-size">
-      <img className="header-logo-img" alt="We Vote logo" src={cordovaDot('/img/global/svg-icons/we-vote-logo-horizontal-color-dark.svg')} />
+      <img className="header-logo-img" alt="We Vote logo" src={cordovaDot(logo)} />
       {isBeta && <span className="beta-marker"><span className="beta-marker-inner">beta</span></span>}
     </Link>
   </span>
