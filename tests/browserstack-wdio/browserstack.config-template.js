1f2e868d19785335b32bcfcd71ed39af68edb99e
Phil Marshall <phillipsolomonmarshall@gmail.com>
2019-04-02 17:42:24 -0500

    added basic crossplatform test with webdriverio

---
+++ tests/browserstack-wdio/browserstack.config-template.js
@@ -0,0 +1,7 @@
+module.exports = {
+  BUILD: 'Enter a build name here',
+  NAME: 'Enter a name here',
+  BROWSERSTACK_USER: '',
+  BROWSERSTACK_KEY: '',
+  BROWSERSTACK_APK_URL: 'bs://b56867e436f1a46488678aa5f339b22c807742ab',
+};
---
+++ tests/browserstack-wdio/specs/test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+describe('Basic cross-platform WeVote test',  () => {
+  it('can visit the different pages in the app', async () => {
+    const isWebApp = !!driver.getContexts;
+    if (isWebApp) {
+      // switch contexts and click through intro
+      await driver.switchContext('WEBVIEW_org.wevote.cordova');
+      const firstNextButton = await $('.background--image2 .btn.btn-lg');
+      await firstNextButton.click();
+      const secondNextButton = await $('.background--image4 .btn.btn-lg');
+      await secondNextButton.click();
+      const thirdNextButton = await $('.background--image5 .btn.btn-lg');
+      await thirdNextButton.click();
+      await browser.pause(1000);
+    } else {
+      // navigate browser to WeVote QA site
+      await browser.url('https://quality.wevote.us/');
+
+    }
+    const valuesButtonSelector = (isWebApp) ? 'span=Values' : 'button[id="valuesTabHeaderBar"]';
+    const valuesButton =
+      await $(valuesButtonSelector);
+    await valuesButton.click();
+    await browser.pause(1000);
+    assert(true);
+  });
+});
