170292fdaa7688d1039b1f51921826da327f2523
utsab <utsabsaha@Utsabs-MacBook-Air.local>
2019-04-24 20:58:04 -0700

    Modify the webpack build script to generate a production build that does not use eval (was breaking Cordova)

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
   output: {
-    filename: 'bundle.[hash].js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
   }, 
   plugins: [
