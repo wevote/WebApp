7dc0e82cc746cce482ffb43ed6dd0bbab3ea59c9
Jarod Peachey <jwpeachey107@aol.com>
2019-05-06 14:56:24 -0400

    Update layout of sticky header - first commit

--- src/js/components/Ballot/CandidateStickyHeader.jsx
+++ src/js/components/Ballot/CandidateStickyHeader.jsx
@@ -14,27 +14,33 @@ class CandidateStickyHeader extends Component {
     const { candidate } = this.props;
     const {
       ballot_item_display_name: displayName,
+      contest_office_name: officeName,
       we_vote_id: candidateWeVoteId,
       candidate_photo_url_medium: candidatePhotoUrl,
     } = candidate;
     console.log(candidate);
     return (
       <Wrapper>
-        <MeasureInfo>
-          <Profile>
-            <Avatar src={candidatePhotoUrl} alt="candidate-photo" />
-            <Title>{displayName}</Title>
-          </Profile>
-
-          <BallotItemSupportOpposeCountDisplay ballotItemWeVoteId={candidateWeVoteId} />
-        </MeasureInfo>
-        <ActionContainer>
+        <Flex>
+          <ColumnOne>
+            <Profile>
+              <Avatar src={candidatePhotoUrl} alt="candidate-photo" />
+              <div>
+                <Title>{displayName}</Title>
+                <SubTitle>{officeName}</SubTitle>
+              </div>
+            </Profile>
+          </ColumnOne>
+          <ColumnTwo>
+            <BallotItemSupportOpposeCountDisplay ballotItemWeVoteId={candidateWeVoteId} />
+          </ColumnTwo>
+        </Flex>
+        <BallotCommentContainer>
           <BallotItemSupportOpposeComment
             ballotItemWeVoteId={candidateWeVoteId}
             showPositionStatementActionBar={false}
           />
-          <hr />
-        </ActionContainer>
+        </BallotCommentContainer>
       </Wrapper>
     );
   }
@@ -50,66 +56,82 @@ const slideDown = keyframes`
 `;
 
 const Wrapper = styled.div`
+  max-width: 100%;
   position: fixed;
+  padding: 16px;
   padding-top: 48px;
-  padding-left: 32px;
   top: 0;
   left: 0;
   background: white;
   z-index: 2;
   width: 100vw;
-  display: flex;
-  flex-flow: column;
   box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
   animation: ${slideDown} 150ms ease-in;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    padding-left: 16px;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    padding: 32px;
+    padding-top: 48px;
   }
 `;
 
-const MeasureInfo = styled.div`
-  max-width: 60%;
+const ColumnOne = styled.div`
+  width: 100%;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    flex: 1 1 0;
+  }
   display: flex;
-  flex-flow: row;
-  justify-content: space-between;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    max-width: 100%;
+  align-items: flex-start; 
+`;
+
+const ColumnTwo = styled.div`
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    display: block;
+    width: fit-content;
   }
 `;
 
 const Title = styled.h1`
-  font-size: 22px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    font-size: 16px;
-    margin: auto 0;
+  font-size: 16px;
+  margin-bottom: 2px;
+  margin-top: 0;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    font-size: 22px;
+    font-weight: bold;
   }
 `;
-/*
+
 const SubTitle = styled.p`
-  font-size: 18px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    font-size: 12px;
+  font-size: 11px;
+  margin: 0;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    font-size: 15px;
   }
 `;
-*/
+
 const Profile = styled.div`
   display: flex;
   flex-flow: row;
-`;
-
-const ActionContainer = styled.div`
-  margin-top: -24px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    margin-top: -12px;
-    margin-left: -12px;
-  }
 `;
 
 const Avatar = styled.img`
   width: 48px;
   height: 48px;
   border-radius: 50%;
-  margin: 1em 1em 1em 0;
+  margin: 0 1em 1em 0;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  jusitify-content: space-evenly;
+  position: relative;
+  top: 8px;
+`;
+
+const BallotCommentContainer = styled.div`
+  width: fit-content;
+  margin-top: 8px;
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    margin-left: -16px;
+  }
 `;
 
 export default CandidateStickyHeader;
