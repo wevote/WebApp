79cb42dfd924b64179c80c979c8ea2d7b2a8f084
Tom Lee <user512@users.noreply.github.com>
2019-05-08 20:49:57 -0700

    Fix img in Cordova

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build')
-  }, 
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html'
@@ -37,6 +37,7 @@ module.exports = {
             loader: 'file-loader',
             options: {
               publicPath: '/',
+              name: 'img/[name].[ext]',
             },
           },
         ],
