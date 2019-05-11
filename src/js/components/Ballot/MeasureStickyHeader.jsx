b83bd1fcdd30faf580cd336390fd9fa7086c3733
Jarod Peachey <jwpeachey107@aol.com>
2019-05-11 11:08:55 -0400

    Update measure sticky header

--- src/js/components/Ballot/MeasureStickyHeader.jsx
+++ src/js/components/Ballot/MeasureStickyHeader.jsx
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 import BallotItemSupportOpposeComment from '../Widgets/BallotItemSupportOpposeComment';
 import MeasureStore from '../../stores/MeasureStore';
+import BallotItemSupportOpposeCountDisplay from '../Widgets/BallotItemSupportOpposeCountDisplay';
 
 class MeasureStickyHeader extends Component {
   static propTypes = {
@@ -45,67 +46,143 @@ class MeasureStickyHeader extends Component {
     const { ballotItemDisplayName } = this.state;
     const ballotItemDisplay = ballotItemDisplayName ? ballotItemDisplayName.split(':') : [];
     return (
+      // <Wrapper>
+      //   <MeasureInfo>
+      //     <Title>{ballotItemDisplay[0]}</Title>
+      //     <SubTitle>{ballotItemDisplay[1]}</SubTitle>
+      //     <ActionContainer>
+      //       <BallotItemSupportOpposeComment
+      //         ballotItemWeVoteId={measureWeVoteId}
+      //         showPositionStatementActionBar={false}
+      //       />
+      //     </ActionContainer>
+      //   </MeasureInfo>
+      // </Wrapper>
       <Wrapper>
-        <MeasureInfo>
-          <Title>{ballotItemDisplay[0]}</Title>
-          <SubTitle>{ballotItemDisplay[1]}</SubTitle>
-          <ActionContainer>
+        <Container>
+          <Flex>
+            <ColumnOne>
+              <Profile>
+                <div>
+                  <Title>{ballotItemDisplay[0]}</Title>
+                  <SubTitle>{ballotItemDisplay[1]}</SubTitle>
+                </div>
+              </Profile>
+              <MobileSubtitle className="u-show-mobile-tablet">
+                {ballotItemDisplay[1]}
+              </MobileSubtitle>
+            </ColumnOne>
+            <ColumnTwo>
+              <BallotItemSupportOpposeCountDisplay ballotItemWeVoteId={measureWeVoteId} />
+            </ColumnTwo>
+          </Flex>
+          <BallotCommentContainer>
             <BallotItemSupportOpposeComment
               ballotItemWeVoteId={measureWeVoteId}
               showPositionStatementActionBar={false}
             />
-          </ActionContainer>
-        </MeasureInfo>
+          </BallotCommentContainer>
+        </Container>
       </Wrapper>
     );
   }
 }
 
+const slideDown = keyframes`
+  from {
+    transform: translateY(-100%);
+  }
+  to {
+    transform: translateY(0);
+  }
+`;
+
 const Wrapper = styled.div`
+  max-width: 100%;
   position: fixed;
+  padding: 16px;
   padding-top: 48px;
-  padding-left: 32px;
   top: 0;
   left: 0;
   background: white;
   z-index: 2;
   width: 100vw;
-  display: flex;
-  flex-flow: row;
   box-shadow: 0px 2px 4px -1px rgba(0,0,0,0.2), 0px 4px 5px 0px rgba(0,0,0,0.14), 0px 1px 10px 0px rgba(0,0,0,0.12);
-  transition: all 100ms ease-in;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    padding-left: 16px;
+  animation: ${slideDown} 150ms ease-in;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    padding: 32px;
+    padding-top: 48px;
   }
 `;
 
-const MeasureInfo = styled.div`
-  max-width: 60%;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    max-width: 100%;
+const Container = styled.div`
+  max-width: calc(960px - 18px);
+  margin: 0 auto;
+`;
+
+const ColumnOne = styled.div`
+  width: 100%;
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    flex: 1 1 0;
+  }
+`;
+
+const ColumnTwo = styled.div`
+  @media (min-width: ${({ theme }) => theme.breakpoints.sm}) {
+    display: block;
+    width: fit-content;
   }
 `;
 
 const Title = styled.h1`
-  font-size: 22px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    font-size: 16px;
-    margin: auto 0;
+  font-size: 16px;
+  margin-bottom: 2px;
+  font-size: 26px;
+  margin-top: 8px;
+  font-weight: bold;
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    margin-top: 0;
+    font-size: 22px;
   }
 `;
 
 const SubTitle = styled.p`
-  font-size: 18px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    font-size: 12px;
+  display: none;
+  @media (min-width: ${({ theme }) => theme.breakpoints.md}) {
+    font-size: 15px;
+    display: block;
   }
 `;
 
-const ActionContainer = styled.div`
-  margin-top: -24px;
-  @media (max-width: ${({ theme }) => theme.breakpoints.lg}) {
-    margin-top: -12px;
-    margin-left: -12px;
+const MobileSubtitle = styled.h2`
+  display: none;
+  @media (max-width: ${({ theme }) => theme.breakpoints.md}) {
+    font-size: 18px;
+    display: block;
+    font-weight: 400;
+  }
+`;
+
+const Profile = styled.div`
+  display: flex;
+  flex-flow: row;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  jusitify-content: space-evenly;
+  position: relative;
+  top: 8px;
+`;
+
+const BallotCommentContainer = styled.div`
+  width: fit-content;
+  margin-top: 8px;
+  @media (max-width : ${({ theme }) => theme.breakpoints.sm}) {
+    padding-top: 8px;
+  }
+  > * {
+    padding: 0;
   }
 `;
 
