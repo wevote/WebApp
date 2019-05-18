1e22fb13b96e22cd43f52e7b0f43b75df72ee53d
Jarod Peachey <jwpeachey107@aol.com>
2019-05-18 11:32:53 -0400

    Update toolbar icons to display on about page

--- src/js/routes/More/ToolBar.jsx
+++ src/js/routes/More/ToolBar.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
 import IconButton from '@material-ui/core/IconButton';
+import styled from 'styled-components';
 import Tooltip from '@material-ui/core/Tooltip';
 import { withStyles } from '@material-ui/core';
 import { renderLog } from '../../utils/logging';
@@ -18,42 +20,96 @@ class ToolBar extends Component {
     const hideGitHub = this.props.hideGitHub ? this.props.hideGitHub : false;
 
     return (
-      <div className="btn-toolbar">
-        <OpenExternalWebSite
-          url="https://twitter.com/WeVote"
-          target="_blank"
-          body={<Tooltip title="Twitter"><IconButton><ion-icon name="logo-twitter" /></IconButton></Tooltip>}
-        />
-
-        <OpenExternalWebSite
-          url="https://www.facebook.com/WeVoteUSA"
-          target="_blank"
-          body={<Tooltip title="Facebook"><IconButton><ion-icon name="logo-facebook" /></IconButton></Tooltip>}
-        />
-
-        <OpenExternalWebSite
-          url="http://eepurl.com/cx_frP"
-          target="_blank"
-          body={<Tooltip title="Newsletter"><IconButton><ion-icon name="mail" /></IconButton></Tooltip>}
-        />
-
-        {!hideGitHub && (
+      <div>
+        <Helmet>
+          <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossOrigin="anonymous" />
+        </Helmet>
+        <ToolBarContainer className="btn-toolbar">
           <OpenExternalWebSite
-            url="https://github.com/WeVote"
+            className="u-no-underline"
+            url="https://twitter.com/WeVote"
             target="_blank"
-            body={<Tooltip title="GitHub"><IconButton><ion-icon name="logo-github" /></IconButton></Tooltip>}
+            body={(
+              <Tooltip title="Twitter">
+                <IconButton>
+                  <Icon className="fab fa-twitter" />
+                </IconButton>
+              </Tooltip>
+            )}
           />
-        )
-        }
-        <OpenExternalWebSite
-          url="https://blog.wevote.us/"
-          target="_blank"
-          body={<Tooltip title="Blog"><IconButton><ion-icon name="logo-wordpress" /></IconButton></Tooltip>}
-        />
+
+          <OpenExternalWebSite
+            className="u-no-underline"
+            url="https://www.facebook.com/WeVoteUSA"
+            target="_blank"
+            body={(
+              <Tooltip title="Facebook">
+                <IconButton>
+                  <Icon className="fab fa-facebook-square" />
+                </IconButton>
+              </Tooltip>
+            )}
+          />
+
+          <OpenExternalWebSite
+            className="u-no-underline"
+            url="http://eepurl.com/cx_frP"
+            target="_blank"
+            body={(
+              <Tooltip title="Newsletter">
+                <IconButton>
+                  <Icon className="fas fa-envelope" />
+                </IconButton>
+              </Tooltip>
+            )}
+          />
+
+          {!hideGitHub && (
+            <OpenExternalWebSite
+              className="u-no-underline"
+              url="https://github.com/WeVote"
+              target="_blank"
+              body={(
+                <Tooltip title="Github">
+                  <IconButton>
+                    <Icon className="fab fa-github" />
+                  </IconButton>
+                </Tooltip>
+              )}
+            />
+          )
+          }
+          <OpenExternalWebSite
+            className="u-no-underline"
+            url="https://blog.wevote.us/"
+            target="_blank"
+            body={(
+              <Tooltip title="Blog">
+                <IconButton>
+                  <Icon className="fab fa-wordpress" />
+                </IconButton>
+              </Tooltip>
+            )}
+          />
+        </ToolBarContainer>
       </div>
     );
   }
 }
+
+const Icon = styled.i`
+  color: white;
+  text-decoration: none;
+`;
+
+const ToolBarContainer = styled.div`
+  text-align: center;
+  margin-top: 0;
+  justify-content: center;
+  display: flex;
+  position: relative;
+  top: -10px;
+`;
 
 const styles = theme => ({
   iconButtonRoot: {
