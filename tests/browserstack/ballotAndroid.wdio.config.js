1d168c5c24742e79919a0ff6e9602deab05e5aa5
Dale John McGrew <dale.mcgrew@wevote.us>
2019-05-30 11:37:30 -0700

    Turning off the Friends navigation until we can spend more time debugging. Underlying Friends code is still active.

--- tests/browserstack/ballotAndroid.wdio.config.js
+++ tests/browserstack/ballotAndroid.wdio.config.js
@@ -14,24 +14,25 @@ exports.config = {
   capabilities: [
     {
       // capabilities for a cordova android test
-      name: 'ballotMainTest-Android',
+      name: 'ballotMainTest-AndroidGalaxyTabS4',
       build: buildNameForDisplay,
       device: 'Samsung Galaxy Tab S4',
       os_version: '8.1',
       app: browserStackConfig.BROWSERSTACK_APK_URL,
+      'browserstack.console': 'info',
       'browserstack.debug': true,
       'browserstack.geoLocation': 'US',
     },
   ],
   coloredLogs: true,
   baseUrl: '',
-  waitforTimeout: 50000,
+  waitforTimeout: 180000, // 3 minutes
   connectionRetryTimeout: 90000,
   connectionRetryCount: 3,
 
   framework: 'mocha',
   mochaOpts: {
     ui: 'bdd',
-    timeout: 50000,
+    timeout: 180000, // 3 minutes
   },
 };
