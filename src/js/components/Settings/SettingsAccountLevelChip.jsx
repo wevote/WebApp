891056bb947f8aec47b92831335bc2086c4eceff
Phil Marshall <phillipsolomonmarshall@gmail.com>
2019-07-22 18:46:28 -0500

    implemented rest of component

--- src/js/components/Settings/SettingsAccountLevelChip.jsx
+++ src/js/components/Settings/SettingsAccountLevelChip.jsx
@@ -3,41 +3,66 @@ import Chip from '@material-ui/core/Chip';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import { renderLog } from '../../utils/logging';
+import AppActions from '../../actions/AppActions';
 
 const styles = {
   root: {
-    height: 24,
+    height: 20,
     marginLeft: 4,
   },
   label: {
     paddingLeft: 8,
     paddingRight: 8,
+    fontSize: 12,
+  },
+  notUpgraded: {
+    backgroundColor: 'rgb(242, 230, 210)',
+    color: 'rgb(192, 133, 17)',
+    '&:hover, &:focus': {
+      backgroundColor: 'rgb(44, 58, 95)',
+      color: 'white',
+    },
+  },
+  alreadyUpgraded: {
+    backgroundColor: 'rgb(232, 232, 232)',
+    color: 'rgb(170, 170, 170)',
+    '&:hover, &:focus': {
+      backgroundColor: 'rgb(4,193,108)',
+      color: 'white',
+    },
   },
 };
 
 class SettingsAccountLevelChip extends Component {
   static propTypes = {
-    accountLevel: PropTypes.oneOf(['free', 'pro', 'enterprise']),
-    featureIsEnterpriseLevel: PropTypes.bool,
+    userAccountLevel: PropTypes.oneOf(['free', 'pro', 'enterprise']),
+    featureAccountLevel: PropTypes.oneOf(['pro', 'enterprise']),
     classes: PropTypes.object,
-    size: PropTypes.string,
   };
 
-  constructor (props) {
-    super(props);
-    this.state = {};
+  onClickHandler = () => {
+    const mode = this.props.featureAccountLevel === 'pro' ? 'professional' : 'enterprise';
+    this.openPaidAccountUpgradeModal(mode);
+  }
+
+  openPaidAccountUpgradeModal = (paidAccountUpgradeMode) => {
+    AppActions.setShowPaidAccountUpgradeModal(paidAccountUpgradeMode);
   }
 
   render () {
     renderLog(__filename);
-    const { accountLevel, classes, size } = this.props;
+    const { userAccountLevel, featureAccountLevel, classes } = this.props;
+    const userAlreadyUpgraded = userAccountLevel === 'enterprise' || (userAccountLevel === 'pro' && featureAccountLevel === 'pro');
+    const chipLabel = userAccountLevel === 'enterprise' ? 'ENTERPRISE' : featureAccountLevel.toUpperCase();
     return (
       <Chip
-        classes={size === 'small' ? {
-          root: classes.root,
+        classes={{
+          root: `${classes.root} ${userAlreadyUpgraded ? classes.alreadyUpgraded : classes.notUpgraded}`,
           label: classes.label,
-        } : {}}
-        label={accountLevel === 'enterprise' ? 'ENTERPRISE' : 'PRO'}
+        }}
+        label={chipLabel}
+        onClick={userAlreadyUpgraded ? undefined : this.onClickHandler}
+        clickable
       />
     );
   }
