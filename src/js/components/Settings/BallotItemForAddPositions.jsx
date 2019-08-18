43da2f3fa7868e864452c12d473038a183203b10
Jarod Peachey <jwpeachey107@aol.com>
2019-08-18 09:15:10 -0400

    Remove error

--- src/js/components/Settings/BallotItemForAddPositions.jsx
+++ src/js/components/Settings/BallotItemForAddPositions.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-// import styled from 'styled-components';
+import styled from 'styled-components';
 import { withStyles, withTheme } from '@material-ui/core';
 import { renderLog } from '../../utils/logging';
 import MeasureItemForAddPositions from './MeasureItemForAddPositions';
