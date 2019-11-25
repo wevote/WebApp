6d4e8427f71da7eba2b86e2d3e3aa575e954a92d
Jarod Peachey <jwpeachey107@aol.com>
2019-11-25 14:31:31 -0500

    Get tabs working

--- src/js/routes/Friends.jsx
+++ src/js/routes/Friends.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
 import styled from 'styled-components';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Info from '@material-ui/icons/Info';
-import { Tooltip } from '@material-ui/core';
+import { Tooltip, Paper } from '@material-ui/core';
 import { withStyles } from '@material-ui/core/styles';
 import AnalyticsActions from '../actions/AnalyticsActions';
 import BrowserPushMessage from '../components/Widgets/BrowserPushMessage';
@@ -28,26 +30,52 @@ class Friends extends Component {
   constructor (props) {
     super(props);
     this.state = {
+      value: '',
     };
+
+    this.handleResize = this.handleResize.bind(this);
   }
 
   componentDidMount () {
     this.onVoterStoreChange();
     this.voterStoreListener = VoterStore.addListener(this.onVoterStoreChange.bind(this));
     AnalyticsActions.saveActionNetwork(VoterStore.electionId());
+
+    this.handleResize();
+    window.addEventListener('resize', this.handleResize);
+
+    this.setState({ value: this.props.params.tabItem || 'requests' });
   }
+
+  // shouldComponentUpdate (nextState) {
+  //   if (this.state.mobileMode !== nextState.mobileMode) return true;
+  //   return false;
+  // }
 
   componentWillUnmount () {
     this.voterStoreListener.remove();
+    window.removeEventListener('resize', this.handleResize);
   }
 
   onVoterStoreChange () {
     this.setState({ voter: VoterStore.getVoter() });
   }
 
+    handleResize () {
+    this.setState({
+      windowWidth: window.innerWidth,
+    });
+
+    if (window.innerWidth < 769) {
+      this.setState({ mobileMode: true });
+    } else {
+      this.setState({ mobileMode: false });
+    }
+  }
+
   render () {
     renderLog('Friends');  // Set LOG_RENDER_EVENTS to log all renders
-    const { voter } = this.state;
+    const { voter, value } = this.state;
     const { classes } = this.props;
 
     if (!voter) {
@@ -57,47 +85,86 @@ class Friends extends Component {
 
     return (
       <span>
-        <Helmet title="Friends - We Vote" />
-        <BrowserPushMessage incomingProps={this.props} />
-        <div className="row">
-          <div className="col-sm-12 col-md-8">
-            <FriendInvitationsSentToMePreview />
-            <SuggestedFriendsPreview />
-            {voterIsSignedIn && (
-              <FirstAndLastNameRequiredAlert />
-            )}
-            <section className="card">
-              <div className="card-main">
-                <SectionTitle>
-                  Invite Friends
-                </SectionTitle>
-                <Icon>
-                  <Tooltip
-                    classes={{ tooltip: classes.tooltip }}
-                    title="These friends will see what you support and oppose."
-                  >
-                    <Info />
-                  </Tooltip>
-                </Icon>
-                <AddFriendsByEmail />
+        {this.state.mobileMode ? (
+          <>
+            <Helmet title="Friends - We Vote" />
+              <Paper elevation={1}>
+                <Tabs
+                  value={value}
+                  // onChange={handleChange}
+                  indicatorColor="primary"
+                  textColor="primary"
+                  variant="scrollable"
+                  scrollButtons="auto"
+                  aria-label="scrollable auto tabs example"
+                >
+                  <Tab value="requests" label="Requests" onClick={() => {
+                    this.setState({ value: 'requests' });
+                    window.history.pushState({ tabItem: 'requests' }, '', '/friends/requests');
+                  }} />
+                  <Tab value="suggested" label="Suggested" onClick={() => {
+                    this.setState({ value: 'suggested' });
+                    window.history.pushState({ tabItem: 'suggested' }, '', '/friends/suggested');
+                  }} />
+                  <Tab value="invite" label="Add Contacts" onClick={() => {
+                    this.setState({ value: 'invite' });
+                    window.history.pushState({ tabItem: 'invite' }, '', '/friends/invite');
+                  }} />
+                  <Tab value="current" label="Friends" onClick={() => {
+                    this.setState({ value: 'current' });
+                    window.history.pushState({ tabItem: 'current' }, '', '/friends/current');
+                  }} />
+                  <Tab value="sent-requests" label="Sent Requests" onClick={() => {
+                    this.setState({ value: 'sent-requests' });
+                    window.history.pushState({ tabItem: 'sent-requests' }, '', '/friends/sent-requests');
+                  }} />
+                </Tabs>         
+              </Paper>
+          </>
+        ) : (
+          <>
+            <Helmet title="Friends - We Vote" />
+            <BrowserPushMessage incomingProps={this.props} />
+            <div className="row">
+              <div className="col-sm-12 col-lg-8">
+                <FriendInvitationsSentToMePreview />
+                <SuggestedFriendsPreview />
+                {voterIsSignedIn && (
+                  <FirstAndLastNameRequiredAlert />
+                )}
+                <FriendsCurrentPreview />
+                {voter.signed_in_twitter ? null : (
+                  <div className="u-show-mobile">
+                    <TwitterSignInCard />
+                  </div>
+                )}
+                {voterIsSignedIn && (
+                  <FriendInvitationsSentByMePreview />
+                )}
               </div>
-            </section>
-            <FriendsCurrentPreview />
-            {voter.signed_in_twitter ? null : (
-              <div className="u-show-mobile">
-                <TwitterSignInCard />
+              <div className="col-md-12 col-lg-4 d-none d-md-block">
+                {voterIsSignedIn ? (
+                  <section className="card">
+                    <div className="card-main">
+                      <SectionTitle>
+                        Invite Friends
+                      </SectionTitle>
+                      <Icon>
+                        <Tooltip
+                          classes={{ tooltip: classes.tooltip }}
+                          title="These friends will see what you support and oppose."
+                        >
+                          <Info />
+                        </Tooltip>
+                      </Icon>
+                      <AddFriendsByEmail />
+                    </div>
+                  </section>
+                ) : null}
               </div>
-            )}
-            {voterIsSignedIn && (
-              <FriendInvitationsSentByMePreview />
-            )}
-          </div>
-          <div className="col-md-4 d-none d-md-block">
-            {voter.signed_in_twitter ? null : (
-              <TwitterSignInCard />
-            )}
-          </div>
-        </div>
+            </div>
+          </>
+        )}
       </span>
     );
   }
