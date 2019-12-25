7b475c4aa55c08d32d2bf38954132894f985d988
stevepodell <steve@podell.com>
2019-12-25 13:53:50 -0800

    Added UnusedWebpackPlugin to identify unused files in the bundle.js In webpack.config.js, disable isProduction boolean to show unused files and to turn on the BundleAnalyzerPlugin. The BundleAnalyzerPlugin will then start a server on port 8888 that will display the interactive bundle analyzer. Deleted unused 370 glyphicons-pro svgs. Moved 60 icons into a DO-NOT-BUNDLE directory, that does not go into the bundle. Identified about 45 unused javascript class files in issue wevote/WebApp/issues/2700 -- Dale said he would take this one.

--- src/js/components/Search/SearchBar.jsx
+++ src/js/components/Search/SearchBar.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { cordovaDot } from '../../utils/cordovaUtils';
 import { renderLog } from '../../utils/logging';
+// Dec 2019 TODO: This is the last icon from the svg-icons package used in the Web App, all the other have been removed from git
 import removeCircleIcon from '../../../img/global/svg-icons/glyphicons-pro-halflings/glyphicons-halflings-88-remove-circle.svg';
 
 export default class SearchBar extends Component {
