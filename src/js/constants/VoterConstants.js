a3f832342668c2d8132be07e1614d85932c30de7
Dale John McGrew <Dale.McGrew@WeVote.US>
2020-02-22 17:23:09 -0800

    Update VoterConstants.js

--- src/js/constants/VoterConstants.js
+++ src/js/constants/VoterConstants.js
@@ -11,7 +11,7 @@ const VoterConstants = {
   BALLOT_INTRO_SHARE_COMPLETED: 64, // ...the voter has shared at least one item (no need for intro)
   BALLOT_INTRO_VOTE_COMPLETED: 128, // ...the voter learned about casting their vote (no need for intro)
   POSITION_PUBLIC_MODAL_SHOWN: 256, // ...the voter has seen the initial public position modal
-  HOW_IT_WORKS_WATCHED: 512, // ...the voter has seen the initial bookmark action modal
+  HOW_IT_WORKS_WATCHED: 512, // ...the voter has watched the 'How It Works' animation
 
   // Used for notification_settings bits. Which notification options has the voter chosen?
   NOTIFICATION_ZERO: 0,
