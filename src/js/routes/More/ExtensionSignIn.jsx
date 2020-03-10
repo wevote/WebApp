9a636e26bc912fb978c4737bc9cece53a3dce201
stevepodell <steve@podell.com>
2020-03-10 15:37:47 -0700

    Simplified ExtensionSignIn If the device ID stored in extension differs from the device ID in the web app, and the web app is signed in, when you navigate to the web app you will get the "you're signed in" dialogue, you press the button, the web app closes and you're back to the extension. If the device IDs match, and the web app is not signed in you'll get the login dialogue, and after login you will be at the US signed in dialogue

--- src/js/routes/More/ExtensionSignIn.jsx
+++ src/js/routes/More/ExtensionSignIn.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
-import { cordovaDot } from '../../utils/cordovaUtils';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
 import { renderLog } from '../../utils/logging';
 import LoadingWheel from '../../components/LoadingWheel';
 import SettingsAccount from '../../components/Settings/SettingsAccount';
@@ -9,6 +10,10 @@ import DelayedLoad from '../../components/Widgets/DelayedLoad';
 
 
 class ExtensionSignIn extends Component {
+  static propTypes = {
+    location: PropTypes.object,
+  };
+
   constructor (props) {
     super(props);
     this.state = {
@@ -18,7 +23,8 @@ class ExtensionSignIn extends Component {
   }
 
   componentDidMount () {
-    // console.log('SettingsDomain componentDidMount');
+    const { title } = this.props.location.query;  // https://localhost:3000/more/extensionsignin?title=2020%20Endorsements%20%7C%20Sierra%20Club&
+    console.log(`SettingsDomain ExtensionSignIn title: ${title}`);
     this.onVoterStoreChange();
     // this.onOrganizationStoreChange();
     this.voterStoreListener = VoterStore.addListener(this.onVoterStoreChange.bind(this));
@@ -31,7 +37,6 @@ class ExtensionSignIn extends Component {
   onVoterStoreChange () {
     const voter = VoterStore.getVoter();
     const voterIsSignedIn = voter.is_signed_in;
-    // console.log('onVoterStoreChange organization: ', organization);
     this.setState({
       voter,
       voterIsSignedIn,
@@ -40,12 +45,18 @@ class ExtensionSignIn extends Component {
 
   render () {
     renderLog('extensionSignIn');  // Set LOG_RENDER_EVENTS to log all renders
+    /* eslint-disable react/jsx-one-expression-per-line */
+    /* eslint-disable react/jsx-max-props-per-line */
     const {
       voter, voterIsSignedIn,
     } = this.state;
     if (!voter) {
       return LoadingWheel;
     }
+    // let title = localStorage.extensionTitle;
+    // Unfortunately even local storage doesn't survive the Twitter redirects, we come back in the new instance, and the request handleOpenURL(
+    //   comes back without the search terms.
+
     if (!voterIsSignedIn) {
       // console.log('voterIsSignedIn is false');
       return (
@@ -53,72 +64,41 @@ class ExtensionSignIn extends Component {
           <SignInOuterWrapper>
             <SignInInnerWrapper>
               <SignInIntro>
-                Please sign in here to start:
+                Please sign in here:
               </SignInIntro>
               <SettingsAccount />
             </SignInInnerWrapper>
           </SignInOuterWrapper>
         </DelayedLoad>
       );
+    } else {
+      return (
+        <Wrapper>
+          <div className="card" style={{ marginTop: '15%' }}>
+            <Success>
+              You are now signed in!
+              <br />
+              <br />
+              {/* This button is a placeholder, feel free to improve */}
+              <Button onClick={() => window.close()} color="primary" autoFocus style={{
+                transition: 'none',
+                color: '#fff',
+                backgroundColor: '#1976d2',
+                boxShadow: '0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)',
+                transitionTimingFunction: 'none !important',
+              }}
+              >
+                Return to the chrome extension
+              </Button>
+              <br />
+              <br />
+            </Success>
+          </div>
+        </Wrapper>
+      );
     }
-    return (
-      <Wrapper>
-        <Success>
-          You are now signed in! To finish this process, please follow these 3 steps.
-        </Success>
-        <Step>Step 1</Step>
-        <Description>Click the We Vote Chrome extension icon in browser navigation bar.</Description>
-        <ImageWrapper>
-          <IconImage src={cordovaDot('/img/global/screens/chrome-extension-icon-198x25.png')} />
-        </ImageWrapper>
-        <Step>Step 2</Step>
-        <Description>You will see a menu appear. Click the &quot;Sign In&quot; button.</Description>
-        <ImageWrapper>
-          <ScreenImage src={cordovaDot('/img/global/screens/chrome-extension-sign-in-452x246.png')} />
-        </ImageWrapper>
-        <Step>Step 3</Step>
-        <Description>
-          Go back to original tab in your browser where you clicked &quot;Sign In&quot;, and refresh the browser.
-        </Description>
-      </Wrapper>
-    );
   }
 }
-
-const Description = styled.h4`
-  font-size: 22px;
-  text-align: center;
-  @media (max-width: 576px) {
-    font-size: 15px;
-  }
-`;
-
-const IconImage = styled.img`
-  height: auto;
-  margin-top: 8px;
-  margin-bottom: 12px;
-  width: 60%;
-  @media (max-width: 576px) {
-    width: 100%;
-  }
-`;
-
-const ImageWrapper = styled.div`
-    text-align: center;
-`;
-
-const ScreenImage = styled.img`
-  border: 1px solid #999;
-  border-radius: 16px;
-  box-shadow: 2px 2px 4px 2px ${({ theme }) => theme.colors.grayLight};
-  width: 70%;
-  height: auto;
-  margin-top: 8px;
-  margin-bottom: 12px;
-  @media (max-width: 576px) {
-    width: 100%;
-  }
-`;
 
 const SignInOuterWrapper = styled.div`
   display: flex;
@@ -145,18 +125,6 @@ const SignInIntro = styled.div`
   }
 `;
 
-const Step = styled.h3`
-  color: #A9A9A9;
-  font-size: 25px;
-  font-weight: bold;
-  margin-top: 12px;
-  margin-bottom: 8px;
-  text-align: center;
-  @media (max-width: 576px) {
-    font-size: 15px;
-  }
-`;
-
 const Success = styled.div`
   color: #2e3c5d;
   font-size: 25px;
@@ -168,7 +136,6 @@ const Success = styled.div`
     font-size: 20px;
   }
 `;
-
 
 const Wrapper = styled.div`
   background-color: #E9EBEE;
