772b42f177a97267c58660d2af3315f4ed48fe03
4D617278 <4D617278@protonmail.com>
2020-05-21 23:05:42 -0500

    Added scripts for automation

--- tests/browserstack/template.wdio.config.js
+++ tests/browserstack/template.wdio.config.js
@@ -7,20 +7,46 @@ const buildNameForDisplay = `${browserStackConfig.BUILD}: ${dateForDisplay}`;
 exports.config = {
   user: browserStackConfig.BROWSERSTACK_USER,
   key: browserStackConfig.BROWSERSTACK_KEY,
+  webAppRootUrl: browserStackConfig.WEB_APP_ROOT_URL,
+  updateJob: false,
   specs: [
     './tests/browserstack/specs/%scriptNameMainTest.js',
-  ]
-
-  capabilities: [{
+  ],
+  logLevel: 'trace',
+  twitterUserName: browserStackConfig.TWITTER_USER_NAME,
+  twitterPassword: browserStackConfig.TWITTER_PASSWORD,
+  exclude: [],
+  capabilities: [
+    {
       name: '%name',
-      'build': buildNameForDisplay,
-      'project': '%project',
-      'device': '%device',
-      'os_version': '%os_version',
+      build: buildNameForDisplay,
+      device: '%device',
+      os: '%os',
+      os_version: '%OS_VERSION',
+      real_mobile: %real_mobile,
+      acceptSslCerts: %ssl,
       browser: '%browser',
-      'acceptSslCerts': '%ssl',
-      'browserstack.local': %local,
-      'browserstack.console': %console,
-      'browserstack.debug': %debug
-    }]
+      'browserstack.appium_version': '1.9.1',
+      'browserstack.console': '%console',
+      'browserstack.debug': %debug,
+      'browserstack.local': %localTest,
+      isAndroid: %isAndroid,
+      isCordovaFromAppStore: %isCordova,
+      isIOS: %isIOS,
+      isMobileScreenSize: %isMobileScreenSize,
+      project: '%project',
+    },
+  ],
+  coloredLogs: true,
+  logLevel: 'trace',
+  baseUrl: '',
+  waitforTimeout: 180000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+
+  framework: 'mocha',
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 180000,
+  },
 }
