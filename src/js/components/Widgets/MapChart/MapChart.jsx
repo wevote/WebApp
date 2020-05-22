cc6025cd30e7ebe9a8e377b19071afa041a4ae14
Jarod Peachey <jwpeachey107@aol.com>
2020-05-22 12:24:06 -0400

    Updates to map chart

--- src/js/components/Widgets/MapChart/MapChart.jsx
+++ src/js/components/Widgets/MapChart/MapChart.jsx
@@ -25,6 +25,12 @@ const offsets = {
   DC: [49, 21],
 };
 
+const positions = {
+  FL: [10, 5],
+  LA: [-10, 0],
+  ID: [0, 15],
+};
+
 /* eslint-disable react/prop-types */
 const MapChart = props => (
   <ComposableMap className="map-svg" projection="geoAlbersUsa">
@@ -33,14 +39,14 @@ const MapChart = props => (
         <>
           {/* eslint-disable-next-line arrow-body-style */}
           {geographies.map((geo) => {
-            // const cur = allStates.find(s => s.val === geo.id);
+            const cur = allStates.find(s => s.val === geo.id);
             // console.log(cur);
 
             return (
               <StyledGeography
                 className="map-svg"
-                onClick={props.onClickFunction}
-                onMouseDown={props.onClickFunction}
+                onClick={() => props.onClickFunction(cur.id)}
+                onMouseDown={() => props.onClickFunction(cur.id)}
                 key={geo.rsmKey}
                 stroke="#FFF"
                 geography={geo}
@@ -52,13 +58,20 @@ const MapChart = props => (
             const centroid = geoCentroid(geo);
             const cur = allStates.find(s => s.val === geo.id);
             return (
-              <StyledG style={{ cursor: 'pointer' }} onClick={props.onClickFunction} onMouseDown={props.onClickFunction} key={`${geo.rsmKey}-name`}>
+              <StyledG
+                dx={positions[cur.id] ? positions[cur.id][0] : 0}
+                dy={positions[cur.id] ? positions[cur.id][1] : 0}
+                style={{ cursor: 'pointer' }}
+                onClick={() => props.onClickFunction(cur.id)}
+                onMouseDown={() => props.onClickFunction(cur.id)}
+                key={`${geo.rsmKey}-name`}
+              >
                 {cur &&
                     centroid[0] > -160 &&
                     centroid[0] < -67 &&
                     (Object.keys(offsets).indexOf(cur.id) === -1 ? (
                       <>
-                        <StyledMarker onClick={props.onClickFunction} onMouseDown={props.onClickFunction} coordinates={centroid}>
+                        <StyledMarker onClick={() => props.onClickFunction(cur.id)} onMouseDown={() => props.onClickFunction(cur.id)} coordinates={centroid}>
                           <text onClick={props.onClickFunction} onMouseDown={props.onClickFunction} stroke={cur.color} y="2" fontSize={14} textAnchor="middle">
                             {cur.id}
                           </text>
@@ -105,6 +118,9 @@ const StyledG = styled.g`
   display: block !important;
   width: 10px !important;
   height: 10px !important;
+  position: relative;
+  left: ${props => props.dx}px;
+  top: ${props => props.dy}px;
 `;
 
 const StyledMarker = styled(Marker)`
