32070fbf88ab2437239c0eb781cb82dc0088395d
Dale John McGrew <Dale.McGrew@WeVote.US>
2020-06-15 10:53:22 -0700

    New code to fix problem if voter object missing.

--- src/js/utils/voterPhoto.js
+++ src/js/utils/voterPhoto.js
@@ -3,9 +3,13 @@ import { isIOS } from './cordovaUtils';
 
 // eslint-disable-next-line import/prefer-default-export
 export function voterPhoto (voter) {
+  const placeholderImageUrl = 'https://wevote.us/img/global/logos/Apple_logo_grey.svg';	
+  if (!voter) {	
+    return placeholderImageUrl;	
+  }
   const { voter_photo_url_medium: voterPhotoUrlMedium, signedInWithApple } = voter;
   if (!voterPhotoUrlMedium && signedInWithApple  && isIOS()) {
-    return 'https://wevote.us/img/global/logos/Apple_logo_grey.svg';
+    return placeholderImageUrl;
   }
   return voterPhotoUrlMedium;
 }
