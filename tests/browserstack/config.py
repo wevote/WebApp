ee8f2acb9f9ae3b6a68537795286812e85788a02
4D617278 <4D617278@protonmail.com>
2020-08-06 18:04:03 -0500

    Formatting changes

--- tests/browserstack/config.py
+++ tests/browserstack/config.py
@@ -6,65 +6,78 @@ from os import path
 from shutil import copyfile
 from subprocess import Popen
 
-parser = argparse.ArgumentParser(description='Generate wdio configuration file')
-parser.add_argument('-l', '--browserstack.local', action='store_true', help='Test on localhost')
-parser.add_argument('-s', '--scriptName', required=True, type=str, help='Script to test')
-parser.add_argument('-c', '--cookie', default='', type=str, help='voter_device_id cookie')
-parser.add_argument('-b', '--batch', required=True, type=str, choices=['Android', 'iOS', 'Browser'], help='test batch to test')
-parser.add_argument('-t', '--template', default='wdio.conf.template', type=str, help='path to template file (.template)')
-parser.add_argument('-f', '--file', default='parallel.wdio.config.js', type=str, help='path to js configuration file (.js)')
-parser.add_argument('-j', '--json', default='devices_to_test.json', type=str, help='path to json file')
-parser.add_argument('-n', '--numberOfTests', default=5, type=int, choices=range(1, 6), help='run first n number of tests')
-parser.add_argument('-o', '--offset', default=0, type=int, help='starting test number')
-parser.add_argument('-a', '--app', action='store_true', help='test mobile app instead of web app')
-parser.add_argument('-i', '--interchange', action='store_true', help='select specific test cases')
-parser.add_argument('-w', '--write', action='store_true', help='write to configuration file')
-parser.add_argument('-m', '--isMobileScreenSize', action='store_true', help='set mobile-screen to true')
-parser.add_argument('-d', '--isCordova', action='store_true', help='set isCordovaFromAppStore to true')
-parser.add_argument('-g', '--generate', action='store_true', help='generate template file')
-parser.add_argument('-r', '--run', action='store_true', help='run wdio configuration file')
+parser = argparse.ArgumentParser(description='Generate wdio conf file')
+parser.add_argument('-l', '--browserstack.local', action='store_true', 
+                    help='Test on localhost')
+parser.add_argument('-s', '--script', required=True, type=str, 
+                    help='Script to test')
+parser.add_argument('-c', '--cookie', default='', type=str, 
+                    help='voter_device_id cookie')
+parser.add_argument('-b', '--batch', required=True, type=str, 
+                    choices=['Android', 'iOS', 'Browser'], 
+                    help='test batch to test')
+parser.add_argument('-t', '--template', default='wdio.conf.template', type=str, 
+                    help='path to template file (.template)')
+parser.add_argument('-f', '--file', default='parallel.config.js', type=str, 
+                    help='path to js configuration file (.js)')
+parser.add_argument('-j', '--json', default='devices_to_test.json', type=str, 
+                    help='path to json file')
+parser.add_argument('-n', '--number', default=5, type=int, choices=range(1, 6), 
+                    help='run first n number of tests')
+parser.add_argument('-o', '--offset', default=0, type=int, 
+                    help='starting test number')
+parser.add_argument('-a', '--app', action='store_true', 
+                    help='test mobile app instead of web app')
+parser.add_argument('-t', '--trade', action='store_true', 
+                    help='select specific test cases')
+parser.add_argument('-w', '--write', action='store_true', 
+                    help='write to configuration file')
+parser.add_argument('-m', '--isMobileScreenSize', action='store_true', 
+                    help='set mobile-screen to true')
+parser.add_argument('-d', '--isCordova', action='store_true', 
+                    help='set isCordovaFromAppStore to true')
+parser.add_argument('-g', '--generate', action='store_true', 
+                    help='generate template file')
+parser.add_argument('-r', '--run', action='store_true', 
+                    help='run wdio configuration file')
+
 args = vars(parser.parse_args())
 
-if args['generate']:
-  if not args['numberOfTests'] == 5:
-    with open(args['file'], 'r') as f:
-      template = sub("{(?:(?!{).)*os%d.*?]" % args['numberOfTests'], ']', f.read(), flags=S)
-    with open(args['template'], 'w') as f:
-      f.write(template)
-  else:
-    copyfile(args['file'], args['template'])
+if args['generate'] and args['number'] != 5:
+  with open(args['file'], 'r') as f:
+    temp = sub("{(?:(?!{).)*os%d.*?]" % args['number'], ']', f.read(), flags=S)
+  with open(args['template'], 'w') as f:
+    f.write(temp)
 
-  if not path.isfile(f'./specs/{args["scriptName"]}MainTest.js'):
-    copyfile('./specs/exampleTest.js', f'./specs/{args["scriptName"]}MainTest.js')
+elif args['generate']:
+  copyfile(args['file'], args['template'])
+  if not path.isfile(f'./specs/{args["script"]}MainTest.js'):
+    copyfile('./specs/exampleTest.js', f'./specs/{args["script"]}MainTest.js')
 
-replace = {}
-replace.update(args)
+conf = {}
+conf.update(args)
 
 if args['batch'] == 'Android':
-  replace['real_mobile'] = True
-  replace['isAndroid'] = True
-  replace['isIOS'] = False
+  conf['real_mobile'] = True
+  conf['isAndroid'] = True
+  conf['isIOS'] = False
 elif args['batch'] == 'iOS':
-  replace['real_mobile'] = True
-  replace['isAndroid'] = False
-  replace['isIOS'] = True
+  conf['real_mobile'] = True
+  conf['isAndroid'] = False
+  conf['isIOS'] = True
 else:
-  replace['real_mobile'] = False
-  replace['isAndroid'] = False
-  replace['isIOS'] = False
+  conf['real_mobile'] = False
+  conf['isAndroid'] = False
+  conf['isIOS'] = False
 
 if args['app'] and args['batch'] == 'Android':
-  replace['app'] = 'browserStackConfig.BROWSERSTACK_APK_URL'
+  conf['app'] = 'browserStackConfig.BROWSERSTACK_APK_URL'
 elif args['app'] and args['batch'] == 'iOS':
-  replace['app'] = 'browserStackConfig.BROWSERSTACK_IPA_URL'
+  conf['app'] = 'browserStackConfig.BROWSERSTACK_IPA_URL'
 else:
-  replace['app'] = "''"
+  conf['app'] = "''"
 
-del replace['batch']
-del replace['file']
-del replace['json']
-del replace['write']
-del replace['interchange']
+del conf['batch'], conf['file'], conf['json'], conf['write'], conf['trade']
 
 # Load json
 with open(args['json'], 'r') as f:
@@ -72,32 +85,32 @@ with open(args['json'], 'r') as f:
 
 devices = devices[args['batch']]
 
-if args['interchange']:
+if args['trade']:
   while True:
-    print(f'{args["numberOfTests"]} test case(s) will be run.')
+    print(f'{args["number"]} test case(s) will be run. (Ctrl-C to exit)')
     for key, value in devices.items():
       print(f'{key}: {value}')
     try:
-      testNumber1, testNumber2 = input('Enter two tests to swap. (ctrl-c to exit): ').split()
+      test1, test2 = input('Enter two tests to swap.').split()
     except KeyboardInterrupt:
       break
     except ValueError:
       print('Example: 2 3')
-    if devices[testNumber1] and devices[testNumber2]:
+    if devices[test1] and devices[test2]:
       # swap tests
-      devices[testNumber1], devices[testNumber2] = devices[testNumber2], devices[testNumber1]
+      devices[test1], devices[test2] = devices[test2], devices[test1]
 
-for i in range(args['numberOfTests']):
+for i in range(args['number']):
   # generate default values
-  replace[f'os{i}'] = ''
-  replace[f'browser_version{i}'] = ''
-  replace[f'browserstack.appium_version{i}'] = ''
-  replace[f'device{i}'] = ''
+  conf[f'os{i}'] = ''
+  conf[f'browser_version{i}'] = ''
+  conf[f'browserstack.appium_version{i}'] = ''
+  conf[f'device{i}'] = ''
 
-for i in range(args['offset'], args['offset'] + args['numberOfTests']):
+for i in range(args['offset'], args['offset'] + args['number']):
   # import from json file
   for key, value in devices[str(i)].items():
-    replace[f'{key}{i}'] = value
+    conf[f'{key}{i}'] = value
     
 if args['write']:
   config = open('wdio.conf.js', 'w')
@@ -105,7 +118,7 @@ if args['write']:
 with open(args['template'], 'r') as template:
   for line in template.readlines():
     # create output file with configuration values
-    for configOption, value in replace.items():
+    for configOption, value in conf.items():
       # Replace string 
       if type(value) == bool and value:
         line = sub(f'%{configOption}', 'true', line)
