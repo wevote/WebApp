df09937759729edd99b584fd0ae8cfdfa7c7058c
dalemcgrew <DaleMcGrew@yahoo.com>
2020-08-19 12:28:39 -0700

    Updated route "applesigninprocess". Still needs to be tested.

--- src/js/routes/Process/AppleSignInProcess.jsx
+++ src/js/routes/Process/AppleSignInProcess.jsx
@@ -4,13 +4,9 @@ import AppStore from '../../stores/AppStore';
 import cookies from '../../utils/cookies';
 import { cordovaScrollablePaneTopPadding } from '../../utils/cordovaOffsets';
 import { historyPush, isWebApp } from '../../utils/cordovaUtils';
-import LoadingWheel from '../../components/LoadingWheel';
 import { oAuthLog, renderLog } from '../../utils/logging';
 import { stringContains } from '../../utils/textFormat';
-import TwitterActions from '../../actions/TwitterActions';
-import TwitterStore from '../../stores/TwitterStore';
 import VoterStore from '../../stores/VoterStore';
-import VoterActions from '../../actions/VoterActions';
 
 export default class AppleSignInProcess extends Component {
   static propTypes = {
@@ -20,27 +16,25 @@ export default class AppleSignInProcess extends Component {
     super(props);
     this.state = {
       hostname: '',
-      mergingTwoAccounts: false,
       redirectInProgress: false,
-      twitterAuthResponse: {},
-      yesPleaseMergeAccounts: false,
+      voterWeVoteId: '',
     };
   }
 
   componentDidMount () {
     this.appStoreListener = AppStore.addListener(this.onAppStoreChange.bind(this));
-    this.twitterStoreListener = TwitterStore.addListener(this.onTwitterStoreChange.bind(this));
     this.voterStoreListener = VoterStore.addListener(this.onVoterStoreChange.bind(this));
-    this.twitterSignInRetrieve();
     const hostname = AppStore.getHostname();
+    const voter = VoterStore.getVoter();
+    const { we_vote_id: voterWeVoteId } = voter;
     this.setState({
       hostname,
+      voterWeVoteId,
     });
   }
 
   componentWillUnmount () {
     this.appStoreListener.remove();
-    this.twitterStoreListener.remove();
     this.voterStoreListener.remove();
   }
 
@@ -51,28 +45,29 @@ export default class AppleSignInProcess extends Component {
     });
   }
 
-  onTwitterStoreChange () {
-    this.setState({
-      twitterAuthResponse: TwitterStore.getTwitterAuthResponse(),
-    });
-  }
-
   onVoterStoreChange () {
-    const { redirectInProcess } = this.state;
+    const { redirectInProcess, voterWeVoteId } = this.state;
     // console.log('AppleSignInProcess onVoterStoreChange, redirectInProcess:', redirectInProcess);
     if (!redirectInProcess) {
-      const twitterSignInStatus = VoterStore.getTwitterSignInStatus();
       // console.log('twitterSignInStatus:', twitterSignInStatus);
       const voter = VoterStore.getVoter();
-      const { signed_in_twitter: voterIsSignedInTwitter } = voter;
-      if (voterIsSignedInTwitter || (twitterSignInStatus && twitterSignInStatus.voter_merge_two_accounts_attempted)) {
-        // Once the Twitter merge returns successfully, redirect to starting page
+      if (!voterWeVoteId) {
+        const { we_vote_id: voterWeVoteId } = voter;
+        this.setState({
+          voterWeVoteId,
+        });
+      }
+      const { signed_in_with_apple: voterIsSignedInWithApple } = voter;
+      this.setState({
+        voterIsSignedInWithApple,
+      });
+      if (voterIsSignedInWithApple) {
+        // Once the voter data returns successfully, redirect to starting page
         let redirectFullUrl = '';
         let signInStartFullUrl = cookies.getItem('sign_in_start_full_url');
         // console.log('AppleSignInProcess signInStartFullUrl:', signInStartFullUrl);
-        if (signInStartFullUrl && stringContains('twitter_sign_in', signInStartFullUrl)) {
-          // Do not support a redirect to facebook_sign_in
-          // console.log('AppleSignInProcess Ignore facebook_sign_in url');
+        if (signInStartFullUrl && stringContains('applesigninprocess', signInStartFullUrl)) {
+          // Do not support a redirect to applesigninprocess
           signInStartFullUrl = null;
         }
         if (signInStartFullUrl) {
@@ -81,9 +76,6 @@ export default class AppleSignInProcess extends Component {
           cookies.removeItem('sign_in_start_full_url', '/');
           cookies.removeItem('sign_in_start_full_url', '/', 'wevote.us');
           redirectFullUrl = signInStartFullUrl;
-          // if (!voterHasDataToPreserve) {
-          //   redirectFullUrl += '?voter_refresh_timer_on=1';
-          // }
           let useWindowLocationAssign = true;
           if (window && window.location && window.location.origin) {
             if (stringContains(window.location.origin, redirectFullUrl)) {
@@ -92,17 +84,17 @@ export default class AppleSignInProcess extends Component {
               const newRedirectPathname = isWebApp() ? redirectFullUrl.replace(window.location.origin, '') : '/ballot';
               // console.log('newRedirectPathname:', newRedirectPathname);
               this.setState({ redirectInProcess: true });
-              oAuthLog(`Twitter sign in (1), onVoterStoreChange - push to ${newRedirectPathname}`);
+              oAuthLog(`Apple sign in (1), onVoterStoreChange - push to ${newRedirectPathname}`);
 
               historyPush({
                 pathname: newRedirectPathname,
                 state: {
-                  message: 'You have successfully signed in with Twitter.',
+                  message: 'You have successfully signed in with Apple.',
                   message_type: 'success',
                 },
               });
             } else {
-              // console.log('window.location.origin empty');
+              console.log('window.location.origin empty');
             }
           }
           if (useWindowLocationAssign) {
@@ -113,12 +105,12 @@ export default class AppleSignInProcess extends Component {
         } else {
           this.setState({ redirectInProcess: true });
           const redirectPathname = '/ballot';
-          oAuthLog(`Twitter sign in (2), onVoterStoreChange - push to ${redirectPathname}`);
+          oAuthLog(`Apple sign in (2), onVoterStoreChange - push to ${redirectPathname}`);
           historyPush({
             pathname: redirectPathname,
             // query: {voter_refresh_timer_on: voterHasDataToPreserve ? 0 : 1},
             state: {
-              message: 'You have successfully signed in with Twitter.',
+              message: 'You have successfully signed in with your Apple Id.',
               message_type: 'success',
             },
           });
@@ -127,137 +119,39 @@ export default class AppleSignInProcess extends Component {
     }
   }
 
-  voterMergeTwoAccountsByTwitterKey (twitterSecretKey) {  // , voterHasDataToPreserve = true
-    const { mergingTwoAccounts } = this.state;
-    if (mergingTwoAccounts) {
-      // console.log('In process of mergingTwoAccounts');
-    } else {
-      // console.log('About to make voterMergeTwoAccountsByTwitterKey API call');
-      VoterActions.voterMergeTwoAccountsByTwitterKey(twitterSecretKey);
-      // Prevent voterMergeTwoAccountsByFacebookKey from being called multiple times
-      this.setState({ mergingTwoAccounts: true });
-    }
-  }
-
-  // This creates the public.twitter_twitterlinktovoter entry, which is needed
-  // to establish is_signed_in within the voter.voter
-  voterTwitterSaveToCurrentAccount () {
-    // console.log('voterTwitterSaveToCurrentAccount');
-    VoterActions.voterTwitterSaveToCurrentAccount();
-    // const signInStartFullUrl = cookies.getItem('sign_in_start_full_url');
-    // if (signInStartFullUrl) {
-    //   AppActions.unsetStoreSignInStartFullUrl();
-    //   cookies.removeItem('sign_in_start_full_url', '/');
-    //   cookies.removeItem('sign_in_start_full_url', '/', 'wevote.us');
-    //   window.location.assign(signInStartFullUrl);
-    // } else {
-    //   const redirectPathname = '/ballot';
-    //   historyPush({
-    //     pathname: redirectPathname,
-    //     state: {
-    //       message: 'You have successfully signed in with Twitter.',
-    //       message_type: 'success',
-    //     },
-    //   });
-    // }
-    if (VoterStore.getVoterPhotoUrlMedium().length === 0) {
-      // This only fires once, for brand new users on their very first login
-      VoterActions.voterRetrieve();
-    }
-  }
-
-  twitterSignInRetrieve () {
-    TwitterActions.twitterSignInRetrieve();
-  }
-
   render () {
     renderLog('AppleSignInProcess');  // Set LOG_RENDER_EVENTS to log all renders
-    const { hostname, mergingTwoAccounts, redirectInProgress, twitterAuthResponse, yesPleaseMergeAccounts } = this.state;
+    const { hostname, redirectInProgress, voterIsSignedInWithApple, voterWeVoteId } = this.state;
     // console.log('AppleSignInProcess render, redirectInProgress:', redirectInProgress);
-    if (redirectInProgress || !hostname || hostname === '') {
+    if (redirectInProgress || !hostname || hostname === '' || !voterWeVoteId) {
       return null;
     }
 
     oAuthLog('AppleSignInProcess render');
-    if (!twitterAuthResponse ||
-      !twitterAuthResponse.twitter_retrieve_attempted) {
-      oAuthLog('STOPPED, missing twitter_retrieve_attempted: twitterAuthResponse:', twitterAuthResponse);
+    if (!voterIsSignedInWithApple) {
+      oAuthLog('STOPPED, voterIsSignedInWithApple is FALSE');
       return (
-        <div className="twitter_sign_in_root">
+        <div className="apple_sign_in_root">
           <div className="page-content-container" style={{ paddingTop: `${cordovaScrollablePaneTopPadding()}` }}>
             <div style={{ textAlign: 'center' }}>
-              Waiting for response from Twitter...
-            </div>
-            <div className="u-loading-spinner__wrapper">
-              <div className="u-loading-spinner">Please wait...</div>
+              Apple Sign in did not work for some reason.
+              <br />
+              <br />
+              Please try again, or contact us using the &apos;Help&apos; icon below.
             </div>
           </div>
         </div>
       );
     }
-    oAuthLog('=== Passed initial gate === with twitterAuthResponse: ', twitterAuthResponse);
-    const { twitter_secret_key: twitterSecretKey } = twitterAuthResponse;
 
-    if (twitterAuthResponse.twitter_sign_in_failed) {
-      oAuthLog('Twitter sign in failed - push to /settings/account');
-      this.setState({ redirectInProcess: true });
-      historyPush({
-        pathname: '/settings/account',
-        state: {
-          message: 'Twitter sign in failed. Please try again.',
-          message_type: 'success',
-        },
-      });
-      return LoadingWheel;
-    }
-
-    if (yesPleaseMergeAccounts) {
-      // Go ahead and merge this voter record with the voter record that the twitterSecretKey belongs to
-      oAuthLog('this.voterMergeTwoAccountsByTwitterKey -- yes please merge accounts');
-      this.voterMergeTwoAccountsByTwitterKey(twitterSecretKey);
-      return LoadingWheel;
-    }
-
-    // This process starts when we return from attempting voterTwitterSignInRetrieve
-    // If twitter_sign_in_found NOT True, go back to the sign in page to try again
-    if (!twitterAuthResponse.twitter_sign_in_found) {
-      this.setState({ redirectInProcess: true });
-      oAuthLog('twitterAuthResponse.twitter_sign_in_found', twitterAuthResponse.twitter_sign_in_found);
-      historyPush({
-        pathname: '/settings/account',
-        state: {
-          message: 'Twitter authentication not found. Please try again.',
-          message_type: 'warning',
-        },
-      });
-      return LoadingWheel;
-    }
-
-    // Is there a collision of two accounts?
-    if (twitterAuthResponse.existing_twitter_account_found) {
-      // For now are not asking to merge accounts
-      if (!mergingTwoAccounts) {
-        oAuthLog('twitterAuthResponse voterMergeTwoAccountsByTwitterKey');
-        this.voterMergeTwoAccountsByTwitterKey(twitterSecretKey);  // , twitterAuthResponse.voter_has_data_to_preserve
-      } else {
-        oAuthLog('twitterAuthResponse NOT CALLING voterMergeTwoAccountsByTwitterKey');
-      }
-      return (
-        <div className="twitter_sign_in_root">
-          <div className="page-content-container" style={{ paddingTop: `${cordovaScrollablePaneTopPadding()}` }}>
-            <div style={{ textAlign: 'center' }}>
-              Loading your account...
-            </div>
-            <div className="u-loading-spinner__wrapper">
-              <div className="u-loading-spinner">Please wait...</div>
-            </div>
+    return (
+      <div className="apple_sign_in_waiting_root">
+        <div className="page-content-container" style={{ paddingTop: `${cordovaScrollablePaneTopPadding()}` }}>
+          <div className="u-loading-spinner__wrapper">
+            <div className="u-loading-spinner">Please wait...</div>
           </div>
         </div>
-      );
-    } else {
-      oAuthLog('Setting up new Twitter entry - voterTwitterSaveToCurrentAccount');
-      this.voterTwitterSaveToCurrentAccount();
-      return LoadingWheel;
-    }
+      </div>
+    );
   }
 }
