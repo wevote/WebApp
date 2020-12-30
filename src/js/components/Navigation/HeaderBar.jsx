b81b81e8f99e9b14f53750e3f603e4cdb20dd9b0
stevepodell <steve@podell.com>
2020-12-30 15:39:52 -0800

    Now capture the react-router-dom history object in global.weVoteGlobalHistory, no need to pass history everywhere we need to do a pushHistory Removed all passing of history

--- src/js/components/Navigation/HeaderBar.jsx
+++ src/js/components/Navigation/HeaderBar.jsx
@@ -346,7 +346,7 @@ class HeaderBar extends Component {
     if (stringContains('/modal/share', href) && isWebApp()) {
       const pathnameWithoutModalShare = href.replace('/modal/share', '');  // Cordova
       // console.log('Navigation closeShareModal ', pathnameWithoutModalShare)
-      historyPush.push(null, pathnameWithoutModalShare);
+      historyPush.push(pathnameWithoutModalShare);
     }
   }
 
