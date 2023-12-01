2fac5039a65c8857a186ae8e5fdbb5b54c16c7f6
dalemcgrew <DaleMcGrew@yahoo.com>
2023-11-30 20:59:23 -0800

    A snyk security upgrade moved forward the version of glob we are using, which required a minor syntax change. Also fixed spacing errors caught by ESLINT. Added comment to PoliticianImageDesktop fix.

--- src/js/common/components/Style/PoliticianDetailsStyles.jsx
+++ src/js/common/components/Style/PoliticianDetailsStyles.jsx
@@ -46,7 +46,7 @@ export const PoliticianImageMobile = styled('img')`
 export const PoliticianImageDesktop = styled('img')`
   border-radius: 5px;
   height: 100%;
-  max-height: 200px;
+  max-height: 200px; // Needs to be revisited when rectangular campaign images are used
 `;
 
 export const PoliticianImageSharedStyles = css`
