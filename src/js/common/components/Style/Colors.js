f58e5ef838199ea134134d51d44da828b3044d8f
Ivan Kordonets <ivankordonets@gmail.com>
2023-12-08 16:54:05 -0500

    add storybook components

---
+++ src/js/common/components/Style/Colors.js
@@ -0,0 +1,12 @@
+const colors = {
+  primary: '#0834CD',
+  primaryHover: '#09288A',
+  secondaryHover: '#F5F7FD',
+  darkGrey: '#454F69',
+  middleGrey: '#8C92A2',
+  grey: '#AEB2BE',
+  lightGrey: '#E5E6EA',
+  white: '#ffffff',
+};
+
+export default colors;
