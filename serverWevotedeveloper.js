437c5ce0e76816915ac5ceba891b1f26a0252da4
stevepodell <steve@podell.com>
2024-02-20 13:24:58 -0800

    Updated webpack to use wevotedeveloper.com SSL if configured Works on Safari and Firefox for webapp, but not on Chrome. Works on Chrome for local Python API server.

---
+++ serverWevotedeveloper.js
@@ -0,0 +1,25 @@
+/* eslint-disable */
+// start the express server
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const https = require("https");
+const path = require("path");
+const webAppConfig = require("./src/js/config");
+
+const port = 3000;
+const opts = { redirect: true };
+const hostname = "wevotedeveloper.com";
+
+app.use("/", express.static("build", opts));
+app.all("*", (req, res) => res.sendFile(__dirname + "/build/index.html"));
+
+const certOptions = {
+  key: fs.readFileSync(path.resolve(__dirname + "/src/cert/wevotedeveloper.com_key.txt")),
+  cert: fs.readFileSync(path.resolve(__dirname + "/src/cert/wevotedeveloper.com.crt")),
+};
+
+const server = https.createServer(certOptions, app).listen(port, function () {
+  console.log("INFO: https server started", new Date());
+  console.log(`INFO: Server is at https://${hostname}:${port}`);
+});
