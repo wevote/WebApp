5c9ce0c4c30f3fe9a50d41c5a289799787cded82
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2024-08-06 19:16:05 -0400

    added popover for like and dislike container, need to work on popover style

--- src/js/common/components/Widgets/HeartFavoriteToggle/HeartFavoriteToggleBase.jsx
+++ src/js/common/components/Widgets/HeartFavoriteToggle/HeartFavoriteToggleBase.jsx
@@ -5,6 +5,8 @@ import CampaignActions from '../../../actions/CampaignActions';
 import DesignTokenColors from '../../Style/DesignTokenColors';
 import numberWithCommas from '../../../utils/numberWithCommas';
 import HeartFavoriteToggleIcon from './HeartFavoriteToggleIcon';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 
 class HeartFavoriteToggleBase extends Component {
   constructor (props) {
@@ -14,6 +16,7 @@ class HeartFavoriteToggleBase extends Component {
       campaignXSupportersCountLocal: 0,
       voterOpposesLocal: false,
       voterSupportsLocal: false,
+      anchorEl: null, // Added for popovers
     };
   }
 
@@ -66,52 +69,55 @@ class HeartFavoriteToggleBase extends Component {
     }
   };
 
-  handleOpposeClick = () => {
+  handleOpposeClick = (event) => {
     const oppose = true;
     const support = false;
     const stopOpposing = false;
     const stopSupporting = false;
-    this.handleActionClick(support, oppose, stopSupporting, stopOpposing);
+    this.handleActionClick(event, support, oppose, stopSupporting, stopOpposing, 'Don’t like this politician?');
   }
 
-  handleStopOpposingClick = () => {
+  handleStopOpposingClick = (event) => {
     const oppose = false;
     const support = false;
     const stopOpposing = true;
     const stopSupporting = false;
-    this.handleActionClick(support, oppose, stopSupporting, stopOpposing);
+    this.handleActionClick(event, support, oppose, stopSupporting, stopOpposing, 'Don’t like this politician?');
   }
 
-  handleStopSupportingClick = () => {
+  handleStopSupportingClick = (event) => {
     const oppose = false;
     const support = false;
     const stopOpposing = false;
     const stopSupporting = true;
-    this.handleActionClick(support, oppose, stopSupporting, stopOpposing);
+    this.handleActionClick(event, support, oppose, stopSupporting, stopOpposing, 'Like this politician?');
   }
 
-  handleSupportClick = () => {
+  handleSupportClick = (event) => {
     const oppose = false;
     const support = true;
     const stopOpposing = false;
     const stopSupporting = false;
-    this.handleActionClick(support, oppose, stopSupporting, stopOpposing);
+    this.handleActionClick(event, support, oppose, stopSupporting, stopOpposing, 'Like this politician?');
   }
 
-  handleActionClick = (support = true, oppose = false, stopSupporting = false, stopOpposing = false) => {
+  handleActionClick = (event, support = true, oppose = false, stopSupporting = false, stopOpposing = false, popoverText = '') => {
     const { campaignXWeVoteId, voterSignedInWithEmail } = this.props;
     const {
       campaignXOpposersCountLocal: campaignXOpposersCountLocalPrevious,
       campaignXSupportersCountLocal: campaignXSupportersCountLocalPrevious,
-      showSignInPromptSupports: showSignInPromptSupportsPrevious,
       voterOpposesLocal: voterOpposesLocalPrevious,
       voterSupportsLocal: voterSupportsLocalPrevious,
     } = this.state;
+
     if (!voterSignedInWithEmail) {
       // Toggle sign in prompt
+      const popoverText = support ? 'like this politician?' : 'dislike this politician?';
       this.setState({
-        showSignInPromptSupports: (support) ? !showSignInPromptSupportsPrevious : false,
-        showSignInPromptOpposes: (oppose) ? !showSignInPromptSupportsPrevious : false,
+        showSignInPromptSupports: support ? !this.state.showSignInPromptSupports : false,
+        showSignInPromptOpposes: oppose ? !this.state.showSignInPromptOpposes : false,
+        anchorEl: event.currentTarget,
+        popoverText: popoverText,
       });
     } else {
       this.setState({
@@ -126,19 +132,14 @@ class HeartFavoriteToggleBase extends Component {
               if (this.props.submitSupport) {
                 this.props.submitSupport();
               }
-              // Local quick update of supporters_count in CampaignX object
-              const supportersCountLocal = this.state.campaignXSupportersCountLocal;
-              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, supportersCountLocal);
+              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, this.state.campaignXSupportersCountLocal);
             });
           }
           if (voterOpposesLocalPrevious) {
             this.setState({
               campaignXOpposersCountLocal: Math.max(0, campaignXOpposersCountLocalPrevious - 1),
             }, () => {
-              // Local quick update of opposers_count in CampaignX object
-              const opposersCountLocal = this.state.campaignXOpposersCountLocal;
-              const supportersCountLocal = false;
-              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, supportersCountLocal, opposersCountLocal);
+              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, this.state.campaignXSupportersCountLocal, this.state.campaignXOpposersCountLocal);
             });
           }
         } else if (stopSupporting) {
@@ -149,9 +150,7 @@ class HeartFavoriteToggleBase extends Component {
               if (this.props.submitStopSupporting) {
                 this.props.submitStopSupporting();
               }
-              // Local quick update of supporters_count in CampaignX object
-              const supportersCountLocal = this.state.campaignXSupportersCountLocal;
-              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, supportersCountLocal);
+              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, this.state.campaignXSupportersCountLocal);
             });
           }
         } else if (oppose) {
@@ -162,19 +161,14 @@ class HeartFavoriteToggleBase extends Component {
               if (this.props.submitOppose) {
                 this.props.submitOppose();
               }
-              // Local quick update of opposers_count in CampaignX object
-              const opposersCountLocal = this.state.campaignXOpposersCountLocal;
-              const supportersCountLocal = false;
-              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, supportersCountLocal, opposersCountLocal);
+              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, this.state.campaignXSupportersCountLocal, this.state.campaignXOpposersCountLocal);
             });
           }
           if (voterSupportsLocalPrevious) {
             this.setState({
               campaignXSupportersCountLocal: Math.max(0, campaignXSupportersCountLocalPrevious - 1),
             }, () => {
-              // Local quick update of supporters_count in CampaignX object
-              const supportersCountLocal = this.state.campaignXSupportersCountLocal;
-              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, supportersCountLocal);
+              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, this.state.campaignXSupportersCountLocal);
             });
           }
         } else if (stopOpposing) {
@@ -185,16 +179,20 @@ class HeartFavoriteToggleBase extends Component {
               if (this.props.submitStopOpposing) {
                 this.props.submitStopOpposing();
               }
-              // Local quick update of opposers_count in CampaignX object
-              const opposersCountLocal = this.state.campaignXOpposersCountLocal;
-              const supportersCountLocal = false;
-              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, supportersCountLocal, opposersCountLocal);
+              CampaignActions.campaignLocalAttributesUpdate(campaignXWeVoteId, this.state.campaignXSupportersCountLocal, this.state.campaignXOpposersCountLocal);
             });
           }
         }
       });
     }
   };
+
+  handlePopoverClose = () => {
+    this.setState({
+      anchorEl: null,
+      popoverText: ''
+    });
+  }
 
   render () {
     const {
@@ -207,16 +205,22 @@ class HeartFavoriteToggleBase extends Component {
       showSignInPromptSupports,
       voterOpposesLocal,
       voterSupportsLocal,
+      anchorEl,
+      popoverText,
     } = this.state;
+
+    const open = Boolean(anchorEl);
+    const id = open ? 'simple-popover' : undefined;
+
     // console.log('campaignXSupportersCountLocal', campaignXSupportersCountLocal, 'campaignXOpposersCountLocal', campaignXOpposersCountLocal);
     // console.log('HeartFavoriteToggleBase voterSupportsLocal', voterSupportsLocal, 'voterOpposesLocal', voterOpposesLocal);
     return (
       <HeartFavoriteToggleContainer>
-        <LikeContainer onClick={() => {
+        <LikeContainer onClick={(event) => {
           if (voterSupportsLocal) {
-            return this.handleStopSupportingClick();
+            return this.handleStopSupportingClick(event);
           } else {
-            return this.handleSupportClick();
+            return this.handleSupportClick(event);
           }
         }}
         >
@@ -230,15 +234,15 @@ class HeartFavoriteToggleBase extends Component {
             </span>
           )}
           {/* Add "like this politician?" popout */}
-          {(!voterSignedInWithEmail && showSignInPromptSupports) && (
+          {/* {(!voterSignedInWithEmail && showSignInPromptSupports) && (
             <span onClick={() => this.handleSignInClick()}>&nbsp;sign in</span>
-          )}
+          )} */}
         </LikeContainer>
-        <DislikeContainer onClick={() => {
+        <DislikeContainer onClick={(event) => {
           if (voterOpposesLocal) {
-            return this.handleStopOpposingClick();
+            return this.handleStopOpposingClick(event);
           } else {
-            return this.handleOpposeClick();
+            return this.handleOpposeClick(event);
           }
         }}
         >
@@ -251,11 +255,26 @@ class HeartFavoriteToggleBase extends Component {
               {numberWithCommas(campaignXOpposersCountLocal)}
             </span>
           )}
-          {/* Add "Don't like this politician?" popout */}
-          {(!voterSignedInWithEmail && showSignInPromptOpposes) && (
+          {/* {(!voterSignedInWithEmail && showSignInPromptOpposes) && (
             <span onClick={() => this.handleSignInClick()}>&nbsp;sign in</span>
-          )}
+          )} */}
         </DislikeContainer>
+          <Popover
+              id={id}
+              open={open}
+              anchorEl={anchorEl}
+              onClose={() => this.handlePopoverClose()}
+              anchorOrigin={{
+                vertical: 'bottom',
+                horizontal: 'left',
+              }}
+            >
+              <Typography variant="h6">{popoverText}</Typography>
+              <Typography variant="body1">Sign in to make your opinion count.</Typography>
+              <Typography variant="body1">
+                <a href="#" onClick={this.handleSignInClick}>Sign In</a>
+              </Typography>
+            </Popover>
       </HeartFavoriteToggleContainer>
     );
   }
