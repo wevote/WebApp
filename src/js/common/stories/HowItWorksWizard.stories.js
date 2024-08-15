5d9e1d9df4cb16f7d0eb9723601b1ab9939d4885
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2024-08-15 10:05:23 -0400

    Added steps objects array and activeStep radio button in controls

--- src/js/common/stories/HowItWorksWizard.stories.js
+++ src/js/common/stories/HowItWorksWizard.stories.js
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import HowItWorks from '../../components/CompleteYourProfile/HowItWorksWizard';
+import { action } from '@storybook/addon-actions';
 
 const steps = [
   {
     id: 1,
     title: 'How WeVote works',
     buttonText: '',
-    completed: true,
+    completed: false,
     description: '',
-    onClick: '',
+    onClick: (event) => action(event),
     titleCanBeClicked: true,
     width: '33.33%',
   },
@@ -19,7 +20,7 @@ const steps = [
     buttonText: '',
     completed: false,
     description: '',
-    onClick: '',
+    onClick: action('Step 2 Clicked'),
     titleCanBeClicked: true,
     width: '33.33%',
   },
@@ -29,19 +30,34 @@ const steps = [
     buttonText: 'Sign up to save choices',
     completed: false,
     description: '',
-    onClick: '',
+    onClick: action('Step 3 Clicked'),
     titleCanBeClicked: false,
     width: '33.33%',
   },
 ];
 
-const activeStep = 1;
+// const activeStep = 1;
 
 export default {
   title: 'Design System/CompleteYourProfile',
   component: HowItWorks,
   parameters: {
     layout: 'centered',
+  },
+  // argType: {
+  //   steps: { control: 'select' },
+  //   activeStep: { 
+  //     options: [1, 2, 3],
+  //     control: { type: 'select' },
+  //    },
+  // },
+  argTypes: {
+    activeStep: {
+      options: steps.map((step) => step.id),
+      control: { type: 'radio' },
+      description: 'Select which step is active',
+      onClick: action('Step Clicked'),
+    },
   },
 };
 
@@ -52,8 +68,19 @@ const Container = styled.div`
   height: 100vh;
 `;
 
-export const HowItWorksWizard = () => (
-  <Container>
-    <HowItWorks steps={steps} activeStep={activeStep} />
-  </Container>
-);
+export const HowItWorksWizard = (args) => {
+  useEffect(() => {
+    action(`Active Step Changed to: ${args.activeStep}`)();
+  }, [args.activeStep]);
+
+  return (
+    <Container>
+      <HowItWorks steps={steps} activeStep={args.activeStep} {...args}/>
+    </Container>
+  );
+};
+
+HowItWorksWizard.args = {
+  steps: steps,
+  activeStep: 1,
+};
