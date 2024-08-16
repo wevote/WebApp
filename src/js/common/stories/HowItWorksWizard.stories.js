d6fee2a26f586af39a6e01dc33e41f4095f3b7d2
Paolo Alberca <91231062+omgitsmiles@users.noreply.github.com>
2024-08-16 06:51:49 -0400

    fixed linting issues on HowItWorksWizard story

--- src/js/common/stories/HowItWorksWizard.stories.js
+++ src/js/common/stories/HowItWorksWizard.stories.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from 'react';
+import { action } from '@storybook/addon-actions';
 import styled from 'styled-components';
 import HowItWorks from '../../components/CompleteYourProfile/HowItWorksWizard';
-import { action } from '@storybook/addon-actions';
+
+// WV-380: Created controls for action ...
 
 const initialSteps = [
   {
@@ -36,8 +38,6 @@ const initialSteps = [
   },
 ];
 
-// const activeStep = 1;
-
 export default {
   title: 'Design System/CompleteYourProfile',
   component: HowItWorks,
@@ -47,13 +47,19 @@ export default {
   argTypes: {
     activeStep: {
       control: { type: 'check' },
-      options: initialSteps.map(step => step.id),
+      options: initialSteps.map((step) => step.id),
       description: 'Select which steps are active',
     },
     completed: {
       control: { type: 'boolean' },
       description: 'Toggle the completed status',
     },
+    // title: {
+    //   control: 'text',
+    //   options: initialSteps.map(step => step.title),
+    //   label: 'How WeVote works',
+    //   description: 'Modify the title',
+    // },
     steps: {
       control: 'object',
       description: 'Modify the steps',
@@ -80,18 +86,18 @@ const Container = styled.div`
 export const HowItWorksWizard = (args) => {
   const [steps, setSteps] = useState(args.steps);
   useEffect(() => {
-    setSteps(steps.map(step => ({
+    setSteps(steps.map((step) => ({
       ...step,
       completed: args.completed && args.activeStep.includes(step.id),
     })));
     action(`Active Step Changed to: ${args.activeStep}`)();
-  }, [args.activeStep]);
+  }, [args.activeStep, args.completed, steps]);
 
 
   const handleStepToggle = (index) => {
-    const updatedSteps = steps.map((step, i) =>
+    const updatedSteps = steps.map((step, i) => (
       i === index ? { ...step, completed: !step.completed } : step
-    );
+    ));
     setSteps(updatedSteps);
     action(`Step ${index + 1} Clicked - Completed: ${updatedSteps[index].completed}`)();
   };
@@ -102,7 +108,8 @@ export const HowItWorksWizard = (args) => {
         ...step,
         onClick: () => handleStepToggle(i),
       }))}
-      activeStep={args.activeStep}/>
+      activeStep={args.activeStep}
+      />
     </Container>
   );
 };
--- src/js/components/Values/IssuesByBallotItemDisplayList.jsx
+++ src/js/components/Values/IssuesByBallotItemDisplayList.jsx
@@ -6,6 +6,8 @@ import IssueStore from '../../stores/IssueStore';
 import VoterGuideStore from '../../stores/VoterGuideStore';
 import signInModalGlobalState from '../../common/components/Widgets/signInModalGlobalState';
 import ValueNameWithPopoverDisplay from './ValueNameWithPopoverDisplay';
+import Chip from '@mui/material/Chip';
+import StickyPopover from '../Ballot/StickyPopover';
 
 // Show a voter a horizontal list of all of the issues they are following that relate to this ballot item
 class IssuesByBallotItemDisplayList extends Component {
@@ -197,7 +199,22 @@ class IssuesByBallotItemDisplayList extends Component {
             showEllipses = false;
           }
           return (
-            <ValueNameWithPopoverDisplay
+            <>
+            <Chip
+              variant="outlined"
+              // onClick={() => { console.log('chip clicked'); }}
+              key={`${ballotItemWeVoteId}-${oneIssue.issue_we_vote_id}`}
+              label={oneIssue.issue_name}
+              ballotItemDisplayName={ballotItemDisplayName}
+              ballotItemWeVoteId={ballotItemWeVoteId}
+              externalUniqueId={`${ballotItemWeVoteId}-${externalUniqueId}`}
+              issueFollowedByVoter={issueFollowedByVoter}
+              oneIssue={oneIssue}
+              showEllipses={showEllipses}
+              style={{ margin: "5px", borderRadius: 2, color: "#7b7b79", }}
+              />
+            {/* </StickyPopover> */}
+            {/* <ValueNameWithPopoverDisplay
               key={`${ballotItemWeVoteId}-${oneIssue.issue_we_vote_id}-${showEllipses}`}
               addComma={localCounter < issueRenderCountTemp}
               ballotItemDisplayName={ballotItemDisplayName}
@@ -206,7 +223,8 @@ class IssuesByBallotItemDisplayList extends Component {
               issueFollowedByVoter={issueFollowedByVoter}
               oneIssue={oneIssue}
               showEllipses={showEllipses}
-            />
+              /> */}
+              </>
           );
         } else {
           return null;
