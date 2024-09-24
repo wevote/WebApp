4718a02cb12a683c1557cc08f132493d118d2dcc
Steven <digitalexile99@gmail.com>
2024-09-24 11:30:54 -0600

    refactor styling

--- src/js/common/components/Challenge/ChallengeLeaderboardList.jsx
+++ src/js/common/components/Challenge/ChallengeLeaderboardList.jsx
@@ -3,28 +3,36 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import ChallengeParticipantListItem from './ChallengeParticipantListItem';
+import VoterStore from '../../../stores/VoterStore';
 
+const ChallengeLeaderboardList = ({ participantList }) => {
+  const [voterWeVoteID, setVoterWeVoteID] = React.useState('');
+  React.useEffect(() => {
+    // console.log('Fetching voterWeVoteID:', voterWeVoteID);
+    setVoterWeVoteID(VoterStore.getVoterWeVoteId());
+  }, []);
+
+  return (
+    <LeaderboardListContainer>
+      {participantList.map((participant) => (
+        <ChallengeParticipantListItem
+          key={`participantKey-${participant.voter_we_vote_id}`}
+          participant={participant}
+          isCurrentUser={participant.voter_we_vote_id === voterWeVoteID}
+        />
+      ))}
+    </LeaderboardListContainer>
+  );
+};
+ChallengeLeaderboardList.propTypes = {
+  currentVoterWeVoteId: PropTypes.string,
+  participantList: PropTypes.array,
+};
 const LeaderboardListContainer = styled.div`
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
   height: calc(100vh - 270px);
 `;
-
-const ChallengeLeaderboardList = ({ currentVoterWeVoteId, participantList }) => (
-  <LeaderboardListContainer>
-    {participantList.map((participant) => (
-      <ChallengeParticipantListItem
-          key={`participantKey-${participant.voter_we_vote_id}`}
-          participant={participant}
-          isCurrentUser={participant.voter_we_vote_id === currentVoterWeVoteId}
-      />
-    ))}
-  </LeaderboardListContainer>
-);
-ChallengeLeaderboardList.propTypes = {
-  currentVoterWeVoteId: PropTypes.string,
-  participantList: PropTypes.array,
-};
 
 export default ChallengeLeaderboardList;
--- src/js/common/components/Challenge/ChallengeParticipantListItem.jsx
+++ src/js/common/components/Challenge/ChallengeParticipantListItem.jsx
@@ -2,68 +2,76 @@ import React from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@mui/material';
 import DesignTokenColors from '../Style/DesignTokenColors';
+import speakerDisplayNameToInitials from '../../utils/speakerDisplayNameToInitials';
+
+const ChallengeParticipantListItem = ({ participant, isCurrentUser }) => {
+  const { sx, children } = speakerDisplayNameToInitials(participant.participant_name);
+  return (
+    <ParticipantItem isCurrentUser={isCurrentUser}>
+      <ParticipantRow>
+        <Rank>{`#${participant.rank}`}</Rank>
+        <Name>
+          <Avatar sx={sx}>
+            {children}
+          </Avatar>
+          {participant.participant_name}
+        </Name>
+        <Points>{participant.points}</Points>
+        <FriendsJoined>{participant.friends_who_joined}</FriendsJoined>
+      </ParticipantRow>
+      <Details>
+        {`${participant.friends_invited} invited, ${participant.friends_who_viewed} viewed challenge - ${participant.friends_who_viewed_plus} total views`}
+      </Details>
+    </ParticipantItem>
+  );
+};
 
 const ParticipantItem = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  padding: 15px 20px;
   background-color: ${(props) => (props.isCurrentUser ? '#f9e79f' : '#fff')};
+  padding: 15px 0px;
   border-bottom: 1px solid ${DesignTokenColors.neutral100};
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+`;
+
+const ParticipantRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 `;
 
 const Rank = styled.div`
   font-weight: bold;
   color: ${DesignTokenColors.neutral900};
-  width: 60px;
+  width: 60px; /* Adjust width as needed */
 `;
 
 const Name = styled.div`
+  flex: 1;
   display: flex;
   align-items: center;
-  gap: 10px;
-  flex: 1;
   margin-left: 10px;
+  gap: 10px;
   color: ${DesignTokenColors.neutral900};
 `;
 
 const Points = styled.div`
-  width: 200px;
   text-align: center;
   font-size: 14px;
   font-weight: bold;
   color: ${DesignTokenColors.neutral900};
+  width: 80px;
 `;
 
 const FriendsJoined = styled.div`
-  width: 30px;
   text-align: center;
   font-size: 14px;
   color: ${DesignTokenColors.neutral900};
+  width: 100px;
 `;
 
 const Details = styled.div`
   text-align: left;
   font-size: 14px;
+  margin-top: 8px;
 `;
-
-const ChallengeParticipantListItem = ({ participant, isCurrentUser }) => (
-  <ParticipantItem isCurrentUser={isCurrentUser}>
-    <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
-      <Rank>{`#${participant.rank}`}</Rank>
-      <Name>
-        <Avatar>{`${participant.participant_name.split(' ')[0][0]}${participant.participant_name.split(' ')[1][0]}`}</Avatar>
-        {' '}
-        {participant.participant_name}
-      </Name>
-      <Points>{participant.points}</Points>
-      <FriendsJoined>{participant.friends_who_joined}</FriendsJoined>
-    </div>
-    <Details>
-      {`${participant.friends_invited} invited, ${participant.friends_who_viewed} viewed challenge - ${participant.friends_who_viewed_plus} total views`}
-    </Details>
-  </ParticipantItem>
-);
 
 export default ChallengeParticipantListItem;
