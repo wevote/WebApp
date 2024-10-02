d64bc18ee6341416c7fc2f94c26abab17bfd64fa
fdiazq~ <fdiaz1231@outlook.com>
2024-10-02 11:42:14 -0700

    Added padding to chip

--- src/js/common/pages/ChallengeInviteFriends/ChallengeInviteFriends.jsx
+++ src/js/common/pages/ChallengeInviteFriends/ChallengeInviteFriends.jsx
@@ -234,8 +234,8 @@ const StyledChip = styled(Chip)`
   background-color: ${DesignTokenColors.confirmation700};
   color: ${DesignTokenColors.whiteUI};
   height: 20px;
-  align-items: center;
-  justify-content: center;
+  padding-top: 2px;
+  padding-bottom: 2px;
 `;
 
 export default withStyles(commonMuiStyles)(ChallengeInviteFriends);
