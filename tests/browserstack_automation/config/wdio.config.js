f9c23efe1fbb8f53683508d94c800b194245b748
lavishaGit <97263933+lavishaGit@users.noreply.github.com>
2024-10-03 11:59:59 -0700

    Update wdio.config.js

--- tests/browserstack_automation/config/wdio.config.js
+++ tests/browserstack_automation/config/wdio.config.js
@@ -3,17 +3,17 @@ const { readFileSync } = require('fs');
 const browserStackConfig = require('./browserstack.config');
 const browserCapabilities = require('../capabilities/browser.json');
 
-// let mobileCapabilities = [];
+let mobileCapabilities = [];
 
-// try {
-//   const data = readFileSync('./tests/browserstack_automation/capabilities/mobile.json', { encoding: 'utf8' });
-//   mobileCapabilities = JSON.parse(data);
-// } catch (error) {
-//   // Run `npm run wdio:setup`
-// }
+try {
+  const data = readFileSync('./tests/browserstack_automation/capabilities/mobile.json', { encoding: 'utf8' });
+  mobileCapabilities = JSON.parse(data);
+} catch (error) {
+  // Run `npm run wdio:setup`
+}
 
-// const capabilities = [...browserCapabilities, ...mobileCapabilities];
-const capabilities = [...browserCapabilities];
+const capabilities = [...browserCapabilities, ...mobileCapabilities];
+
 const date = new Date();
 
 const dateForDisplay = date.toDateString();
