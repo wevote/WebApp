d7363a8108bb5a636bf88d44278f014bcc6364e5
erinw <erinwise88@gmail.com>
2024-10-23 17:09:19 -0400

    add viewInviteeDetails component

--- src/js/common/components/ChallengeInviteeListRoot/ChallengeInviteeListItem.jsx
+++ src/js/common/components/ChallengeInviteeListRoot/ChallengeInviteeListItem.jsx
@@ -1,16 +1,26 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import withTheme from '@mui/styles/withTheme';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Avatar } from '@mui/material';
 import { withStyles } from '@mui/styles';
-import { RemoveRedEye, CheckCircle, Check, MoreHoriz } from '@mui/icons-material';
+import { RemoveRedEye, CheckCircle, Check, InfoOutlined, EditOutlined, MoreHoriz } from '@mui/icons-material';
+import Popover from '@mui/material/Popover';
+import Typography from '@mui/material/Typography';
 import DesignTokenColors from '../Style/DesignTokenColors';
 import ConfirmYouSentInviteButton from './ConfirmYouSentInviteButton';
 import InviteAgainButton from './InviteAgainButton';
 import speakerDisplayNameToInitials from '../../utils/speakerDisplayNameToInitials';
+import ViewInviteeDetails from '../ChallengeInviteFriends/ViewInviteeDetails';
+
 
 const ChallengeInviteeListItem = ({ invitee, classes }) => {
+
   // console.log('ChallengeInviteeListItem:', invitee);
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [viewInviteeDetails, setViewInviteeDetails] = useState(false);
+
+  const { sx, children } = speakerDisplayNameToInitials(invitee.invitee_name);
   let challengeStatusIconJsx = <></>;
   let challengeStatusMessage = '';
   if (invitee.challenge_joined) {
@@ -24,56 +34,91 @@ const ChallengeInviteeListItem = ({ invitee, classes }) => {
     challengeStatusMessage = 'Invite sent';
   }
 
-  let underNameJsx = <></>;
-  if (invitee.challenge_joined || invitee.invite_viewed || invitee.invite_sent === true) {
-    underNameJsx = (
-      <MessageContainer>
-        <MessageStatus>
-          {challengeStatusIconJsx}
-        </MessageStatus>
-        <MessageText>
-          {challengeStatusMessage}
-        </MessageText>
-      </MessageContainer>
-    );
-  } else if (invitee.invite_sent === false) {
-    underNameJsx = (
-      <ConfirmYouSentInviteButton
-        challengeInviteeId={invitee.invitee_id}
-        challengeWeVoteId={invitee.challenge_we_vote_id}
-      />
-    );
-  }
-  const inviteeName = invitee.invitee_voter_name || invitee.invitee_name;
-  const { sx, children } = speakerDisplayNameToInitials(inviteeName);
+  const onDotButtonClick = (e) => {
+    setAnchorEl(e.currentTarget);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const openViewInviteeDetails = () => {
+    setViewInviteeDetails(true);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
   return (
     <InvitedFriendDetails>
       <PrimaryDetails>
         <FriendName>
-          {invitee.we_vote_hosted_profile_image_url_medium !== '' ? (
-            <AvatarDetails src={invitee.we_vote_hosted_profile_image_url_medium} alt={inviteeName} />
-          ) : (
-            <AvatarDetails sx={sx}>{children}</AvatarDetails>
-          )}
+          <AvatarDetails sx={sx}>{children}</AvatarDetails>
           {' '}
-          <Name>{inviteeName}</Name>
+          <Name>{invitee.invitee_name}</Name>
         </FriendName>
         <VerticalLine />
         <ActivityCommentEditWrapper>
-          <MoreHoriz />
+          <button type="button" aria-label="source" onClick={onDotButtonClick}>
+            <MoreHoriz />
+          </button>
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handlePopoverClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <PopoverWrapper>
+              <PopoverNameAndMessageText>
+                <StyledTypography>
+                  <EditOutlined style={{ fontSize:'14px', cursor: 'pointer', marginRight: '4px' }}/>
+                  Edit name & message
+                </StyledTypography>
+              </PopoverNameAndMessageText>
+              <PopoverViewDetailsText>
+                <StyledTypography onClick={ openViewInviteeDetails } >
+                  <InfoOutlined style={{ fontSize: '14px', cursor: 'pointer', marginRight: '4px' }}/>
+                  View details
+                </StyledTypography>
+              </PopoverViewDetailsText>
+            </PopoverWrapper>
+          </Popover>
+          <ViewInviteeDetails
+            show={viewInviteeDetails}
+            setShow={setViewInviteeDetails}
+            setAnchorEl={setAnchorEl}
+          />
         </ActivityCommentEditWrapper>
       </PrimaryDetails>
       <Options>
         <div>
-          {underNameJsx}
+          {invitee.invite_sent === false ? (
+            <ConfirmYouSentInviteButton
+              challengeInviteeId={invitee.invitee_id}
+              challengeWeVoteId={invitee.challenge_we_vote_id}
+            />
+          ) : (
+            <MessageContainer>
+              <MessageStatus>
+                {challengeStatusIconJsx}
+              </MessageStatus>
+              {challengeStatusMessage}
+            </MessageContainer>
+          )}
         </div>
-        {!(invitee.challenge_joined) ? (
+        {invitee.messageStatus !== 'Challenge Joined' && (
           <InviteAgainButton
             challengeInviteeId={invitee.invitee_id}
             challengeWeVoteId={invitee.challenge_we_vote_id}
           />
-        ) : (
-          <div>&nbsp;</div>
         )}
       </Options>
     </InvitedFriendDetails>
@@ -89,10 +134,10 @@ const styles = () => ({
   searchButton: {
     borderRadius: 50,
   },
+
 });
 
-
-const InvitedFriendDetails = styled('div')`
+const InvitedFriendDetails = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
@@ -101,13 +146,13 @@ const InvitedFriendDetails = styled('div')`
   border-bottom: 1px solid ${DesignTokenColors.neutral100};
 `;
 
-const PrimaryDetails = styled('div')`
+const PrimaryDetails = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
 `;
 
-const FriendName = styled('div')`
+const FriendName = styled.div`
   display: flex;
   align-items: center;
   gap: 10px;
@@ -120,29 +165,23 @@ const AvatarDetails = styled(Avatar)`
   font-size: 1rem;
 `;
 
-const Name = styled('div')`
+const Name = styled.div`
   font-weight: bold;
   color: ${DesignTokenColors.neutral900};
 `;
 
-const MessageContainer = styled('div')`
-  align-items: center;
+const MessageContainer = styled.div`
   display: flex;
-  margin-left: 45px;
 `;
 
-const MessageStatus = styled('div')`
+const MessageStatus = styled.div`
   text-align: center;
   font-size: 14px;
   color: ${DesignTokenColors.confirmation800};
   margin-right: 10px;
 `;
 
-const MessageText = styled('div')`
-  margin-top: 3px;
-`;
-
-const VerticalLine = styled('div')`
+const VerticalLine = styled.div`
   border-left: 1px solid ${DesignTokenColors.neutral200};
   height: 30px;
   margin: 0 10px;
@@ -157,11 +196,34 @@ const ActivityCommentEditWrapper = styled('div')`
   }
 `;
 
-const Options = styled('div')`
+const Options = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   font-size: 14px;
 `;
 
-export default withStyles(styles)(ChallengeInviteeListItem);
+const Invite = styled.a`
+  padding: 5px;
+  color: #4371cc;
+`;
+
+const PopoverWrapper = styled('div')`
+  padding: 5px;
+`;
+
+const PopoverNameAndMessageText = styled('div')`
+  padding: 6px;
+`;
+
+const PopoverViewDetailsText = styled('div')`
+  padding: 6px;
+  cursor: pointer;
+`;
+
+const StyledTypography = styled('div')`
+  font-size: 12px;
+  font-family: inherit;
+`
+// component="div" style={{ fontSize: '12px', fontFamily: 'inherit' }}
+export default withTheme(withStyles(styles)(ChallengeInviteeListItem));
