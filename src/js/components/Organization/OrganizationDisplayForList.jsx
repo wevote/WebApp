aea47c833c87ca54b5628cc15185edacfe7badb7
stevepodell <steve@podell.com>
2025-07-09 12:13:22 -0700

    Upgrade Storybook, to clear npm  high severity vulnerabilities

--- src/js/components/Organization/OrganizationDisplayForList.jsx
+++ src/js/components/Organization/OrganizationDisplayForList.jsx
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import React, { Component, Suspense } from 'react';
 import { Link } from 'react-router-dom';
+import TagManager from 'react-gtm-module';
 import styled from 'styled-components';
 import { renderLog } from '../../common/utils/logging';
 import removeTwitterNameFromDescription from '../../common/utils/removeTwitterNameFromDescription';
@@ -10,6 +11,8 @@ import PositionInformationOnlySnippet from '../PositionItem/PositionInformationO
 import PositionRatingSnippet from '../PositionItem/PositionRatingSnippet';
 import PositionSupportOpposeSnippet from '../PositionItem/PositionSupportOpposeSnippet';
 import TwitterAccountStats from '../Widgets/TwitterAccountStats';
+import VoterStore from '../../stores/VoterStore';
+import lookupPageNameAndPageTypeDict from '../../utils/lookupPageNameAndPageTypeDict';
 
 const FollowToggle = React.lazy(() => import(/* webpackChunkName: 'FollowToggle' */ '../Widgets/FollowToggle'));
 
@@ -103,6 +106,37 @@ class OrganizationDisplayForList extends Component {
     });
   }
 
+  sendEndorserClickEvent (buttonId) {
+    const { location: { pathname: currentPathname } } = window;
+    const { pageName, pageType } = lookupPageNameAndPageTypeDict(currentPathname);
+    const { twitterHandle } = this.state;
+    const { organizationWeVoteId } = this.props;
+    const destinationPathname = twitterHandle ? `/${twitterHandle}` : `/voterguide/${organizationWeVoteId}`;
+    const { pageName: destinationPageName, pageType: destinationPageType } = lookupPageNameAndPageTypeDict(destinationPathname);
+
+    const dataLayerObject = {
+      actionDetails: {
+        actionType: 'navigate',
+        buttonId,
+      },
+      event: 'action',
+      destinationDetails: {
+        destinationPageName,
+        destinationPageType,
+        destinationPathname,
+
+      },
+      pageDetails: {
+        pageName,
+        pageType,
+        pathname: currentPathname,
+      },
+      userDetails: VoterStore.getAnalyticsUserDetails(),
+    };
+    TagManager.dataLayer({ dataLayer: dataLayerObject });
+  }
+
+
   render () {
     renderLog('OrganizationDisplayForList');  // Set LOG_RENDER_EVENTS to log all renders
     // console.log('OrganizationDisplayForList render');
@@ -186,13 +220,20 @@ class OrganizationDisplayForList extends Component {
       <OrganizationDisplayForListWrapper>
         <OrganizationDetailsWrapper>
           <OrganizationLogoWrapper>
-            <Link id="organizationDisplayLogo" to={voterGuideLink} className="u-no-underline">
+            <Link id="organizationDisplayLogo"
+                  to={voterGuideLink}
+                  className="u-no-underline"
+                  onClick={() => this.sendEndorserClickEvent(organizationName)}
+            >
               {organizationLogo}
             </Link>
           </OrganizationLogoWrapper>
           <div>
             <NameAndTwitter>
-              <Link id="organizationDisplayName" to={voterGuideLink}>
+              <Link id="organizationDisplayName"
+                    to={voterGuideLink}
+                    onClick={() => this.sendEndorserClickEvent(organizationName)}
+              >
                 <OrganizationName>{organizationName}</OrganizationName>
               </Link>
               {twitterHandle && (
