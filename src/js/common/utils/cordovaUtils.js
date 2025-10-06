a8350b035e4cfc5946515ea0a68a215c2b93bae2
stevepodell <steve@podell.com>
2025-10-06 12:29:43 -0700

    Cordova Android, handle "close keyboard" button on the Android navigation bar Fix for https://wevoteusa.atlassian.net/issues/?filter=10941&selectedIssue=WV-299 Relies on a corresponding Cordova change

--- src/js/common/utils/cordovaUtils.js
+++ src/js/common/utils/cordovaUtils.js
@@ -697,16 +697,24 @@ export function setGlobalScreenSize (result) {
   window.pbakondyScreenSize = result;
 }
 
+// eslint-disable-next-line no-unused-vars
 export function focusTextFieldAndroid (clue) {
-  if (isAndroid()) {
-    prepareForCordovaKeyboard(clue);
-  }
+  // https://stackoverflow.com/questions/79783205/javascript-webview-on-android-need-to-be-able-to-detect-the-virtual-keyboard-be
+  // Changed android:windowSoftInputMode from adjustResize to adjustPan to allow Android to cover the window with the virtual keyboard
+  // instead of reducing the size of the DOM area when the virtual menu appears and the navigation bar down arrow
+  // (remove virtual keyboard) button is pressed
+
+  // 10/9/25 so this prepareForCordovaKeyboard() is not needed anymore;
+
+  // if (isAndroid()) {
+  //   prepareForCordovaKeyboard(clue);
+  // }
 }
 
 export function blurTextFieldAndroid () {
-  if (isAndroid()) {
-    restoreStylesAfterCordovaKeyboard('AddFriendsByEmail');
-  }
+  // if (isAndroid()) {
+  //   restoreStylesAfterCordovaKeyboard('AddFriendsByEmail');
+  // }
 }
 
 export function chipLabelText (fullLabel) {
