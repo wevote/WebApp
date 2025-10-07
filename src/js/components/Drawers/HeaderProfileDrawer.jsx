d23c9583958fd58a88cd651a76d260cc031ef4c6
stevepodell <steve@podell.com>
2025-10-07 14:58:41 -0700

    Cordova Android, handle "close keyboard" button on the Android navigation bar Fix for https://wevoteusa.atlassian.net/browse/WV-299 Relies on a corresponding Cordova change https://github.com/wevote/WeVoteCordova/pull/184 Also added: Fix for https://wevoteusa.atlassian.net/browse/WV-1893   Pixel 7 top header Fix for https://wevoteusa.atlassian.net/browse/WV-1368   Restore non-working "Help Wind" button

--- src/js/components/Drawers/HeaderProfileDrawer.jsx
+++ src/js/components/Drawers/HeaderProfileDrawer.jsx
@@ -1,24 +1,21 @@
-import {
-  AccountCircle, CampaignRounded,
-  ContentCopy, ExitToAppRounded, ImportContactsOutlined, Lock,
-  Menu, SecurityRounded,
-} from '@mui/icons-material';
+import { AccountCircle, CampaignRounded, ContentCopy, ExitToAppRounded, ImportContactsOutlined, Lock, Menu, SecurityRounded } from '@mui/icons-material';
 import React, { useCallback, useEffect, useState } from 'react';
 import CopyToClipboard from 'react-copy-to-clipboard';
 import TagManager from 'react-gtm-module';
 import styled from 'styled-components';
-import DrawerTemplateHeaderProfile from './DrawerTemplateHeaderProfile';
-import DesignTokenColors from '../../common/components/Style/DesignTokenColors';
-import webAppConfig from '../../config';
-import SettingsProfile from '../Settings/SettingsProfile';
-import lookupPageNameAndPageTypeDict, { getPageDetails } from '../../utils/lookupPageNameAndPageTypeDict';
-import VoterStore from '../../stores/VoterStore';
 import VoterSessionActions from '../../actions/VoterSessionActions';
+import DesignTokenColors from '../../common/components/Style/DesignTokenColors';
 import AppObservableStore, { messageService } from '../../common/stores/AppObservableStore';
-import SettingsYourData from '../Settings/SettingsYourData';
-import SettingsNotifications from '../Settings/SettingsNotifications';
+import historyPush from '../../common/utils/historyPush';
+import webAppConfig from '../../config';
+import VoterStore from '../../stores/VoterStore';
+import lookupPageNameAndPageTypeDict, { getPageDetails } from '../../utils/lookupPageNameAndPageTypeDict';
 import SettingsSectionFooter from '../Navigation/SettingsSectionFooter';
-import historyPush from "../../common/utils/historyPush";
+import SettingsNotifications from '../Settings/SettingsNotifications';
+import SettingsProfile from '../Settings/SettingsProfile';
+import SettingsYourData from '../Settings/SettingsYourData';
+import { NavLinksContainer } from '../Style/drawerLayoutStyles';
+import DrawerTemplateHeaderProfile from './DrawerTemplateHeaderProfile';
 
 // const OpenExternalWebSite = React.lazy(() => import(/* webpackChunkName: 'OpenExternalWebSite' */ '../../common/components/Widgets/OpenExternalWebSite'));
 const SignInOptionsPanel = React.lazy(() => import(/* webpackChunkName: 'SignInOptionsPanel' */ '../../common/components/SignIn/SignInOptionsPanel'));
@@ -412,12 +409,14 @@ const NavLinkContainer = styled('div')`
   }
 `;
 
-const NavLinksContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-  margin-left: -16px;
-  position: fixed;
-`;
+// Please do not copy styles -- centralize them somewhere, so that same named styles don't diverge,
+// and so that we don't have to maintain them twice
+// const NavLinksContainer = styled('div')`
+//   display: flex;
+//   flex-direction: column;
+//   margin-left: -16px;
+//   position: fixed;
+// `;
 
 // const ProfileComponentTitle = styled('div')`
 //   font-size: 20px;
